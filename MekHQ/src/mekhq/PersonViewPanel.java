39b3c915442ba653f08a5e0d8518ca885eaa28df
bombaijin <bombaijin@4d104363-cc00-4a03-b0b8-302e3804504c>
2011-04-26 17:38:31 +0000

    + replaced Personnel#GetDossier with a PersonnelViewPanel

--- MekHQ/src/mekhq/PersonViewPanel.java
+++ MekHQ/src/mekhq/PersonViewPanel.java
@@ -40,8 +40,6 @@ public class PersonViewPanel extends javax.swing.JPanel {
 	private javax.swing.JPanel pnlStats;
 	private javax.swing.JTextArea txtDesc;
 	
-	private javax.swing.JPanel pnlColFluff;
-	private javax.swing.JPanel pnlColStats;
 	private javax.swing.JLabel lblType;
 	private javax.swing.JLabel lblCall1;
 	private javax.swing.JLabel lblCall2;
@@ -102,9 +100,7 @@ public class PersonViewPanel extends javax.swing.JPanel {
 		pnlStats.setName("pnlStats");
 		pnlStats.setBorder(BorderFactory.createCompoundBorder(
 				BorderFactory.createTitledBorder(campaign.getFullTitleFor(person)),
-                BorderFactory.createEmptyBorder(5,5,5,5)));
-		//pnlStats.setMinimumSize(new java.awt.Dimension(250,200));
-		//pnlStats.setPreferredSize(new java.awt.Dimension(250,200));
+                BorderFactory.createEmptyBorder(2,2,2,2)));
 		pnlStats.setBackground(Color.WHITE);
 		fillStats();
 		gridBagConstraints = new java.awt.GridBagConstraints();
@@ -168,8 +164,9 @@ public class PersonViewPanel extends javax.swing.JPanel {
     
     public void fillStats() {
     	
-    	pnlColFluff = new javax.swing.JPanel();
-    	pnlColStats = new javax.swing.JPanel();
+    	org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(mekhq.MekHQApp.class).getContext().getResourceMap(PersonViewPanel.class);
+    	
+    	lblType = new javax.swing.JLabel();
     	lblCall1 = new javax.swing.JLabel();
 		lblCall2 = new javax.swing.JLabel();
 		lblAge1 = new javax.swing.JLabel();
@@ -184,109 +181,121 @@ public class PersonViewPanel extends javax.swing.JPanel {
     	java.awt.GridBagConstraints gridBagConstraints;
 		pnlStats.setLayout(new java.awt.GridBagLayout());
 		
-		pnlColFluff.setName("pnlColFluff");
-		pnlColFluff.setBackground(Color.RED);
-		pnlColFluff.setLayout(new java.awt.GridBagLayout());
+		lblType.setName("lblType"); // NOI18N
+		lblType.setText("<html><i>" + person.getTypeDesc() + "</i></html>");
 		gridBagConstraints = new java.awt.GridBagConstraints();
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
-		gridBagConstraints.weightx = 0.5;
-		gridBagConstraints.weighty = 1.0;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;	
-		pnlStats.add(pnlColFluff, gridBagConstraints);
-		
-		lblCall1.setName("lblCall1"); // NOI18N
-		lblCall1.setText("Callsign:");
-		gridBagConstraints = new java.awt.GridBagConstraints();
-		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridwidth = 4;
+		gridBagConstraints.weightx = 1.0;
+		gridBagConstraints.insets = new java.awt.Insets(0, 0, 5, 0);
 		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
 		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-		pnlColFluff.add(lblCall1, gridBagConstraints);
+		pnlStats.add(lblType, gridBagConstraints);
+		
+		lblCall1.setName("lblCall1"); // NOI18N
+		lblCall1.setText(resourceMap.getString("lblCall1.text"));
+		gridBagConstraints = new java.awt.GridBagConstraints();
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 1;
+		gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
+		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+		pnlStats.add(lblCall1, gridBagConstraints);
 		
 		lblCall2.setName("lblCall2"); // NOI18N
 		lblCall2.setText(person.getCallsign());
 		gridBagConstraints = new java.awt.GridBagConstraints();
 		gridBagConstraints.gridx = 1;
-		gridBagConstraints.gridy = 0;
-		gridBagConstraints.weightx = 1.0;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+		gridBagConstraints.gridy = 1;
+		gridBagConstraints.weightx = 0.5;
+		gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 0);
+		gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
 		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-		pnlColFluff.add(lblCall2, gridBagConstraints);
+		pnlStats.add(lblCall2, gridBagConstraints);
 		
 		lblAge1.setName("lblAge1"); // NOI18N
-		lblAge1.setText("Age:");
+		lblAge1.setText(resourceMap.getString("lblAge1.text"));
 		gridBagConstraints = new java.awt.GridBagConstraints();
 		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 1;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+		gridBagConstraints.gridy = 2;
+		gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
 		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-		pnlColFluff.add(lblAge1, gridBagConstraints);
+		pnlStats.add(lblAge1, gridBagConstraints);
 		
 		lblAge2.setName("lblAge2"); // NOI18N
 		lblAge2.setText(Integer.toString(person.getAge(campaign.getCalendar())));
 		gridBagConstraints = new java.awt.GridBagConstraints();
 		gridBagConstraints.gridx = 1;
-		gridBagConstraints.gridy = 1;
-		gridBagConstraints.weightx = 1.0;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+		gridBagConstraints.gridy = 2;
+		gridBagConstraints.weightx = 0.5;
+		gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 0);
+		gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
 		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-		pnlColFluff.add(lblAge2, gridBagConstraints);
+		pnlStats.add(lblAge2, gridBagConstraints);
 		
 		lblGender1.setName("lblGender1"); // NOI18N
-		lblGender1.setText("Gender:");
+		lblGender1.setText(resourceMap.getString("lblGender1.text"));
 		gridBagConstraints = new java.awt.GridBagConstraints();
 		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 2;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+		gridBagConstraints.gridy = 3;
+		gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
 		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-		pnlColFluff.add(lblGender1, gridBagConstraints);
+		pnlStats.add(lblGender1, gridBagConstraints);
 		
 		lblGender2.setName("lblGender2"); // NOI18N
 		lblGender2.setText(person.getGenderName());
 		gridBagConstraints = new java.awt.GridBagConstraints();
 		gridBagConstraints.gridx = 1;
-		gridBagConstraints.gridy = 2;
-		gridBagConstraints.weightx = 1.0;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-		pnlColFluff.add(lblGender2, gridBagConstraints);
-		
-		pnlColStats.setName("pnlColStats");
-		pnlColStats.setBackground(Color.RED);
-		pnlColStats.setLayout(new java.awt.GridBagLayout());
-		gridBagConstraints = new java.awt.GridBagConstraints();
-		gridBagConstraints.gridx = 1;
-		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridy = 3;
 		gridBagConstraints.weightx = 0.5;
-		gridBagConstraints.weighty = 1.0;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;	
-		pnlStats.add(pnlColStats, gridBagConstraints);
-		
-		
-		
-		lblGun1.setName("lblGun1"); // NOI18N
-		lblGun1.setText("Gunnery:");
-		gridBagConstraints = new java.awt.GridBagConstraints();
-		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 0;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+		gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 0);
+		gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
 		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-		pnlColStats.add(lblGun1, gridBagConstraints);
+		pnlStats.add(lblGender2, gridBagConstraints);
 		
-		lblGun2.setName("lblGun2"); // NOI18N
 		if(person instanceof PilotPerson) {
-			lblGun2.setText(Integer.toString(((PilotPerson)person).getPilot().getGunnery()));
+			PilotPerson pp = (PilotPerson)person;
+			lblGun1.setName("lblGun1"); // NOI18N
+			lblGun1.setText(resourceMap.getString("lblGun1.text"));
+			gridBagConstraints = new java.awt.GridBagConstraints();
+			gridBagConstraints.gridx = 2;
+			gridBagConstraints.gridy = 1;
+			gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
+			gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+			pnlStats.add(lblGun1, gridBagConstraints);
+			
+			lblGun2.setName("lblGun2"); // NOI18N
+			lblGun2.setText(Integer.toString(pp.getPilot().getGunnery()));
+			gridBagConstraints = new java.awt.GridBagConstraints();
+			gridBagConstraints.gridx = 3;
+			gridBagConstraints.gridy = 1;
+			gridBagConstraints.weightx = 0.5;
+			gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 0);
+			gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
+			gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+			pnlStats.add(lblGun2, gridBagConstraints);
+			
+			lblPilot1.setName("lblPilot1"); // NOI18N
+			lblPilot1.setText(resourceMap.getString("lblPilot1.text"));
+			gridBagConstraints = new java.awt.GridBagConstraints();
+			gridBagConstraints.gridx = 2;
+			gridBagConstraints.gridy = 2;
+			gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
+			gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+			pnlStats.add(lblPilot1, gridBagConstraints);
+			
+			lblPilot2.setName("lblPilot2"); // NOI18N
+			lblPilot2.setText(Integer.toString(pp.getPilot().getPiloting()));
+			gridBagConstraints = new java.awt.GridBagConstraints();
+			gridBagConstraints.gridx = 3;
+			gridBagConstraints.gridy = 2;
+			gridBagConstraints.weightx = 0.5;
+			gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 0);
+			gridBagConstraints.fill = java.awt.GridBagConstraints.NONE;
+			gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+			pnlStats.add(lblPilot2, gridBagConstraints);
+			
 		}
-		gridBagConstraints = new java.awt.GridBagConstraints();
-		gridBagConstraints.gridx = 1;
-		gridBagConstraints.gridy = 0;
-		gridBagConstraints.weightx = 1.0;
-		gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-		gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-		pnlColStats.add(lblGun2, gridBagConstraints);
 		
     	
     }
