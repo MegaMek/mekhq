3ea4a947b950889e45804efab238475df8eca4fd
bombaijin <bombaijin@4d104363-cc00-4a03-b0b8-302e3804504c>
2014-04-28 21:15:18 +0000

    + new MM.jar + better instructions for how to fix unit names in loading error dialog

--- MekHQ/src/mekhq/gui/dialog/DataLoadingDialog.java
+++ MekHQ/src/mekhq/gui/dialog/DataLoadingDialog.java
@@ -165,7 +165,7 @@ public class DataLoadingDialog extends JDialog implements PropertyChangeListener
         			fis.close();
         		} catch (NullEntityException ex) {
         			JOptionPane.showMessageDialog(null, 
-        					"The following units could not be loaded by the campaign:\n" + ex.getError() + "\n\nPlease be sure to copy over any custom units before starting a new version of MekHQ.\nIf you believe the units listed are not customs, then try deleting the file data/mechfiles/units.cache and restarting MekHQ." ,
+        					"The following units could not be loaded by the campaign:\n" + ex.getError() + "\n\nPlease be sure to copy over any custom units before starting a new version of MekHQ.\nIf you believe the units listed are not customs, then try deleting the file data/mechfiles/units.cache and restarting MekHQ.\nIt is also possible that unit chassi and model names have changed across versions of MegaMek. You can check this by\nopening up MegaMek and searching for the units. Chassis and models can be edited in your MekHQ save file with a text editor." ,
         					"Unit Loading Error",
         					JOptionPane.ERROR_MESSAGE);
         			cancelled = true;
