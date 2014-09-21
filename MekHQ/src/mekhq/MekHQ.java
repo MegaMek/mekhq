2e5b31283a4654826c8df17573488ec1489d2aa0
Dylan Myers <Dylan-M@users.noreply.github.com>
2014-09-21 23:27:41 +0000

    + Bug: Automatic resolution not working in r1978

--- MekHQ/src/mekhq/MekHQ.java
+++ MekHQ/src/mekhq/MekHQ.java
@@ -1,20 +1,20 @@
 /*
  * MekBayApp.java
- * 
+ *
  * Copyright (c) 2009 Jay Lawson <jaylawson39 at yahoo.com>. All rights reserved.
- * 
+ *
  * This file is part of MekHQ.
- * 
+ *
  * MekHQ is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * MekHQ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -81,35 +81,35 @@ public class MekHQ implements GameListener {
 	public static int VERBOSITY_LEVEL = 5;
 	public static String CAMPAIGN_DIRECTORY = "./campaigns/";
 	public static String PROPERTIES_FILE = "mmconf/mekhq.properties";
-	
+
 	//stuff related to MM games
     private Server myServer = null;
     private GameThread gameThread = null;
     private Scenario currentScenario = null;
     private Client client = null;
-    
-    //the actual campaign - this is where the good stuff is  
+
+    //the actual campaign - this is where the good stuff is
     private Campaign campaign;
     private CampaignGUI campaigngui;
-    
+
     private IconPackage iconPackage = new IconPackage();
-    
+
 	private Properties preferences;
-	
+
 	/**
 	 * Designed to centralize output and logging.
 	 * Purely a pass-through to the version with a log level.
 	 * Default to log level 3.
-	 * 
+	 *
 	 * @param msg The message you want to log.
 	 */
 	public static void logMessage(String msg) {
 		logMessage(msg, 3);
 	}
-	
+
 	/**
 	 * Designed to centralize output and logging.
-	 * 
+	 *
 	 * @param msg The message you want to log.
 	 * @param logLevel The log level of the message.
 	 */
@@ -121,16 +121,16 @@ public class MekHQ implements GameListener {
 	public static void logError(String err) {
 		System.err.println(err);
 	}
-	
+
 	public static void logError(Exception ex) {
 		System.err.println(ex);
 		ex.printStackTrace();
 	}
-    
+
 	protected static MekHQ getInstance() {
 		return new MekHQ();
 	}
-	
+
     /**
      * At startup create and show the main frame of the application.
      */
@@ -144,7 +144,7 @@ public class MekHQ implements GameListener {
         StartUpGUI sud = new StartUpGUI(this);
         sud.setVisible(true);
     }
-    
+
     public void exit() {
     	if(JOptionPane.showConfirmDialog(null,
                 "Do you really want to quit MekHQ?",
@@ -157,9 +157,9 @@ public class MekHQ implements GameListener {
         	}
     		savePreferences();
         	System.exit(0);
-    	} 	
+    	}
     }
-    
+
     public void showNewView() {
     	campaigngui = new CampaignGUI(this);
     }
@@ -169,18 +169,18 @@ public class MekHQ implements GameListener {
      */
     public static void main(String[] args) {
     	System.setProperty("apple.laf.useScreenMenuBar", "true");
-        System.setProperty("com.apple.mrj.application.apple.menu.about.name","MekHQ");  
+        System.setProperty("com.apple.mrj.application.apple.menu.about.name","MekHQ");
         //redirect output to log file
         redirectOutput();
         MekHQ.getInstance().startup();
     }
-    
+
     protected static Properties setDefaultPreferences() {
     	Properties defaults = new Properties();
     	defaults.setProperty("laf", UIManager.getSystemLookAndFeelClassName());
     	return defaults;
     }
-    
+
     protected void readPreferences() {
     	preferences = new Properties(setDefaultPreferences());
         try {
@@ -192,7 +192,7 @@ public class MekHQ implements GameListener {
             e.printStackTrace();
         }
     }
-    
+
     protected void savePreferences() {
     	preferences.setProperty("laf", UIManager.getLookAndFeel().getClass().getName());
     	try {
@@ -204,7 +204,7 @@ public class MekHQ implements GameListener {
 			e.printStackTrace();
 		}
     }
-    
+
     protected void setLookAndFeel() {
     	//TODO: we can extend this with other look and feel options
     	try {
@@ -217,9 +217,9 @@ public class MekHQ implements GameListener {
     		e.printStackTrace();
     	} catch (UnsupportedLookAndFeelException e) {
         	e.printStackTrace();
-        }		
+        }
     }
-    
+
     /**
      * This function redirects the standard error and output streams to the
      * given File name.
@@ -240,19 +240,19 @@ public class MekHQ implements GameListener {
             e.printStackTrace();
         }
     }
-    
+
     public Server getMyServer() {
     	return myServer;
     }
-    
+
     public Campaign getCampaign() {
     	return campaign;
     }
-    
+
     public void setCampaign(Campaign c) {
     	campaign = c;
     }
-    
+
     public void joinGame(Scenario scenario, ArrayList<Unit> meks) {
 		LaunchGameDialog lgd = new LaunchGameDialog(campaigngui.getFrame(), false, campaign);
 		lgd.setVisible(true);
@@ -267,12 +267,12 @@ public class MekHQ implements GameListener {
 
     	client.getGame().addGameListener(this);
         currentScenario = scenario;
-        
+
         //Start the game thread
         gameThread = new GameThread(lgd.playerName, client, this, meks, false);
         gameThread.start();
     }
-    
+
     public void startHost(Scenario scenario, boolean loadSavegame, ArrayList<Unit> meks) {
     	LaunchGameDialog lgd = new LaunchGameDialog(campaigngui.getFrame(), true, campaign);
 		lgd.setVisible(true);
@@ -290,12 +290,12 @@ public class MekHQ implements GameListener {
 			MekHQ.logError(ex);
             return;
         }
-        
+
         client = new Client(lgd.playerName, "127.0.0.1", lgd.port);
 
         client.getGame().addGameListener(this);
         currentScenario = scenario;
-        
+
         //Start the game thread
         if (campaign.getCampaignOptions().getUseAtB() && scenario instanceof AtBScenario) {
         	gameThread = new AtBGameThread(lgd.playerName, client, this, meks, (AtBScenario)scenario);
@@ -318,62 +318,62 @@ public class MekHQ implements GameListener {
 	@Override
 	public void gameBoardChanged(GameBoardChangeEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameBoardNew(GameBoardNewEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameEnd(GameEndEvent e) {
-			
+
 	}
 
 	@Override
 	public void gameEntityChange(GameEntityChangeEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameEntityNew(GameEntityNewEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameEntityNewOffboard(GameEntityNewOffboardEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameEntityRemove(GameEntityRemoveEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameMapQuery(GameMapQueryEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameNewAction(GameNewActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gamePhaseChange(GamePhaseChangeEvent e) {
 	}
-	
+
 	@Override
-	public void gameVictory(GameVictoryEvent e) {
+	public void gameVictory(GameVictoryEvent gve) {
 		if (gameThread.stopRequested()) {
 			return;
 		}
@@ -386,6 +386,7 @@ public class MekHQ implements GameListener {
                     JOptionPane.YES_OPTION;
         	ResolveScenarioTracker tracker = new ResolveScenarioTracker(currentScenario, campaign, control);
             tracker.setClient(gameThread.getClient());
+            tracker.setEvent(gve);
         	tracker.processGame();
         	ResolveScenarioWizardDialog resolveDialog = new ResolveScenarioWizardDialog(campaigngui.getFrame(), true, tracker);
         	resolveDialog.setVisible(true);
@@ -426,50 +427,50 @@ public class MekHQ implements GameListener {
 	@Override
 	public void gamePlayerChange(GamePlayerChangeEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gamePlayerChat(GamePlayerChatEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gamePlayerConnected(GamePlayerConnectedEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gamePlayerDisconnected(GamePlayerDisconnectedEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameReport(GameReportEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameSettingsChange(GameSettingsChangeEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void gameTurnChange(GameTurnChangeEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void gameClientFeedbackRquest(GameCFREvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public IconPackage getIconPackage() {
 	    return iconPackage;
 	}
