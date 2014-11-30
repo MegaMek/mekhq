d5988728ab074e4ad61d76dfea97ad1ecadf53c4
ralgith-erian <ralgith-erian@4d104363-cc00-4a03-b0b8-302e3804504c>
2014-11-30 17:40:04 +0000

    + Bug [#608]: 0.3.6 : Clicking the "x" button for bulk buys does not cancel the purchase

--- trunk/MekHQ/src/mekhq/gui/dialog/PopupValueChoiceDialog.java
+++ trunk/MekHQ/src/mekhq/gui/dialog/PopupValueChoiceDialog.java
@@ -8,30 +8,52 @@ package mekhq.gui.dialog;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.util.ResourceBundle;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.WindowConstants;
 
 /**
  *
  * @author natit
  */
-public class PopupValueChoiceDialog extends javax.swing.JDialog {
+public class PopupValueChoiceDialog extends JDialog implements WindowListener {
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnDone;
+    private javax.swing.JButton btnCancel;
+    private javax.swing.JPanel pnlButton;
+    private javax.swing.JSpinner value;
+    private javax.swing.SpinnerNumberModel model;
+    // End of variables declaration//GEN-END:variables
 
     /**
-	 * This was originally set up as a text entry dialog, but there is 
+	 * This was originally set up as a text entry dialog, but there is
 	 * really no reason to use it instead of the pre-fab inputdialog that
 	 * comes with java and it was actually causing problems because it uses
-	 * a textpane instead of a textfield. Since it is currently only called by 
-	 * the set xp command in MekHQView, I am going to refactor it into a 
+	 * a textpane instead of a textfield. Since it is currently only called by
+	 * the set xp command in MekHQView, I am going to refactor it into a
 	 * numeric value setter using a spinner.
 	 */
 	private static final long serialVersionUID = 8376874926997734492L;
 	/** Creates new form */
     public PopupValueChoiceDialog(java.awt.Frame parent, boolean modal, String title, int current, int min, int max) {
         super(parent, modal);
-        model = new javax.swing.SpinnerNumberModel(current, min, max, 1);
+        model = new SpinnerNumberModel(current, min, max, 1);
         setTitle(title);
-        initComponents();      
+        initComponents();
         setLocationRelativeTo(parent);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(this);
     }
 
     /** This method is called from within the constructor to
@@ -41,27 +63,27 @@ public class PopupValueChoiceDialog extends javax.swing.JDialog {
      */
     private void initComponents() {
 
-    	pnlButton = new javax.swing.JPanel();
-        btnDone = new javax.swing.JButton();
-        btnCancel = new javax.swing.JButton();
-        value = new javax.swing.JSpinner(model);
+    	pnlButton = new JPanel();
+        btnDone = new JButton();
+        btnCancel = new JButton();
+        value = new JSpinner(model);
 
 		ResourceBundle resourceMap = ResourceBundle.getBundle("mekhq.resources.PopupValueChoiceDialog");
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setName("Form"); // NOI18N
 
         btnDone.setText(resourceMap.getString("btnDone.text")); // NOI18N
         btnDone.setName("btnDone"); // NOI18N
-        btnDone.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        btnDone.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 btnDoneActionPerformed(evt);
             }
         });
-        
+
         btnCancel.setText(resourceMap.getString("btnCancel.text")); // NOI18N
         btnCancel.setName("btnCancel"); // NOI18N
-        btnCancel.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        btnCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 btnCancelActionPerformed(evt);
             }
         });
@@ -69,32 +91,32 @@ public class PopupValueChoiceDialog extends javax.swing.JDialog {
         pnlButton.setLayout(new GridLayout(0,2));
         pnlButton.add(btnDone);
         pnlButton.add(btnCancel);
-        
+
         value.setName("value"); // NOI18N
 
         getContentPane().setLayout(new BorderLayout());
-        
+
         getContentPane().add(value,BorderLayout.CENTER);
         getContentPane().add(pnlButton, BorderLayout.PAGE_END);
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnDoneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDoneActionPerformed
+    private void btnDoneActionPerformed(ActionEvent evt) {//GEN-FIRST:event_btnDoneActionPerformed
         this.setVisible(false);
     }//GEN-LAST:event_btnDoneActionPerformed
 
-    private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDoneActionPerformed
+    private void btnCancelActionPerformed(ActionEvent evt) {//GEN-FIRST:event_btnDoneActionPerformed
         value.getModel().setValue(-1);
     	this.setVisible(false);
     }
-    
+
     /**
     * @param args the command line arguments
     */
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                PopupValueChoiceDialog dialog = new PopupValueChoiceDialog(new javax.swing.JFrame(), true, "Label", 0, 0, 1);
+                PopupValueChoiceDialog dialog = new PopupValueChoiceDialog(new JFrame(), true, "Label", 0, 0, 1);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     public void windowClosing(java.awt.event.WindowEvent e) {
                         System.exit(0);
@@ -104,18 +126,42 @@ public class PopupValueChoiceDialog extends javax.swing.JDialog {
             }
         });
     }
-    
+
     public int getValue() {
     	return (Integer)value.getValue();
     }
 
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnDone;
-    private javax.swing.JButton btnCancel;
-    private javax.swing.JPanel pnlButton;
-    private javax.swing.JSpinner value;
-    private javax.swing.SpinnerNumberModel model;
-    // End of variables declaration//GEN-END:variables
+    @Override
+    public void windowActivated(WindowEvent arg0) {
+    }
+
+    @Override
+    public void windowClosed(WindowEvent arg0) {
+    }
+
+    @Override
+    public void windowClosing(WindowEvent arg0) {
+        if (arg0.getComponent() != this.btnDone) {
+            value.getModel().setValue(-1);
+            this.setVisible(false);
+        }
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent arg0) {
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent arg0) {
+    }
+
+    @Override
+    public void windowIconified(WindowEvent arg0) {
+    }
+
+    @Override
+    public void windowOpened(WindowEvent arg0) {
+    }
 
 }
