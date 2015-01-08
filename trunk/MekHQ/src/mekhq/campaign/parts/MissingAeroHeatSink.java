4bf7c69ff2e94103ba6c2a42b9857e3299c5d4c3
ralgith-erian <ralgith-erian@4d104363-cc00-4a03-b0b8-302e3804504c>
2015-01-08 03:14:25 +0000

    + Bug: MissingBattleArmorSuit NPE due to unit variable not being set

--- trunk/MekHQ/src/mekhq/campaign/parts/MissingAeroHeatSink.java
+++ trunk/MekHQ/src/mekhq/campaign/parts/MissingAeroHeatSink.java
@@ -1,20 +1,20 @@
 /*
  * MissingAeroHeatSink.java
- * 
+ *
  * Copyright (c) 2009 Jay Lawson <jaylawson39 at yahoo.com>. All rights reserved.
- * 
+ *
  * This file is part of MekHQ.
- * 
+ *
  * MekHQ is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * MekHQ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -35,24 +35,24 @@ import org.w3c.dom.Node;
 public class MissingAeroHeatSink extends MissingPart {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 2806921577150714477L;
 
 	private int type;
-	
+
 	public MissingAeroHeatSink() {
     	this(0, Aero.HEAT_SINGLE, null);
     }
-    
+
     public MissingAeroHeatSink(int tonnage, int type, Campaign c) {
-    	super(0, c);
+    	super(tonnage, c);
     	this.time = 90;
     	this.difficulty = -2;
     	this.type = type;
     	this.name = "Aero Heat Sink";
     }
-    
+
 	@Override
 	public String checkFixable() {
 		return null;
@@ -105,7 +105,7 @@ public class MissingAeroHeatSink extends MissingPart {
 			}
 		}
 	}
-	
+
 	@Override
 	protected void loadFieldsFromXmlNode(Node wn) {
 		//nothing to load
