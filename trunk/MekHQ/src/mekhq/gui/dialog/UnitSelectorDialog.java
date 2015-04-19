600310aa901dd882fb89a7285d2231234831b59c
hammer-gs <hammer-gs@4d104363-cc00-4a03-b0b8-302e3804504c>
2015-04-19 21:22:51 +0000

    + Changes to Infantry Costs doesn't use Alternate Cost anymore. + Updated MegaMek and MegaMekLab Jar files.

--- trunk/MekHQ/src/mekhq/gui/dialog/UnitSelectorDialog.java
+++ trunk/MekHQ/src/mekhq/gui/dialog/UnitSelectorDialog.java
@@ -730,10 +730,10 @@ public class UnitSelectorDialog extends JDialog {
 
 	        private long getPurchasePrice(MechSummary ms) {
 	        	long cost = ms.getCost();
-	        	if(ms.getUnitType().equals(UnitType.getTypeName(UnitType.INFANTRY))
-	        	        || ms.getUnitType().equals(UnitType.getTypeName(UnitType.BATTLE_ARMOR))) {
-	        		cost = ms.getAlternateCost();
-	        	}
+//	        	if(ms.getUnitType().equals(UnitType.getTypeName(UnitType.INFANTRY))
+//	        	        || ms.getUnitType().equals(UnitType.getTypeName(UnitType.BATTLE_ARMOR))) {
+//	        		cost = ms.getAlternateCost();
+//	        	}
 	        	if(TechConstants.isClan(ms.getType())) {
 	        		cost *= campaign.getCampaignOptions().getClanPriceModifier();
 	        	}
