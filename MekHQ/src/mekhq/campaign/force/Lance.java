506570a2d131d2a329767dfdba8c65f4056c7bd8
Dylan Myers <Dylan-M@users.noreply.github.com>
2015-05-31 16:14:41 +0000

    + Bug [#880]: NPE: Loading a saved campaign - Lance.java

--- MekHQ/src/mekhq/campaign/force/Lance.java
+++ MekHQ/src/mekhq/campaign/force/Lance.java
@@ -55,7 +55,7 @@ import org.w3c.dom.NodeList;
  * on the TO&E that has at least one unit assigned. Extra info includes whether
  * the force counts as a lance (or star or level II) eligible for assignment
  * to a mission role and what the assignment is on which contract.
- * 
+ *
  * @author Neoancient
  *
  */
@@ -63,25 +63,25 @@ import org.w3c.dom.NodeList;
 public class Lance implements Serializable, MekHqXmlSerializable {
 
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -1197697940987478509L;
-	
+
 	public static final int ROLE_UNASSIGNED = 0;
 	public static final int ROLE_FIGHT = 1;
 	public static final int ROLE_DEFEND = 2;
 	public static final int ROLE_SCOUT = 3;
 	public static final int ROLE_TRAINING = 4;
 	public static final int ROLE_NUM = 5;
-	
+
 	public static final String[] roleNames = {
 		"Unassigned", "Fight", "Defend", "Scout", "Training"
 	};
-	
+
 	public static final int STR_IS = 4;
 	public static final int STR_CLAN = 5;
 	public static final int STR_CS = 6;
-	
+
 	public static final long ETYPE_GROUND = Entity.ETYPE_MECH |
 			Entity.ETYPE_TANK | Entity.ETYPE_INFANTRY | Entity.ETYPE_PROTOMECH;
 
@@ -89,7 +89,7 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 	private int missionId;
 	private int role;
 	private UUID commanderId;
-	
+
 	public static int getStdLanceSize(Faction f) {
     	if (f.isClan()) {
     		return STR_CLAN;
@@ -99,7 +99,7 @@ public class Lance implements Serializable, MekHqXmlSerializable {
     		return STR_IS;
     	}
 	}
-	
+
 	public Lance() {}
 
 	public Lance(int fid, Campaign c) {
@@ -119,20 +119,20 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 			}
 		}
 		commanderId = findCommander(forceId, c);
-	} 
-	
+	}
+
 	public int getForceId() {
 		return forceId;
 	}
-	
+
 	public int getMissionId() {
 		return missionId;
 	}
-	
+
 	public AtBContract getContract(Campaign c) {
 		return (AtBContract)c.getMission(missionId);
 	}
-	
+
 	public void setContract(AtBContract c) {
 		if (null == c) {
 			missionId = -1;
@@ -140,42 +140,42 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 			missionId = c.getId();
 		}
 	}
-	
+
 	public int getRole() {
 		return role;
 	}
-	
+
 	public void setRole(int role) {
 		this.role = role;
 	}
-	
+
 	public UUID getCommanderId() {
 		return commanderId;
 	}
-	
+
 	public Person getCommander(Campaign c) {
 		return c.getPerson(commanderId);
 	}
-	
+
 	public void setCommander(UUID id) {
 		commanderId = id;
 	}
-	
+
 	public void setCommander(Person p) {
 		commanderId = p.getId();
 	}
-	
+
 	public void refreshCommander(Campaign c) {
 		commanderId = findCommander(forceId, c);
 	}
-	
+
 	public int getSize(Campaign c) {
 		if (c.getFaction().isClan()) {
 			return (int)Math.ceil(getEffectivePoints(c));
 		}
 		return c.getForce(forceId).getUnits().size();
 	}
-	
+
 	public double getEffectivePoints(Campaign c) {
 		/* Used to check against force size limits; for this purpose we
 		 * consider a 'Mech and a Point of BA to be a single Point so that
@@ -187,21 +187,27 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 		double infantry = 0.0;
 		double other = 0.0;
 		for (UUID id : c.getForce(forceId).getUnits()) {
-			if ((c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_MECH) != 0) {
-				armor += 1;
-			} else if ((c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_AERO) != 0) {
-				other += 0.5;
-			} else if ((c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_TANK) != 0) {
-				armor += 0.5;
-			} else if ((c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_PROTOMECH) != 0) {
-				other += 0.2;
-			} else if ((c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_INFANTRY) != 0) {
-				infantry += ((Infantry)c.getUnit(id).getEntity()).isSquad()?0.2:1;
-			}
+            Unit unit = c.getUnit(id);
+            if (null != unit) {
+                Entity entity = unit.getEntity();
+                if (null != entity) {
+        			if ((entity.getEntityType() & Entity.ETYPE_MECH) != 0) {
+        				armor += 1;
+        			} else if ((entity.getEntityType() & Entity.ETYPE_AERO) != 0) {
+        				other += 0.5;
+        			} else if ((entity.getEntityType() & Entity.ETYPE_TANK) != 0) {
+        				armor += 0.5;
+        			} else if ((entity.getEntityType() & Entity.ETYPE_PROTOMECH) != 0) {
+        				other += 0.2;
+        			} else if ((entity.getEntityType() & Entity.ETYPE_INFANTRY) != 0) {
+        				infantry += ((Infantry)entity).isSquad()?0.2:1;
+        			}
+                }
+            }
 		}
 		return Math.max(armor, infantry) + other;
 	}
-	
+
 	public int getWeightClass(Campaign c) {
 		/* Clan units only count half the weight of ASF and vehicles
 		 * (2/Point). IS units only count half the weight of vehicles
@@ -211,23 +217,29 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 		 */
 		double weight = 0.0;
 		for (UUID id : c.getForce(forceId).getUnits()) {
-			if ((c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_MECH) != 0 ||
-					(c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_PROTOMECH) != 0 ||
-					(c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_INFANTRY) != 0) {
-				weight += c.getUnit(id).getEntity().getWeight();
-			} else if ((c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_TANK) != 0) {
-				if (c.getFaction().isClan() || c.getCampaignOptions().getAdjustPlayerVehicles()) {
-					weight += c.getUnit(id).getEntity().getWeight() * 0.5;				
-				} else {
-					weight += c.getUnit(id).getEntity().getWeight();				
-				}
-			} else if ((c.getUnit(id).getEntity().getEntityType() & Entity.ETYPE_AERO) != 0) {
-				if (c.getFaction().isClan()) {
-					weight += c.getUnit(id).getEntity().getWeight() * 0.5;				
-				} else {
-					weight += c.getUnit(id).getEntity().getWeight();				
-				}
-			}
+		    Unit unit = c.getUnit(id);
+		    if (null != unit) {
+		        Entity entity = unit.getEntity();
+		        if (null != entity) {
+        			if ((entity.getEntityType() & Entity.ETYPE_MECH) != 0 ||
+        					(entity.getEntityType() & Entity.ETYPE_PROTOMECH) != 0 ||
+        					(entity.getEntityType() & Entity.ETYPE_INFANTRY) != 0) {
+        				weight += entity.getWeight();
+        			} else if ((entity.getEntityType() & Entity.ETYPE_TANK) != 0) {
+        				if (c.getFaction().isClan() || c.getCampaignOptions().getAdjustPlayerVehicles()) {
+        					weight += entity.getWeight() * 0.5;
+        				} else {
+        					weight += entity.getWeight();
+        				}
+        			} else if ((entity.getEntityType() & Entity.ETYPE_AERO) != 0) {
+        				if (c.getFaction().isClan()) {
+        					weight += entity.getWeight() * 0.5;
+        				} else {
+        					weight += entity.getWeight();
+        				}
+        			}
+		        }
+		    }
 		}
 		weight = weight * 4.0 / getStdLanceSize(c.getFaction());
 		if (weight < 40) {
@@ -258,23 +270,29 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 				return false;
 			}
 		}
-		
+
 		if (c.getCampaignOptions().getLimitLanceWeight() &&
 				getWeightClass(c) > EntityWeightClass.WEIGHT_ASSAULT) {
 			return false;
 		}
-		
+
 		for (UUID id : c.getForce(forceId).getUnits()) {
-			if ((c.getUnit(id).getEntity().getEntityType() & ETYPE_GROUND) != 0) {
-				return true;
-			}
+		    Unit unit = c.getUnit(id);
+		    if (null != unit) {
+    		    Entity entity = unit.getEntity();
+    		    if (null != entity) {
+        			if ((entity.getEntityType() & ETYPE_GROUND) != 0) {
+        				return true;
+        			}
+    		    }
+		    }
 		}
-		
+
 		return false;
 	}
-	
+
 	/* Code to find unit commander from ForceViewPanel */
-	
+
 	public static UUID findCommander(int forceId, Campaign c) {
 		ArrayList<Person> people = new ArrayList<Person>();
 		for(UUID uid : c.getForce(forceId).getAllUnits()) {
@@ -287,7 +305,7 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 			}
 		}
 		//sort person vector by rank
-		Collections.sort(people, new Comparator<Person>(){		 
+		Collections.sort(people, new Comparator<Person>(){
 			public int compare(final Person p1, final Person p2) {
 				return ((Comparable<Integer>)p2.getRankNumeric()).compareTo(p1.getRankNumeric());
 			}
@@ -303,11 +321,11 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 		calendar.add(Calendar.DATE, Compute.randomInt(7));
 		return calendar.getTime();
 	}
-	
+
 	public AtBScenario checkForBattle(Campaign c) {
 		int noBattle;
 		int roll;
-		int battleTypeMod = (AtBContract.MORALE_NORMAL - getContract(c).getMoraleLevel()) * 5; 
+		int battleTypeMod = (AtBContract.MORALE_NORMAL - getContract(c).getMoraleLevel()) * 5;
 		battleTypeMod += getContract(c).getBattleTypeMod();
 		switch (role) {
 		case ROLE_FIGHT:
@@ -465,9 +483,9 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 				+ commanderId
 				+"</commanderId>");
 		pw1.println(MekHqXmlUtil.indentStr(indent) + "</lance>");
-		
+
 	}
-	
+
 	public static Lance generateInstanceFromXML(Node wn) {
 		Lance retVal = null;
 		NamedNodeMap attrs = wn.getAttributes();
@@ -476,10 +494,10 @@ public class Lance implements Serializable, MekHqXmlSerializable {
 		try {
 			retVal = (Lance) Class.forName(className).newInstance();
 			NodeList nl = wn.getChildNodes();
-			
+
 			for (int x=0; x<nl.getLength(); x++) {
 				Node wn2 = nl.item(x);
-				
+
 				if (wn2.getNodeName().equalsIgnoreCase("forceId")) {
 					retVal.forceId = Integer.parseInt(wn2.getTextContent());
 				} else if (wn2.getNodeName().equalsIgnoreCase("missionId")) {
