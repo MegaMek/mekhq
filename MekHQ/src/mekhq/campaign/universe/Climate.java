047b6450095f7ef0096d34e288580f503a20635e
Akjosch <development@akjosch.de>
2016-04-26 12:34:10 +0200

    Typo (ARTIC -> ARCTIC)

--- MekHQ/src/mekhq/campaign/universe/Climate.java
+++ MekHQ/src/mekhq/campaign/universe/Climate.java
@@ -20,14 +20,14 @@ package mekhq.campaign.universe;
 
 public enum Climate {
     // Temperature ranges from Interstellar Operations Beta
-    ARTIC(150, 267, "Arctic"), BOREAL(268, 277, "Boreal"), TEMPERATE(278, 287, "Temperate"),
+    ARCTIC(150, 267, "Arctic"), BOREAL(268, 277, "Boreal"), TEMPERATE(278, 287, "Temperate"),
     WARM(288, 297, "Warm"), TROPICAL(298, 307, "Tropical"), SUPERTROPICAL(308, 317, "Supertropical"),
     HELL(318, 500, "Hellish");
     
     // For old climate data
     public static Climate parseClimate(String val) {
         switch(val) {
-            case "0": return ARTIC;
+            case "0": return ARCTIC;
             case "1": return BOREAL;
             case "2": case "3": return TEMPERATE;
             case "4": return WARM;
