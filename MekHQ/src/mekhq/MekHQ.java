d6143c5d7aff11f19c5c03dd55d1efcc1de413a3
Dylan Myers <ralgith@gmail.com>
2016-09-29 20:28:58 -0400

    + Added history entries for the force icon branch merge in

--- MekHQ/src/mekhq/MekHQ.java
+++ MekHQ/src/mekhq/MekHQ.java
@@ -30,7 +30,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
@@ -39,8 +38,6 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import megamek.client.Client;
-import megamek.common.Entity;
-import megamek.common.Game;
 import megamek.common.event.EventBus;
 import megamek.common.event.GameBoardChangeEvent;
 import megamek.common.event.GameBoardNewEvent;
