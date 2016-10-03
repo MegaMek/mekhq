e1be948762217af01f32735b4dd27864b5854103
Akjosch <development@akjosch.de>
2016-10-03 13:16:50 +0200

    Rewrite the XP handler to make use of the new events

--- MekHQ/src/mekhq/campaign/handler/XPHandler.java
+++ MekHQ/src/mekhq/campaign/handler/XPHandler.java
@@ -23,9 +23,9 @@ import java.util.Calendar;
 import megamek.common.Compute;
 import megamek.common.event.Subscribe;
 import mekhq.campaign.Campaign;
-import mekhq.campaign.CampaignOptions;
 import mekhq.campaign.ExtraData;
 import mekhq.campaign.event.NewDayEvent;
+import mekhq.campaign.event.OptionsChangedEvent;
 import mekhq.campaign.personnel.Person;
 
 /**
@@ -33,33 +33,37 @@ import mekhq.campaign.personnel.Person;
  */
 public class XPHandler {
     private static final ExtraData.Key<Integer> NEXT_ADMIN_XP_DELAY = new ExtraData.IntKey("next_admin_xp_delay");
+    private int adminXP;
+    private int adminXPPeriod;
+    
+    @Subscribe
+    public void campaignOptionsHandler(OptionsChangedEvent event) {
+        this.adminXP = event.getOptions().getAdminXP();
+        this.adminXPPeriod = event.getOptions().getAdminXPPeriod();
+    }
     
     @Subscribe
     public void processAdminXP(NewDayEvent event) {
         final Campaign campaign = event.getCampaign();
-        final CampaignOptions opts = campaign.getCampaignOptions();
-        final int xp = opts.getAdminXP();
-        final int weeksBetweenGains = opts.getAdminXPPeriod();
-        if((xp <= 0)
-                || (campaign.getCalendar().get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY)) {
+        if((adminXP <= 0) || (campaign.getCalendar().get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY)) {
             return;
         }
         for (Person person : campaign.getPersonnel()) {
             if (person.isAdminPrimary()) {
-                if(weeksBetweenGains > 1) {
+                if(adminXPPeriod > 1) {
                     Integer weeksLeft = person.getExtraData().get(NEXT_ADMIN_XP_DELAY);
                     if(null == weeksLeft) {
                         // Assign a random value between 1 and the max
-                        weeksLeft = Compute.randomInt(weeksBetweenGains) + 1;
+                        weeksLeft = Compute.randomInt(adminXPPeriod) + 1;
                     }
                     -- weeksLeft;
                     if(weeksLeft == 0) {
-                        person.awardXP(xp);
-                        weeksLeft = weeksBetweenGains;
+                        person.awardXP(adminXP);
+                        weeksLeft = adminXPPeriod;
                     }
                     person.getExtraData().set(NEXT_ADMIN_XP_DELAY, weeksLeft);
                 } else {
-                    person.awardXP(xp);
+                    person.awardXP(adminXP);
                 }
             }
         }
