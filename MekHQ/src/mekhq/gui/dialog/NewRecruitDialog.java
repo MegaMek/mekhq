b7471a023b7c31cf8df7e8551a112f71df6eecd3
Carl Spain <neoancient@megamek.org>
2017-04-26 11:00:48 -0500

    Code formatting cleanup.

--- MekHQ/src/mekhq/gui/dialog/NewRecruitDialog.java
+++ MekHQ/src/mekhq/gui/dialog/NewRecruitDialog.java
@@ -25,20 +25,20 @@ import mekhq.gui.view.PersonViewPanel;
 
 /**
  *
- * @author  Jay Lawson <jaylawson39 at yahoo.com>
+ * @author Jay Lawson <jaylawson39 at yahoo.com>
  */
 public class NewRecruitDialog extends javax.swing.JDialog {
 
     /**
-	 * This dialog is used to both hire new pilots and to edit existing ones
-	 * 
-	 */
-	private static final long serialVersionUID = -6265589976779860566L;
-	private Person person;
+     * This dialog is used to both hire new pilots and to edit existing ones
+     * 
+     */
+    private static final long serialVersionUID = -6265589976779860566L;
+    private Person person;
     private boolean newHire;
-    
+
     private CampaignGUI hqView;
-        
+
     private javax.swing.JComboBox<String> choiceRanks;
 
     private JScrollPane scrollView;
@@ -51,35 +51,31 @@ public class NewRecruitDialog extends javax.swing.JDialog {
         initComponents();
         setLocationRelativeTo(hqView.getFrame());
     }
-    
+
     private void refreshView() {
-    	scrollView.setViewportView(new PersonViewPanel(person, hqView.getCampaign(), hqView.getIconPackage()));
-		//This odd code is to make sure that the scrollbar stays at the top
-		//I cant just call it here, because it ends up getting reset somewhere later
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() { 
-				scrollView.getVerticalScrollBar().setValue(0);
-			}
-		});
+        scrollView.setViewportView(new PersonViewPanel(person, hqView.getCampaign(), hqView.getIconPackage()));
+        // This odd code is to make sure that the scrollbar stays at the top
+        // I cant just call it here, because it ends up getting reset somewhere
+        // later
+        javax.swing.SwingUtilities.invokeLater(() -> scrollView.getVerticalScrollBar().setValue(0));
     }
 
     private void initComponents() {
         scrollView = new JScrollPane();
         choiceRanks = new javax.swing.JComboBox<String>();
 
-        ResourceBundle resourceMap = ResourceBundle.getBundle("mekhq.resources.NewRecruitDialog",
-                new EncodeControl()); //$NON-NLS-1$
+        ResourceBundle resourceMap = ResourceBundle.getBundle("mekhq.resources.NewRecruitDialog", new EncodeControl());
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         setTitle(resourceMap.getString("Form.title")); // NOI18N
-        if(newHire) {
+        if (newHire) {
             setTitle(resourceMap.getString("Form.title.new")); // NOI18N
         }
         setName("Form"); // NOI18N
         getContentPane().setLayout(new java.awt.BorderLayout());
 
         JPanel panSidebar = createSidebar(resourceMap);
- 
+
         JPanel panBottomButtons = createButtonPanel(resourceMap);
 
         scrollView.setMinimumSize(new java.awt.Dimension(450, 180));
@@ -87,7 +83,7 @@ public class NewRecruitDialog extends javax.swing.JDialog {
         scrollView.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollView.setViewportView(null);
         refreshView();
-        
+
         getContentPane().add(panSidebar, BorderLayout.LINE_START);
         getContentPane().add(scrollView, BorderLayout.CENTER);
         getContentPane().add(panBottomButtons, BorderLayout.PAGE_END);
@@ -115,117 +111,113 @@ public class NewRecruitDialog extends javax.swing.JDialog {
         button.setName("btnClose"); // NOI18N
         button.addActionListener(e -> setVisible(false));
         panButtons.add(button, gridBagConstraints);
-        
+
         return panButtons;
     }
 
     private JPanel createSidebar(ResourceBundle resourceMap) {
         JPanel panSidebar = new JPanel();
         panSidebar.setName("panButtons"); // NOI18N
-        panSidebar.setLayout(new java.awt.GridLayout(6,1));
-        
+        panSidebar.setLayout(new java.awt.GridLayout(6, 1));
+
         choiceRanks.setName("choiceRanks"); // NOI18N
         refreshRanksCombo();
         choiceRanks.addActionListener(e -> changeRank());
         panSidebar.add(choiceRanks);
-        
+
         JButton button = new JButton(resourceMap.getString("btnRandomName.text")); // NOI18N
         button.setName("btnRandomName"); // NOI18N
         button.addActionListener(e -> randomName());
         panSidebar.add(button);
-  
+
         button = new JButton(resourceMap.getString("btnRandomPortrait.text")); // NOI18N
         button.setName("btnRandomPortrait"); // NOI18N
         button.addActionListener(e -> randomPortrait());
         panSidebar.add(button);
-      
+
         button = new JButton(resourceMap.getString("btnChoosePortrait.text")); // NOI18N
         button.setName("btnChoosePortrait"); // NOI18N
         button.addActionListener(e -> choosePortrait());
         panSidebar.add(button);
-      
+
         button = new JButton(resourceMap.getString("btnEditPerson.text")); // NOI18N
         button.setName("btnEditPerson"); // NOI18N
         button.addActionListener(e -> editPerson());
         button.setEnabled(hqView.getCampaign().isGM());
         panSidebar.add(button);
-       
+
         button = new JButton(resourceMap.getString("btnRegenerate.text")); // NOI18N
         button.setName("btnRegenerate"); // NOI18N
         button.addActionListener(e -> regenerate());
         button.setEnabled(hqView.getCampaign().isGM());
         panSidebar.add(button);
-        
+
         return panSidebar;
     }
 
     private void hire() {
-    	if(hqView.getCampaign().recruitPerson(person)) {
-        	person = hqView.getCampaign().newPerson(person.getPrimaryRole());
-        	refreshRanksCombo();
-        	hqView.getCampaign().changeRank(person, hqView.getCampaign().getRanks()
-        	        .getRankNumericFromNameAndProfession(person.getProfession(),
-        	                (String)choiceRanks.getSelectedItem()), false);
-    	}
+        if (hqView.getCampaign().recruitPerson(person)) {
+            person = hqView.getCampaign().newPerson(person.getPrimaryRole());
+            refreshRanksCombo();
+            hqView.getCampaign().changeRank(person, hqView.getCampaign().getRanks().getRankNumericFromNameAndProfession(
+                    person.getProfession(), (String) choiceRanks.getSelectedItem()), false);
+        }
         refreshView();
     }
 
     private void randomName() {
-    	person.setName(hqView.getCampaign().getRNG().generate(person.getGender() == Person.G_FEMALE));
-    	refreshView();
-	}
-    
-    private void randomPortrait() {
-    	hqView.getCampaign().assignRandomPortraitFor(person);
-    	refreshView();
-    }
-    
-    private void choosePortrait() {
-    	ImageChoiceDialog pcd = new ImageChoiceDialog(hqView.getFrame(), true,
-				person.getPortraitCategory(),
-				person.getPortraitFileName(), hqView.getIconPackage().getPortraits());
-		pcd.setVisible(true);
-		person.setPortraitCategory(pcd.getCategory());
-		person.setPortraitFileName(pcd.getFileName());
-		refreshView();
-    }
-    
-    private void editPerson() {
-    	int gender = person.getGender();
-    	CustomizePersonDialog npd = new CustomizePersonDialog(hqView.getFrame(), true, 
-				person, hqView.getCampaign());
-		npd.setVisible(true);
-		if(gender != person.getGender()) {
-			randomPortrait();
-		}
-		refreshRanksCombo();
-    	refreshView();
-    }
-    
-    private void regenerate() {
-    	person = hqView.getCampaign().newPerson(person.getPrimaryRole());
-    	refreshRanksCombo();
+        person.setName(hqView.getCampaign().getRNG().generate(person.getGender() == Person.G_FEMALE));
         refreshView();
     }
-    
-    private void changeRank() {
-    	hqView.getCampaign().changeRank(person, hqView.getCampaign().getRanks()
-    	        .getRankNumericFromNameAndProfession(person.getProfession(),
-    	                (String)choiceRanks.getSelectedItem()), false);
-    	refreshView();
+
+    private void randomPortrait() {
+        hqView.getCampaign().assignRandomPortraitFor(person);
+        refreshView();
     }
-   
+
+    private void choosePortrait() {
+        ImageChoiceDialog pcd = new ImageChoiceDialog(hqView.getFrame(), true, person.getPortraitCategory(),
+                person.getPortraitFileName(), hqView.getIconPackage().getPortraits());
+        pcd.setVisible(true);
+        person.setPortraitCategory(pcd.getCategory());
+        person.setPortraitFileName(pcd.getFileName());
+        refreshView();
+    }
+
+    private void editPerson() {
+        int gender = person.getGender();
+        CustomizePersonDialog npd = new CustomizePersonDialog(hqView.getFrame(), true, person, hqView.getCampaign());
+        npd.setVisible(true);
+        if (gender != person.getGender()) {
+            randomPortrait();
+        }
+        refreshRanksCombo();
+        refreshView();
+    }
+
+    private void regenerate() {
+        person = hqView.getCampaign().newPerson(person.getPrimaryRole());
+        refreshRanksCombo();
+        refreshView();
+    }
+
+    private void changeRank() {
+        hqView.getCampaign().changeRank(person, hqView.getCampaign().getRanks().getRankNumericFromNameAndProfession(
+                person.getProfession(), (String) choiceRanks.getSelectedItem()), false);
+        refreshView();
+    }
+
     private void refreshRanksCombo() {
-    	DefaultComboBoxModel<String> ranksModel = new DefaultComboBoxModel<String>();
-    	
-    	// Determine correct profession to pass into the loop
-    	int profession = person.getProfession();
-    	while (hqView.getCampaign().getRanks().isEmptyProfession(profession) && profession != Ranks.RPROF_MW) {
-    		profession = hqView.getCampaign().getRanks().getAlternateProfession(profession);
-    	}
-    	for (String rankName : hqView.getCampaign().getAllRankNamesFor(profession)) {
-    	    ranksModel.addElement(rankName);
-    	}
+        DefaultComboBoxModel<String> ranksModel = new DefaultComboBoxModel<String>();
+
+        // Determine correct profession to pass into the loop
+        int profession = person.getProfession();
+        while (hqView.getCampaign().getRanks().isEmptyProfession(profession) && profession != Ranks.RPROF_MW) {
+            profession = hqView.getCampaign().getRanks().getAlternateProfession(profession);
+        }
+        for (String rankName : hqView.getCampaign().getAllRankNamesFor(profession)) {
+            ranksModel.addElement(rankName);
+        }
         choiceRanks.setModel(ranksModel);
         choiceRanks.setSelectedIndex(0);
     }
