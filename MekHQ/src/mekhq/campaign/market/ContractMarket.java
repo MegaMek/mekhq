87c23dba5d605efe0278675b0b41b2c09c3ed4e6
HammerGS <drivingguy@hotmail.com>
2017-07-18 11:46:10 -0600

    Commenting out duplicate function from PR#249
    Dylan added coded that created contract names, which duplicates the
    function of PR#249.  I've commented out the PR#249 code.

--- MekHQ/src/mekhq/campaign/market/ContractMarket.java
+++ MekHQ/src/mekhq/campaign/market/ContractMarket.java
@@ -415,10 +415,11 @@ public class ContractMarket implements Serializable {
         contract.initContractDetails(campaign);
         contract.calculateContract(campaign);
 
-        contract.setName(Faction.getFaction(employer).getShortName() + "-" + String.format("%1$tY%1$tm", contract.getStartDate()) 
+        //Ralgith had a version of this then the PR got added. Commenting this Out.
+/*        contract.setName(Faction.getFaction(employer).getShortName() + "-" + String.format("%1$tY%1$tm", contract.getStartDate()) 
         				 + "-" + AtBContract.missionTypeNames[contract.getMissionType()] 
         				 + "-" + Faction.getFaction(contract.getEnemyCode()).getShortName()
-        				 + "-" + contract.getLength());
+        				 + "-" + contract.getLength());*/
         
 		return contract;
 	}
