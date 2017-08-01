3a196eac2ab19917779f0daca068a9fcf3ad2cc6
NickAragua <a_pokrovski@hotmail.com>
2017-08-01 00:03:53 -0400

    Removed unnecessary import

--- MekHQ/src/mekhq/campaign/mission/AtBScenario.java
+++ MekHQ/src/mekhq/campaign/mission/AtBScenario.java
@@ -29,7 +29,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
