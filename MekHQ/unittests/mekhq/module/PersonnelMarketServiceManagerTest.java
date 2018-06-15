39fd0b66d17be8215104de3e3c297b023d57a956
Carl Spain <neoancient@megamek.org>
2018-06-14 21:11:28 -0500

    Expanded unit test to check for each built-in market

--- MekHQ/unittests/mekhq/module/PersonnelMarketServiceManagerTest.java
+++ MekHQ/unittests/mekhq/module/PersonnelMarketServiceManagerTest.java
@@ -1,16 +1,51 @@
 package mekhq.module;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import mekhq.campaign.market.PersonnelMarket;
+import mekhq.campaign.market.PersonnelMarketDylan;
+import mekhq.campaign.market.PersonnelMarketFMMr;
+import mekhq.campaign.market.PersonnelMarketRandom;
+import mekhq.campaign.market.PersonnelMarketStratOps;
+import mekhq.module.atb.PersonnelMarketAtB;
 
 public class PersonnelMarketServiceManagerTest {
 
     @Test
-    public void findsServices() {
-        PersonnelMarketServiceManager manager = PersonnelMarketServiceManager.getInstance();
-        
-        assertTrue(manager.getAllMethods().size() > 0);
+    public void providesRandomMarket() {
+        assertTrue(PersonnelMarketServiceManager.getInstance()
+                .getMethod(PersonnelMarket.getTypeName(PersonnelMarket.TYPE_RANDOM))
+                instanceof PersonnelMarketRandom);
+    }
+
+    @Test
+    public void providesFMMrMarket() {
+        assertTrue(PersonnelMarketServiceManager.getInstance()
+                .getMethod(PersonnelMarket.getTypeName(PersonnelMarket.TYPE_FMMR))
+                instanceof PersonnelMarketFMMr);
+    }
+
+    @Test
+    public void providesStratOpsMarket() {
+        assertTrue(PersonnelMarketServiceManager.getInstance()
+                .getMethod(PersonnelMarket.getTypeName(PersonnelMarket.TYPE_STRAT_OPS))
+                instanceof PersonnelMarketStratOps);
+    }
+
+    @Test
+    public void providesDylansMarket() {
+        assertTrue(PersonnelMarketServiceManager.getInstance()
+                .getMethod(PersonnelMarket.getTypeName(PersonnelMarket.TYPE_DYLANS))
+                instanceof PersonnelMarketDylan);
+    }
+
+    @Test
+    public void providesAtBMarket() {
+        assertTrue(PersonnelMarketServiceManager.getInstance()
+                .getMethod(PersonnelMarket.getTypeName(PersonnelMarket.TYPE_ATB))
+                instanceof PersonnelMarketAtB);
     }
 
 }
