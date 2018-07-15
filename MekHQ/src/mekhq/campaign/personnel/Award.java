7fcb0d1bef6ceaefc0e50e521e72efde297164cb
Carl Spain <neoancient@megamek.org>
2018-07-15 17:01:12 -0500

    Added missing license text to new source files.

--- MekHQ/src/mekhq/campaign/personnel/Award.java
+++ MekHQ/src/mekhq/campaign/personnel/Award.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2018 MegaMek team
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package mekhq.campaign.personnel;
 
 import java.io.PrintWriter;
@@ -178,11 +197,13 @@ public class Award implements MekHqXmlSerializable, Comparable<Award>, Serializa
     public int compareTo(Award other) {
         int result = Integer.compare(this.xp, other.xp);
 
-        if(result == 0)
+        if(result == 0) {
             result = Integer.compare(this.edge, other.edge);
+        }
 
-        if(result == 0)
+        if(result == 0) {
             result = this.getName().compareTo(other.getName());
+        }
 
         return result;
     }
