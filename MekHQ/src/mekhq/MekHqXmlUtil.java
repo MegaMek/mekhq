63504514b32da0ee86fb28b04d43dd67c35cd32c
Christopher <christopher.watford@gmail.com>
2018-08-11 14:16:51 -0400

    Added supporting documentation

--- MekHQ/src/mekhq/MekHqXmlUtil.java
+++ MekHQ/src/mekhq/MekHqXmlUtil.java
@@ -61,9 +61,24 @@ public class MekHqXmlUtil {
 			// instance of the builder factory, as long as it is
 			// XXE safe.
 			dbf = DocumentBuilderFactory.newInstance();
+
+			//
+			// Adapted from: https://www.owasp.org/index.php/XML_External_Entity_(XXE)_Prevention_Cheat_Sheet#JAXP_DocumentBuilderFactory.2C_SAXParserFactory_and_DOM4J
+			//
+			// "...The JAXP DocumentBuilderFactory setFeature method allows a
+			// developer to control which implementation-specific XML processor
+			// features are enabled or disabled. The features can either be set
+			// on the factory or the underlying XMLReader setFeature method. 
+			// Each XML processor implementation has its own features that 
+			// govern how DTDs and external entities are processed."
+			//
+			// "[disable] these as well, per Timothy Morgan's 2014 paper: 'XML 
+			// Schema, DTD, and Entity Attacks'"
 			dbf.setXIncludeAware(false);
 			dbf.setExpandEntityReferences(false);
 
+			// "This is the PRIMARY defense. If DTDs (doctypes) are disallowed,
+			// almost all XML entity attacks are prevented"
 			String FEATURE = "http://apache.org/xml/features/disallow-doctype-decl";
 			dbf.setFeature(FEATURE, true);
 
@@ -85,9 +100,18 @@ public class MekHqXmlUtil {
 			// hit this method. It is Ok to have more than one
 			// instance of the builder factory, as long as it is
 			// XXE safe.
+
+			//
+			// For further background, see newSafeDocumentBuilder()
+			//
 			dbf = DocumentBuilderFactory.newInstance();
 			dbf.setXIncludeAware(false);
 			dbf.setExpandEntityReferences(false);
+
+			//
+			// "If you can't completely disable DTDs, then at least do the 
+			// following:"
+			//
 
 			// Disable external entities
 			String FEATURE = "http://xml.org/sax/features/external-general-entities";
@@ -120,6 +144,16 @@ public class MekHqXmlUtil {
 			// instance of the parser factory, as long as it is
 			// XXE safe.
 			spf = SAXParserFactory.newInstance();
+
+			//
+			// Adapted from: https://www.owasp.org/index.php/XML_External_Entity_(XXE)_Prevention_Cheat_Sheet#JAXB_Unmarshaller
+			//
+			// "Since a javax.xml.bind.Unmarshaller parses XML and does not
+			// support any flags for disabling XXE, it’s imperative to parse 
+			// the untrusted XML through a configurable secure parser first, 
+			// generate a source object as a result, and pass the source
+			// object to the Unmarshaller."
+			//
 			spf.setFeature("http://xml.org/sax/features/external-general-entities", false);
 			spf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
 			spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
