4499eff0f214feed8b681ea0b7a1048ec65dda3e
Vicente Cartas <vicente.cartas@outlook.com>
2019-02-15 00:16:44 -0800

    Update JCheckBoxPreference.java
    Modifies behavior so everytime there is a change we save the value internally. This way, if the control could be disposed but the preference could still be saved later.

--- MekHQ/src/mekhq/gui/preferences/JCheckBoxPreference.java
+++ MekHQ/src/mekhq/gui/preferences/JCheckBoxPreference.java
@@ -3,13 +3,25 @@ package mekhq.gui.preferences;
 import mekhq.preferences.PreferenceElement;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-public class JCheckBoxPreference extends PreferenceElement {
+public class JCheckBoxPreference extends PreferenceElement implements ChangeListener {
     private JCheckBox checkBox;
+    private boolean value;
+
 
     public JCheckBoxPreference(JCheckBox checkBox){
         assert checkBox.getName() != null && checkBox.getName().trim().length() > 0;
+
         this.checkBox = checkBox;
+        this.checkBox.addChangeListener(this);
+        this.value = this.checkBox.isSelected();
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        this.value = this.checkBox.isSelected();
     }
 
     @Override
@@ -19,12 +31,13 @@ public class JCheckBoxPreference extends PreferenceElement {
 
     @Override
     public String getCurrentValue() {
-        return Boolean.toString(this.checkBox.isSelected());
+        return Boolean.toString(this.value);
     }
 
     @Override
     protected void protectedSetInitialValue(String value) {
         assert value != null && value.trim().length() > 0;
+
         this.checkBox.setSelected(Boolean.parseBoolean(value));
     }
 }
