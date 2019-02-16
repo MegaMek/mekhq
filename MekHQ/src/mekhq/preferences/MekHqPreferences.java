d325008f71ea130be333326ab2b7525acd6616f2
Vicente Cartas <vicente.cartas@outlook.com>
2019-02-16 14:07:05 -0800

    Update MekHqPreferences.java
    Cleaning json reading/writing methods.

--- MekHQ/src/mekhq/preferences/MekHqPreferences.java
+++ MekHQ/src/mekhq/preferences/MekHqPreferences.java
@@ -52,10 +52,10 @@ public class MekHqPreferences {
 
         try {
             try (FileInputStream input = new FileInputStream(filePath)) {
-                MekHQ.getLogger().debug(
+                MekHQ.getLogger().info(
                         MekHqPreferences.class,
                         METHOD_NAME,
-                        "Loading MekHQ nameToPreferencesMap from: " + filePath);
+                        "Loading MekHQ user preferences from: " + filePath);
 
                 JsonFactory factory = new JsonFactory();
                 JsonParser parser = factory.createParser(input);
@@ -64,8 +64,7 @@ public class MekHqPreferences {
                     throw new IOException("Expected an object start ({)" + getParserInformation(parser));
                 }
 
-                if (parser.nextToken() != JsonToken.FIELD_NAME &&
-                        !parser.getCurrentName().equals(PREFERENCES_TOKEN)) {
+                if (parser.nextToken() != JsonToken.FIELD_NAME && !parser.getCurrentName().equals(PREFERENCES_TOKEN)) {
                     throw new IOException("Expected a field called (" + PREFERENCES_TOKEN + ")" + getParserInformation(parser));
                 }
 
@@ -73,101 +72,40 @@ public class MekHqPreferences {
                     throw new IOException("Expected an array start ([)" + getParserInformation(parser));
                 }
 
+                // Parse all PreferencesNode
                 while (parser.nextToken() != JsonToken.END_ARRAY){
-                    String className;
-                    HashMap<String, String> elements = new HashMap<>();
-
-                    if (parser.currentToken() != JsonToken.START_OBJECT) {
-                        throw new IOException("Expected an object start ({)" + getParserInformation(parser));
-                    }
-
-                    if (parser.nextToken() != JsonToken.FIELD_NAME &&
-                            !parser.getCurrentName().equals(CLASS_TOKEN)) {
-                        throw new IOException("Expected a field called (" + CLASS_TOKEN + ")" + getParserInformation(parser));
-                    }
-
-                    className = parser.nextTextValue();
-
-                    if (parser.nextToken() != JsonToken.FIELD_NAME &&
-                            !parser.getCurrentName().equals(ELEMENTS_TOKEN)) {
-                        throw new IOException("Expected a field called (" + ELEMENTS_TOKEN + ")" + getParserInformation(parser));
-                    }
-
-                    if (parser.nextToken() != JsonToken.START_ARRAY) {
-                        throw new IOException("Expected an array start ([)" + getParserInformation(parser));
-                    }
-
-                    while (parser.nextToken() != JsonToken.END_ARRAY) {
-                        try {
-                            String name;
-                            String value;
-
-                            if (parser.currentToken() != JsonToken.START_OBJECT) {
-                                throw new IOException("Expected an object start ({)" + getParserInformation(parser));
-                            }
-
-                            if (parser.nextToken() != JsonToken.FIELD_NAME &&
-                                    !parser.getCurrentName().equals(NAME_TOKEN)) {
-                                throw new IOException("Expected a field called (" + NAME_TOKEN + ")" + getParserInformation(parser));
-                            }
-
-                            name = parser.nextTextValue();
-
-                            if (parser.nextToken() != JsonToken.FIELD_NAME &&
-                                    !parser.getCurrentName().equals(VALUE_TOKEN)) {
-                                throw new IOException("Expected a field called (" + VALUE_TOKEN + ")" + getParserInformation(parser));
-                            }
-
-                            value = parser.nextTextValue();
-
-                            if (parser.nextToken() != JsonToken.END_OBJECT) {
-                                throw new IOException("Expected an object end (})" + getParserInformation(parser));
-                            }
-
-                            elements.put(name, value);
-                        }
-                        catch (Exception e) {
-                            MekHQ.getLogger().warning(
-                                    MekHqPreferences.class,
-                                    METHOD_NAME,
-                                    "Error reading elements for node: " + className + ".",
-                                    e);
-                        }
-                    }
-
                     try {
-                        PreferencesNode node = new PreferencesNode(Class.forName(className));
-                        node.setInitialValues(elements);
-                        this.nameToPreferencesMap.put(node.getNodeName(), node);
+                        readPreferencesNode(parser, this.nameToPreferencesMap);
                     }
-                    catch (ClassNotFoundException e) {
+                    catch (IOException e) {
                         MekHQ.getLogger().error(
                                 MekHqPreferences.class,
                                 METHOD_NAME,
-                                "No class found with name: " + className + ". Ignoring it.",
+                                "Error reading node. " + getParserInformation(parser),
                                 e);
                     }
                 }
 
-                if (parser.currentToken() != JsonToken.END_OBJECT) {
-                    throw new IOException("Expected an object end (})" + getParserInformation(parser));
-                }
-
                 parser.close();
+
+                MekHQ.getLogger().info(
+                        MekHqPreferences.class,
+                        METHOD_NAME,
+                        "Finished loading user preferences");
             }
         }
         catch (FileNotFoundException e) {
             MekHQ.getLogger().error(
                     MekHqPreferences.class,
                     METHOD_NAME,
-                    "No MekHQ nameToPreferencesMap file found: " + filePath + ". Reverting to defaults.",
+                    "No MekHQ user preferences file found: " + filePath,
                     e);
         }
         catch (IOException e) {
             MekHQ.getLogger().error(
                     MekHqPreferences.class,
                     METHOD_NAME,
-                    "Error reading from the nameToPreferencesMap file: " + filePath + ". Reverting to defaults.",
+                    "Error reading from the user preferences file: " + filePath,
                     e);
         }
     }
@@ -191,21 +129,9 @@ public class MekHqPreferences {
                 writer.writeFieldName(PREFERENCES_TOKEN);
                 writer.writeStartArray();
 
+                // Write each PreferencesNode
                 for(Map.Entry<String, PreferencesNode> preferences : this.nameToPreferencesMap.entrySet()) {
-                    writer.writeStartObject();
-                    writer.writeStringField(CLASS_TOKEN, preferences.getKey());
-                    writer.writeFieldName(ELEMENTS_TOKEN);
-                    writer.writeStartArray();
-
-                    for (Map.Entry<String, String> element : preferences.getValue().getFinalValues().entrySet()) {
-                        writer.writeStartObject();
-                        writer.writeStringField(NAME_TOKEN, element.getKey());
-                        writer.writeStringField(VALUE_TOKEN, element.getValue());
-                        writer.writeEndObject();
-                    }
-
-                    writer.writeEndArray();
-                    writer.writeEndObject();
+                    writePreferencesNode(writer, preferences);
                 }
 
                 writer.writeEndArray();
@@ -238,7 +164,112 @@ public class MekHqPreferences {
         return preferences;
     }
 
-    private String getParserInformation(JsonParser parser) throws IOException {
+    private static void readPreferencesNode(JsonParser parser, Map<String, PreferencesNode> nodes) throws IOException {
+        final String METHOD_NAME = "readPreferencesNode";
+
+        String className;
+        HashMap<String, String> elements = new HashMap<>();
+
+        if (parser.currentToken() != JsonToken.START_OBJECT) {
+            return;
+        }
+
+        if (parser.nextToken() != JsonToken.FIELD_NAME && !parser.getCurrentName().equals(CLASS_TOKEN)) {
+            return;
+        }
+
+        className = parser.nextTextValue();
+
+        if (parser.nextToken() != JsonToken.FIELD_NAME && !parser.getCurrentName().equals(ELEMENTS_TOKEN)) {
+            return;
+        }
+
+        if (parser.nextToken() != JsonToken.START_ARRAY) {
+            return;
+        }
+
+        // Parse all PreferenceElement in this node
+        while (parser.nextToken() != JsonToken.END_ARRAY) {
+            try {
+                readPreferenceElement(parser, elements);
+            }
+            catch (IOException e) {
+                MekHQ.getLogger().warning(
+                        MekHqPreferences.class,
+                        METHOD_NAME,
+                        "Error reading elements for node: " + className + ".",
+                        e);
+            }
+        }
+
+        try {
+            PreferencesNode node = new PreferencesNode(Class.forName(className));
+            node.setInitialValues(elements);
+            nodes.put(node.getNodeName(), node);
+        }
+        catch (ClassNotFoundException e) {
+            MekHQ.getLogger().error(
+                    MekHqPreferences.class,
+                    METHOD_NAME,
+                    "No class with name " + className + " found",
+                    e);
+        }
+    }
+
+    private static void readPreferenceElement(JsonParser parser, HashMap<String, String> elements) throws IOException {
+        String name;
+        String value;
+
+        if (parser.currentToken() != JsonToken.START_OBJECT) {
+            return;
+        }
+
+        if (parser.nextToken() != JsonToken.FIELD_NAME && !parser.getCurrentName().equals(NAME_TOKEN)) {
+            return;
+        }
+
+        name = parser.nextTextValue();
+
+        if (parser.nextToken() != JsonToken.FIELD_NAME && !parser.getCurrentName().equals(VALUE_TOKEN)) {
+            return;
+        }
+
+        value = parser.nextTextValue();
+
+        if (parser.nextToken() != JsonToken.END_OBJECT) {
+            return;
+        }
+
+        elements.put(name, value);
+    }
+
+    private static void writePreferencesNode(JsonGenerator writer, Map.Entry<String, PreferencesNode> nodeInfo) throws IOException {
+        writer.writeStartObject();
+        writer.writeStringField(CLASS_TOKEN, nodeInfo.getKey());
+        writer.writeFieldName(ELEMENTS_TOKEN);
+        writer.writeStartArray();
+
+        // Write all PreferenceElement in this node
+        for (Map.Entry<String, String> element : nodeInfo.getValue().getFinalValues().entrySet()) {
+            writePreferenceElement(writer, element);
+        }
+
+        writer.writeEndArray();
+        writer.writeEndObject();
+    }
+
+    private static void writePreferenceElement(JsonGenerator writer, Map.Entry<String, String> element) throws IOException {
+        writer.writeStartObject();
+        writer.writeStringField(NAME_TOKEN, element.getKey());
+        writer.writeStringField(VALUE_TOKEN, element.getValue());
+        writer.writeEndObject();
+    }
+
+    private static String getParserInformation(JsonParser parser) throws IOException {
+
+        if (parser == null) {
+            return "";
+        }
 
         return ". Current token: " + parser.getCurrentName() +
                 ". Line number: " + parser.getCurrentLocation().getLineNr() +
