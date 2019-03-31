99298d0ba55dc1fdb77a43de8408599a5db3aa8c
Magnus Kerensky <magnusmd@hotmail.com>
2019-03-31 13:36:06 -0400

    Add JS collar and missing collar parts

---
+++ MekHQ/src/mekhq/campaign/parts/JumpshipDockingCollar.java
@@ -0,0 +1,234 @@
+/*
+ * DropshipDockingCollar.java
+ * 
+ * Copyright (c) 2009 Jay Lawson <jaylawson39 at yahoo.com>. All rights reserved.
+ * 
+ * This file is part of MekHQ.
+ * 
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mekhq.campaign.parts;
+
+import java.io.PrintWriter;
+
+import mekhq.campaign.finances.Money;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import megamek.common.Compute;
+import megamek.common.Dropship;
+import megamek.common.Entity;
+import megamek.common.SimpleTechLevel;
+import megamek.common.TechAdvancement;
+import mekhq.MekHqXmlUtil;
+import mekhq.campaign.Campaign;
+import mekhq.campaign.personnel.SkillType;
+
+/**
+ *
+ * @author MKerensky  <magnusmd@hotmail.com>
+ */
+public class JumpshipDockingCollar extends Part {
+	
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -7060162354112320241L;
+    
+    static final TechAdvancement TA_BOOM = new TechAdvancement(TECH_BASE_ALL)
+            .setAdvancement(2458, 2470, 2500).setPrototypeFactions(F_TH)
+            .setProductionFactions(F_TH).setTechRating(RATING_C)
+            .setAvailability(RATING_C, RATING_C, RATING_C, RATING_C)
+            .setStaticTechLevel(SimpleTechLevel.STANDARD);
+    static final TechAdvancement TA_NO_BOOM = new TechAdvancement(TECH_BASE_ALL)
+            .setAdvancement(2304, 2350, 2364, 2520).setPrototypeFactions(F_TA)
+            .setProductionFactions(F_TH).setTechRating(RATING_B)
+            .setAvailability(RATING_C, RATING_X, RATING_X, RATING_X)
+            .setStaticTechLevel(SimpleTechLevel.ADVANCED);
+    
+    private int collarType = Dropship.COLLAR_STANDARD;
+	
+	public JumpshipDockingCollar() {
+    	this(0, null, Dropship.COLLAR_STANDARD);
+    }
+    
+    public JumpshipDockingCollar(int tonnage, Campaign c, int collarType) {
+        super(tonnage, c);
+        this.collarType = collarType;
+        this.name = "Dropship Docking Collar";
+        if (collarType == Dropship.COLLAR_NO_BOOM) {
+            name += " (No Boom)";
+        } else if (collarType == Dropship.COLLAR_PROTOTYPE) {
+            name += " (Prototype)";
+        }
+    }
+    
+    public JumpshipDockingCollar clone() {
+    	JumpshipDockingCollar clone = new JumpshipDockingCollar(getUnitTonnage(), campaign, collarType);
+        clone.copyBaseData(this);
+    	return clone;
+    }
+    
+    public int getCollarType() {
+        return collarType;
+    }
+        
+	@Override
+	public void updateConditionFromEntity(boolean checkForDestruction) {
+		int priorHits = hits;
+		if(null != unit && unit.getEntity() instanceof Dropship) {
+			 if(((Dropship)unit.getEntity()).isDockCollarDamaged()) {
+				 hits = 1;
+			 } else { 
+				 hits = 0;
+			 }
+			 if(checkForDestruction 
+					 && hits > priorHits
+					 && Compute.d6(2) < campaign.getCampaignOptions().getDestroyPartTarget()) {
+				 remove(false);
+			 }
+		}
+	}
+	
+	@Override 
+	public int getBaseTime() {
+		if(isSalvaging()) {
+			return 2880;
+		}
+		return 120;
+	}
+	
+	@Override
+	public int getDifficulty() {
+		if(isSalvaging()) {
+			return -2;
+		}
+		return 3;
+	}
+
+	@Override
+	public void updateConditionFromPart() {
+		if(null != unit && unit.getEntity() instanceof Dropship) {
+            ((Dropship) unit.getEntity()).setDamageDockCollar(hits > 0);
+		}
+	}
+
+	@Override
+	public void fix() {
+		super.fix();
+		if(null != unit && unit.getEntity() instanceof Dropship) {
+			((Dropship)unit.getEntity()).setDamageDockCollar(false);
+		}
+	}
+
+	@Override
+	public void remove(boolean salvage) {
+		if(null != unit && unit.getEntity() instanceof Dropship) {
+            ((Dropship)unit.getEntity()).setDamageDockCollar(true);
+			Part spare = campaign.checkForExistingSparePart(this);
+			if(!salvage) {
+				campaign.removePart(this);
+			} else if(null != spare) {
+				spare.incrementQuantity();
+				campaign.removePart(this);
+			}
+			unit.removePart(this);
+			Part missing = getMissingPart();
+			unit.addPart(missing);
+			campaign.addPart(missing, 0);
+		}
+		setUnit(null);
+		updateConditionFromEntity(false);
+	}
+
+	@Override
+	public MissingPart getMissingPart() {
+		return new MissingDropshipDockingCollar(getUnitTonnage(), campaign, collarType);
+	}
+
+	@Override
+	public String checkFixable() {
+		return null;
+	}
+
+	@Override
+	public boolean needsFixing() {
+		return (hits > 0);
+	}
+
+	@Override
+	public Money getStickerPrice() {
+	    if (collarType == Dropship.COLLAR_STANDARD) {
+	        return Money.of(10000);
+	    } else {
+	        return Money.of(1010000) ;
+	    }
+	}
+	
+	@Override
+	public double getTonnage() {
+		return 0;
+	}
+
+	@Override
+	public boolean isSamePartType(Part part) {
+		return (part instanceof JumpshipDockingCollar)
+		        && (collarType == ((JumpshipDockingCollar)part).collarType);
+	}
+	
+	@Override
+	public void writeToXml(PrintWriter pw1, int indent) {
+		writeToXmlBegin(pw1, indent);
+        MekHqXmlUtil.writeSimpleXmlTag(pw1, indent, "collarType", collarType);
+		writeToXmlEnd(pw1, indent);
+	}
+
+	@Override
+	protected void loadFieldsFromXmlNode(Node wn) {
+        NodeList nl = wn.getChildNodes();
+
+        for (int x=0; x<nl.getLength(); x++) {
+            Node wn2 = nl.item(x);
+            if (wn2.getNodeName().equalsIgnoreCase("collarType")) {
+                collarType = Integer.parseInt(wn2.getTextContent());
+            }
+        }
+	}
+	
+	@Override
+	public boolean isRightTechType(String skillType) {
+		return skillType.equals(SkillType.S_TECH_AERO);
+	}
+
+	@Override
+	public String getLocationName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLocation() {
+		return Entity.LOC_NONE;
+	}
+	
+	@Override
+	public TechAdvancement getTechAdvancement() {
+	    if (collarType != Dropship.COLLAR_NO_BOOM) {
+	        return TA_BOOM;
+	    } else {
+	        return TA_NO_BOOM;
+	    }
+	}
+}
---
+++ MekHQ/src/mekhq/campaign/parts/MissingJumpshipDockingCollar.java
@@ -0,0 +1,143 @@
+/*
+ * MissingDropshipDockingCollar.java
+ * 
+ * Copyright (c) 2009 Jay Lawson <jaylawson39 at yahoo.com>. All rights reserved.
+ * 
+ * This file is part of MekHQ.
+ * 
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package mekhq.campaign.parts;
+
+import java.io.PrintWriter;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import megamek.common.Dropship;
+import megamek.common.Entity;
+import megamek.common.TechAdvancement;
+import mekhq.MekHqXmlUtil;
+import mekhq.campaign.Campaign;
+
+/**
+ *
+ * @author MKerensky  <magnusmd@hotmail.com>
+ */
+public class MissingJumpshipDockingCollar extends MissingPart {
+
+	/**
+     * 
+     */
+    private static final long serialVersionUID = -1990781062855611553L;
+    
+    private int collarType;
+	
+	public MissingJumpshipDockingCollar() {
+    	this(0, null, Dropship.COLLAR_STANDARD);
+    }
+    
+    public MissingJumpshipDockingCollar(int tonnage, Campaign c, int collarType) {
+        super(tonnage, c);
+        this.collarType = collarType;
+        this.name = "Dropship Docking Collar";
+        if (collarType == Dropship.COLLAR_NO_BOOM) {
+            name += " (No Boom)";
+        } else if (collarType == Dropship.COLLAR_PROTOTYPE) {
+            name += " (Prototype)";
+        }
+    }
+    
+    @Override 
+	public int getBaseTime() {
+		return 2880;
+	}
+	
+	@Override
+	public int getDifficulty() {
+		return -2;
+	}
+
+	@Override
+	public void updateConditionFromPart() {
+		if(null != unit && unit.getEntity() instanceof Dropship) {
+            ((Dropship)unit.getEntity()).setDamageDockCollar(true);
+            ((Dropship)unit.getEntity()).setDamageKFBoom(true);
+		}	
+		
+	}
+
+	@Override
+	public Part getNewPart() {
+		return new DropshipDockingCollar(getUnitTonnage(), campaign, collarType);
+	}
+
+	@Override
+	public String checkFixable() {
+		return null;
+	}
+
+	
+	@Override
+	public double getTonnage() {
+		return 0;
+	}
+
+	@Override
+	public void writeToXml(PrintWriter pw1, int indent) {
+		writeToXmlBegin(pw1, indent);
+        MekHqXmlUtil.writeSimpleXmlTag(pw1, indent, "collarType", collarType);
+		writeToXmlEnd(pw1, indent);
+	}
+
+	@Override
+	protected void loadFieldsFromXmlNode(Node wn) {
+        NodeList nl = wn.getChildNodes();
+
+        for (int x=0; x<nl.getLength(); x++) {
+            Node wn2 = nl.item(x);
+            if (wn2.getNodeName().equalsIgnoreCase("collarType")) {
+                collarType = Integer.parseInt(wn2.getTextContent());
+            }
+        }
+	}
+
+	@Override
+	public boolean isAcceptableReplacement(Part part, boolean refit) {
+		return (part instanceof DropshipDockingCollar)
+		        && (refit || (((DropshipDockingCollar) part).getCollarType() == collarType));
+	}
+
+	@Override
+	public String getLocationName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getLocation() {
+		return Entity.LOC_NONE;
+	}
+	
+	@Override
+	public TechAdvancement getTechAdvancement() {
+        if (collarType != Dropship.COLLAR_NO_BOOM) {
+            return DropshipDockingCollar.TA_BOOM;
+        } else {
+            return DropshipDockingCollar.TA_NO_BOOM;
+        }
+	}
+	
+}
