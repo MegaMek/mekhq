487df0afe350c7abee8e847c24446a5a106d4bc4
MKerensky <magnusmd@hotmail.com>
2019-06-04 10:40:38 -0400

    Fix changes to CIC part

--- MekHQ/src/mekhq/campaign/parts/CombatInformationCenter.java
+++ MekHQ/src/mekhq/campaign/parts/CombatInformationCenter.java
@@ -41,74 +41,71 @@ import mekhq.campaign.personnel.SkillType;
  * @author MKerensky
  */
 public class CombatInformationCenter extends Part {
-	
-	/**
+
+    /**
      * 
      */
     private static final long serialVersionUID = 7069129053879581753L;
-    
+
     private Money cost;
-	
-	public CombatInformationCenter() {
-    	this(0, Money.zero(), null);
+
+    public CombatInformationCenter() {
+        this(0, Money.zero(), null);
     }
-    
+
     public CombatInformationCenter(int tonnage, Money cost, Campaign c) {
         super(tonnage, c);
         this.cost = cost;
         this.name = "Combat Information Center";
     }
-        
-    public CombatInformationCenter clone() {
-    	CombatInformationCenter clone = new CombatInformationCenter(0, cost, campaign);
-        clone.copyBaseData(this);
-    	return clone;
-    }
     
-	@Override
-	public void updateConditionFromEntity(boolean checkForDestruction) {
-		int priorHits = hits;
-		if(null != unit && unit.getEntity() instanceof Aero) {
-			hits = ((Aero)unit.getEntity()).getCICHits();
-			if(checkForDestruction 
-					&& hits > priorHits 
-					&& (hits < 3 && !campaign.getCampaignOptions().useAeroSystemHits())
+    public CombatInformationCenter clone() {
+        CombatInformationCenter clone = new CombatInformationCenter(0, cost, campaign);
+        clone.copyBaseData(this);
+        return clone;
+    }
+
+    @Override
+    public void updateConditionFromEntity(boolean checkForDestruction) {
+        int priorHits = hits;
+        if(null != unit && unit.getEntity() instanceof Aero) {
+            hits = ((Aero)unit.getEntity()).getCICHits();
+            if(checkForDestruction 
+                    && hits > priorHits 
+                    && (hits < 3 && !campaign.getCampaignOptions().useAeroSystemHits())
                     && Compute.d6(2) < campaign.getCampaignOptions().getDestroyPartTarget()) {
-				remove(false);
-			} else if (hits >= 3) {
+                remove(false);
+            } else if (hits >= 3) {
                 remove(false);
             }
-		}
-	}
-	
-	@Override 
-	public int getBaseTime() {
-	    int time = 0;
+        }
+    }
+
+    @Override 
+    public int getBaseTime() {
+        int time = 0;
         if (campaign.getCampaignOptions().useAeroSystemHits()) {
             //Test of proposed errata for repair times
             time = 120;
             if (unit != null && unit.getEntity().hasNavalC3()) {
                 time *= 2;
             }
-            if (hits == 1) {
-                time *= 1;
-            } 
             if (hits == 2) {
                 time *= 2;
             }
             return time;
         }
-		if(isSalvaging()) {
-			time = 4320;
-		} else {
-		    time = 120;
-		}
-		return time;
-	}
-	
-	@Override
-	public int getDifficulty() {
-	    if (campaign.getCampaignOptions().useAeroSystemHits()) {
+        if(isSalvaging()) {
+            time = 4320;
+        } else {
+            time = 120;
+        }
+        return time;
+    }
+
+    @Override
+    public int getDifficulty() {
+        if (campaign.getCampaignOptions().useAeroSystemHits()) {
             //Test of proposed errata for repair time and difficulty
             if (hits == 1) {
                 return 1;
@@ -117,133 +114,131 @@ public class CombatInformationCenter extends Part {
                 return 2;
             }
         }
-		if(isSalvaging()) {
-			return 0;
-		}
-		return 1;
-	}
+        if(isSalvaging()) {
+            return 0;
+        }
+        return 1;
+    }
 
-	@Override
-	public void updateConditionFromPart() {
-		if(null != unit && unit.getEntity() instanceof Aero) {
-			((Aero)unit.getEntity()).setCICHits(hits);
-		}
-		
-	}
+    @Override
+    public void updateConditionFromPart() {
+        if(null != unit && unit.getEntity() instanceof Aero) {
+            ((Aero)unit.getEntity()).setCICHits(hits);
+        }
+    }
 
-	@Override
-	public void fix() {
-		super.fix();
-		if(null != unit && unit.getEntity() instanceof Aero) {
-			((Aero)unit.getEntity()).setCICHits(0);
-		}
-	}
+    @Override
+    public void fix() {
+        super.fix();
+        if(null != unit && unit.getEntity() instanceof Aero) {
+            ((Aero)unit.getEntity()).setCICHits(0);
+        }
+    }
 
-	@Override
-	public void remove(boolean salvage) {
-		if(null != unit && unit.getEntity() instanceof Aero) {
-			((Aero)unit.getEntity()).setCICHits(3);
-			Part spare = campaign.checkForExistingSparePart(this);
-			if(!salvage) {
-				campaign.removePart(this);
-			} else if(null != spare) {
-				spare.incrementQuantity();
-				campaign.removePart(this);
-			}
-			unit.removePart(this);
-			Part missing = getMissingPart();
-			unit.addPart(missing);
-			campaign.addPart(missing, 0);
-		}
-		setUnit(null);
-		updateConditionFromEntity(false);
-	}
+    @Override
+    public void remove(boolean salvage) {
+        if(null != unit && unit.getEntity() instanceof Aero) {
+            ((Aero)unit.getEntity()).setCICHits(3);
+            Part spare = campaign.checkForExistingSparePart(this);
+            if(!salvage) {
+                campaign.removePart(this);
+            } else if(null != spare) {
+                spare.incrementQuantity();
+                campaign.removePart(this);
+            }
+            unit.removePart(this);
+            Part missing = getMissingPart();
+            unit.addPart(missing);
+            campaign.addPart(missing, 0);
+        }
+        setUnit(null);
+        updateConditionFromEntity(false);
+    }
 
-	@Override
-	public MissingPart getMissingPart() {
-		return new MissingCIC(getUnitTonnage(), cost, campaign);
-	}
+    @Override
+    public MissingPart getMissingPart() {
+        return new MissingCIC(getUnitTonnage(), cost, campaign);
+    }
 
-	@Override
-	public String checkFixable() {
-	    if (isSalvaging()) {
+    @Override
+    public String checkFixable() {
+        if (isSalvaging()) {
             // FCS/CIC computers are designed for and built into the ship. Can't salvage and use somewhere else
             return "You cannot salvage a spacecraft CIC. You must scrap it instead.";
         }
-		return null;
-	}
+        return null;
+    }
 
-	@Override
-	public boolean needsFixing() {
-		return hits > 0;
-	}
+    @Override
+    public boolean needsFixing() {
+        return hits > 0;
+    }
 
-	@Override
-	public Money getStickerPrice() {
-		calculateCost();
-		return cost;
-	}
+    @Override
+    public Money getStickerPrice() {
+        calculateCost();
+        return cost;
+    }
 
-	public void calculateCost() {
-		if(null != unit) {
-		    // There's more to CIC than just Fire Control
-		    // Use Bridge + Computer + FC Computer + Gunnery Control System costs, p158 SO.
-		    cost = Money.of(200000 + (10 * unit.getEntity().getWeight()) + 200000 + 100000 + (10000 * ((Jumpship)unit.getEntity()).getArcswGuns()));
-		}
-	}
-	
-	@Override
-	public double getTonnage() {
-		return 0;
-	}
+    public void calculateCost() {
+        if(null != unit) {
+            // There's more to CIC than just Fire Control
+            // Use Bridge + Computer + FC Computer + Gunnery Control System costs, p158 SO.
+            cost = Money.of(200000 + (10 * unit.getEntity().getWeight()) + 200000 + 100000 + (10000 * ((Jumpship)unit.getEntity()).getArcswGuns()));
+        }
+    }
 
-	@Override
-	public boolean isSamePartType(Part part) {
-		return part instanceof CombatInformationCenter && cost == part.getStickerPrice();
-	}
-	
-	@Override
-	public boolean isRightTechType(String skillType) {
-	    return skillType.equals(SkillType.S_TECH_VESSEL);
-	}
-	
-	@Override
-	public void writeToXml(PrintWriter pw1, int indent) {
-		writeToXmlBegin(pw1, indent);
-		pw1.println(MekHqXmlUtil.indentStr(indent+1)
-				+"<cost>"
-				+cost.toXmlString()
-				+"</cost>");
-		writeToXmlEnd(pw1, indent);
-	}
+    @Override
+    public double getTonnage() {
+        return 0;
+    }
 
-	@Override
-	protected void loadFieldsFromXmlNode(Node wn) {
-		NodeList nl = wn.getChildNodes();
-		
-		for (int x=0; x<nl.getLength(); x++) {
-			Node wn2 = nl.item(x);		
-			if (wn2.getNodeName().equalsIgnoreCase("cost")) {
-				cost = Money.fromXmlString(wn2.getTextContent().trim());
-			} 
-		}
-	}
+    @Override
+    public boolean isSamePartType(Part part) {
+        calculateCost();
+        return part instanceof CombatInformationCenter && getStickerPrice().equals(part.getStickerPrice());
+    }
 
-	@Override
-	public String getLocationName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public boolean isRightTechType(String skillType) {
+        return skillType.equals(SkillType.S_TECH_VESSEL);
+    }
 
-	@Override
-	public int getLocation() {
-		return Entity.LOC_NONE;
-	}
-	
-	@Override
-	public TechAdvancement getTechAdvancement() {
-	    return TA_GENERIC;
-	}
-	
-	
+    @Override
+    public void writeToXml(PrintWriter pw1, int indent) {
+        writeToXmlBegin(pw1, indent);
+        pw1.println(MekHqXmlUtil.indentStr(indent+1)
+                +"<cost>"
+                +cost.toXmlString()
+                +"</cost>");
+        writeToXmlEnd(pw1, indent);
+    }
+
+    @Override
+    protected void loadFieldsFromXmlNode(Node wn) {
+        NodeList nl = wn.getChildNodes();
+        
+        for (int x=0; x<nl.getLength(); x++) {
+            Node wn2 = nl.item(x);        
+            if (wn2.getNodeName().equalsIgnoreCase("cost")) {
+                cost = Money.fromXmlString(wn2.getTextContent().trim());
+            } 
+        }
+    }
+
+    @Override
+    public String getLocationName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getLocation() {
+        return Entity.LOC_NONE;
+    }
+
+    @Override
+    public TechAdvancement getTechAdvancement() {
+        return TA_GENERIC;
+    }
 }
