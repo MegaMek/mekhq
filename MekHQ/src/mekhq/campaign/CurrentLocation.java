473a0395f39c823fb31663589f4870694205b83d
Skoraks <steveg646@gmail.com>
2019-09-29 14:09:12 +0100

    Moved reporting logic and corrected some report typos.

--- MekHQ/src/mekhq/campaign/CurrentLocation.java
+++ MekHQ/src/mekhq/campaign/CurrentLocation.java
@@ -170,7 +170,6 @@ public class CurrentLocation implements Serializable {
         DateTime currentDate = Utilities.getDateTimeDay(campaign.getCalendar());
         double neededRechargeTime = currentSystem.getRechargeTime(currentDate);
         rechargeTime = neededRechargeTime;
-        campaign.addReport("Jumpship drives fully charged");
     }
 
     /**
@@ -188,7 +187,7 @@ public class CurrentLocation implements Serializable {
             campaign.addReport("Jumpships spent " + (Math.round(100.0 * usedRechargeTime)/100.0) + " hours recharging drives");
             rechargeTime += usedRechargeTime;
             if(rechargeTime >= neededRechargeTime) {
-                campaign.addReport("Jumpship drives full charged");
+                campaign.addReport("Jumpship drives fully charged");
             }
         }
         if((null == jumpPath) || jumpPath.isEmpty()) {
@@ -229,7 +228,7 @@ public class CurrentLocation implements Serializable {
                     campaign.addReport("Jumpships spent " + (Math.round(100.0 * usedRechargeTime)/100.0) + " hours recharging drives");
                     rechargeTime += usedRechargeTime;
                     if(rechargeTime >= neededRechargeTime) {
-                        campaign.addReport("Jumpship drives full charged");
+                        campaign.addReport("Jumpship drives fully charged");
                     }
                 }
             }
--- MekHQ/src/mekhq/gui/InterstellarMapPanel.java
+++ MekHQ/src/mekhq/gui/InterstellarMapPanel.java
@@ -7,12 +7,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * MekHQ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -94,7 +94,7 @@ import mekhq.campaign.universe.Systems;
  */
 public class InterstellarMapPanel extends JPanel {
     private static final long serialVersionUID = -1110105822399704646L;
-    
+
     private static final Vector2d[] BASE_HEXCOORDS = {
         new Vector2d(1.0, 0.0),
         new Vector2d(Math.cos(Math.PI / 3.0), Math.sin(Math.PI / 3.0)),
@@ -109,7 +109,7 @@ public class InterstellarMapPanel extends JPanel {
     private JViewport optionView;
     private JPanel optionPanel;
     private JButton optionButton;
-    
+
     // Map view options
     private JRadioButton optFactions;
     private JRadioButton optTech;
@@ -129,6 +129,7 @@ public class InterstellarMapPanel extends JPanel {
     private boolean optionPanelHidden;
     
     private ArrayList<PlanetarySystem> systems;
+
     private JumpPath jumpPath;
     private Campaign campaign;
     private InnerStellarMapConfig conf = new InnerStellarMapConfig();
@@ -151,7 +152,7 @@ public class InterstellarMapPanel extends JPanel {
         optionPanelHidden = true;
         optionPanelTimer = new Timer(50, new ActionListener() {
             Point viewPoint = new Point();
-            
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 int width = optionView.getWidth();
@@ -368,6 +369,7 @@ public class InterstellarMapPanel extends JPanel {
                             @Override
                             public void actionPerformed(ActionEvent ae) {
                                 campaign.getLocation().setRecharged(campaign);
+                                campaign.addReport("GM: Jumpship drives fully charged");
                             }
                         });
                     }
@@ -459,7 +461,7 @@ public class InterstellarMapPanel extends JPanel {
                  zoom(Math.pow(1.5,-1 * e.getWheelRotation()), e.getPoint());
              }
         });
-        
+
         pane = new JLayeredPane();
         mapPanel = new JPanel() {
             private static final long serialVersionUID = -6666762147393179909L;
@@ -472,7 +474,7 @@ public class InterstellarMapPanel extends JPanel {
                 g2.fillRect(0, 0, getWidth(), getHeight());
                 double size = 1 + 5 * Math.log(conf.scale);
                 size = Math.max(Math.min(size, conf.maxdotSize), conf.minDotSize);
-                
+
                 final Stroke thick = new BasicStroke(2.0f);
                 final Stroke thin = new BasicStroke(1.2f);
                 final Stroke dashed = new BasicStroke(1.5f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 0, new float[]{3}, 0);
@@ -484,7 +486,7 @@ public class InterstellarMapPanel extends JPanel {
                 maxX = scr2mapX(getWidth() + size * 2.0);
                 maxY = scr2mapY(- size * 2.0);
                 now = Utilities.getDateTimeDay(campaign.getCalendar());
-                
+
                 Arc2D.Double arc = new Arc2D.Double();
                 //first get the jump diameter for selected planet
                 if(null != selectedSystem && conf.scale > conf.showPlanetNamesThreshold) {
@@ -504,7 +506,7 @@ public class InterstellarMapPanel extends JPanel {
                         g2.draw(arc);
                     }
                 }
-                
+
                 if((conf.scale > 1.0) && optISWAreas.isSelected()) {
                     // IDEA: Allow for different hex sizes later on.
                     final double HEX_SIZE = 30.0;
@@ -529,7 +531,7 @@ public class InterstellarMapPanel extends JPanel {
                                     hexFactions.addAll(system.getFactionSet(now));
                                 }
                             }
-                            
+
                             path.transform(transform);
 
                             if(hexFactions.size() == 1) {
@@ -558,7 +560,7 @@ public class InterstellarMapPanel extends JPanel {
                                     paintColors[i * 2 + 1] = factionColor;
                                 }
                                 paintFractions[0] = 0.0f;
-                                
+
                                 // Determine where to anchor the stripes
                                 Point2D firstPoint = new Point2D.Double(map2scrX(coordX), map2scrY(coordY));
                                 Point2D secondPoint = new Point2D.Double(
@@ -580,7 +582,7 @@ public class InterstellarMapPanel extends JPanel {
                         }
                     }
                 }
-                
+
                 //draw a jump path
                 g2.setStroke(new BasicStroke(1.0f));
                 for(int i = 0; i < jumpPath.size(); i++) {
@@ -665,7 +667,7 @@ public class InterstellarMapPanel extends JPanel {
                     }
                     g2.setStroke(new BasicStroke(1.0f));
                 }
-                
+
                 //check to see if the unit is traveling on a jump path currently and if so
                 //draw this one too, in a different color
                 if(null != campaign.getLocation().getJumpPath()) {
@@ -729,7 +731,7 @@ public class InterstellarMapPanel extends JPanel {
                             arc.setArcByCenter(x, y, size * 1.2, 0, 360, Arc2D.OPEN);
                             g2.fill(arc);
                         }
-                        
+
                         //if factions are selected then we need to do it differently, because
                         //of multiple factions per planet
                         if(isFactionsSelected()) {
@@ -780,15 +782,15 @@ public class InterstellarMapPanel extends JPanel {
             }
         };
         pane.add(mapPanel, Integer.valueOf(1));
-        
+
         optionPanel = new JPanel();
         optionPanel.setLayout(new BoxLayout(optionPanel, BoxLayout.Y_AXIS));
         optionPanel.setBackground(new Color(0, 100, 230, 200));
         optionPanel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
-        
+
         Icon checkboxIcon = new ImageIcon("data/images/misc/checkbox_unselected.png");
         Icon checkboxSelectedIcon = new ImageIcon("data/images/misc/checkbox_selected.png");
-        
+
         optionPanel.add(createLabel("Color:"));
 
         optFactions = createOptionRadioButton("Faction", checkboxIcon, checkboxSelectedIcon);
@@ -820,10 +822,9 @@ public class InterstellarMapPanel extends JPanel {
         colorChoice.add(optPopulation);
         colorChoice.add(optHPG);
         colorChoice.add(optRecharge);
-        
         //factions by default
         optFactions.setSelected(true);
-        
+
         optionPanel.add(Box.createRigidArea(new Dimension(0,10)));
         optionPanel.add(createLabel("Options:"));
         optEmptySystems = createOptionCheckBox("Empty systems", checkboxIcon, checkboxSelectedIcon);
@@ -852,11 +853,11 @@ public class InterstellarMapPanel extends JPanel {
 
         optionView = new JViewport();
         optionView.add(optionPanel);
-        
+
         pane.add(optionView, Integer.valueOf(10));
-        
+
         add(pane);
-        
+
         optionPanelTimer.start();
     }
 
@@ -881,7 +882,7 @@ public class InterstellarMapPanel extends JPanel {
 
         super.paintComponent(g);
     }
-    
+
     private JLabel createLabel(String text) {
     	JLabel label = new JLabel(text);
     	label.setOpaque(false);
@@ -889,7 +890,7 @@ public class InterstellarMapPanel extends JPanel {
     	label.setFont(label.getFont().deriveFont(Font.BOLD));
     	return(label);
     }
-    
+
     private JCheckBox createOptionCheckBox(String text, Icon checkboxIcon, Icon checkboxSelectedIcon) {
         JCheckBox checkBox = new JCheckBox(text);
         checkBox.setOpaque(false);
@@ -908,7 +909,7 @@ public class InterstellarMapPanel extends JPanel {
         });
         return checkBox;
     }
-    
+
     private JRadioButton createOptionRadioButton(String text, Icon checkboxIcon, Icon checkboxSelectedIcon) {
         JRadioButton radioButton = new JRadioButton(text);
         radioButton.setOpaque(false);
@@ -927,7 +928,7 @@ public class InterstellarMapPanel extends JPanel {
         });
         return radioButton;
     }
-    
+
     private void setupHexPath(GeneralPath path, double centerX, double centerY, double radius) {
         if(null == path) {
             return;
@@ -940,7 +941,7 @@ public class InterstellarMapPanel extends JPanel {
         }
         path.closePath();
     }
-    
+
     /**
      * Computes the map-coordinate from the screen coordinate system
      */
@@ -967,7 +968,6 @@ public class InterstellarMapPanel extends JPanel {
         transform.translate(conf.centerX, - conf.centerY);
         return transform;
     }
-    
     public void setSelectedSystem(PlanetarySystem p) {
         selectedSystem = p;
         if(conf.scale < 4.0) {
@@ -1005,7 +1005,7 @@ public class InterstellarMapPanel extends JPanel {
                 return false;
             }
         }
-        
+
         return true;
     }
     
@@ -1029,7 +1029,7 @@ public class InterstellarMapPanel extends JPanel {
         }
         return true;
     }
-    
+
     /**
      * Activate and Center
      */
@@ -1064,7 +1064,7 @@ public class InterstellarMapPanel extends JPanel {
         jumpPath = new JumpPath();
         notifyListeners();
     }
-    
+
     /**
      * Return a planet color based on what the user has selected from the radio button options
      * @param a Planet object
@@ -1287,25 +1287,25 @@ public class InterstellarMapPanel extends JPanel {
          */
         int planetID;
     }
-    
-    
+
+
     private transient List<ActionListener> listeners = new ArrayList<>();
-    
+
     public void addActionListener(ActionListener l) {
         if (!listeners.contains(l)) {
             listeners.add(l);
         }
     }
-    
+
     public void removeActionListener(ActionListener l) {
         listeners.remove(l);
     }
-    
+
     private void notifyListeners() {
         ActionEvent ev = new ActionEvent(this, ActionEvent.ACTION_FIRST, "refresh");
         listeners.forEach(l -> l.actionPerformed(ev));
     }
-    
+
     public boolean isFactionsSelected() {
     	return optFactions.isSelected();
     }
