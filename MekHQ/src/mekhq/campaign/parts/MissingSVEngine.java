e0c60051a0a9d5ef4698fca3493e894b418b3e2f
Carl Spain <neoancient@megamek.org>
2019-10-29 11:16:42 -0500

    Copyright headers and serialVersionUIDs

--- MekHQ/src/mekhq/campaign/parts/MissingSVEngine.java
+++ MekHQ/src/mekhq/campaign/parts/MissingSVEngine.java
@@ -1,20 +1,20 @@
 /*
- * MekBuilder - unit design companion of MegaMek
- * Copyright (C) 2017 The MegaMek Team
+ * Copyright (c) 2019 - The MegaMek Team
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This file is part of MekHQ.
  *
- * This program is distributed in the hope that it will be useful,
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
  */
 package mekhq.campaign.parts;
 
@@ -30,6 +30,7 @@ import java.io.PrintWriter;
  * Place holder for an engine that has been destroyed or removed from a support vehicle
  */
 public class MissingSVEngine extends MissingPart {
+    private static final long serialVersionUID = -1388693773192376347L;
 
     private double engineTonnage;
     private int etype;
--- MekHQ/src/mekhq/campaign/parts/SVEnginePart.java
+++ MekHQ/src/mekhq/campaign/parts/SVEnginePart.java
@@ -1,20 +1,20 @@
 /*
- * MekBuilder - unit design companion of MegaMek
- * Copyright (C) 2017 The MegaMek Team
+ * Copyright (c) 2019 - The MegaMek Team
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This file is part of MekHQ.
  *
- * This program is distributed in the hope that it will be useful,
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
  */
 package mekhq.campaign.parts;
 
@@ -34,6 +34,7 @@ import java.io.PrintWriter;
  * ICEs will also have the same fuel type.
  */
 public class SVEnginePart extends Part {
+    private static final long serialVersionUID = -5207004566629089937L;
 
     private double engineTonnage;
     private int etype;
