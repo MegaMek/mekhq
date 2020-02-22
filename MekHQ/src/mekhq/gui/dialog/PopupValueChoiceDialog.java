0e1d34d6d4700198172987f176d72104b8a87e47
unknown <jrbowen24@gmail.com>
2020-02-22 18:10:43 -0500

    1429: Refactoring in PopupValueChoiceDialog

--- MekHQ/src/mekhq/gui/dialog/PopupValueChoiceDialog.java
+++ MekHQ/src/mekhq/gui/dialog/PopupValueChoiceDialog.java
@@ -3,26 +3,16 @@
  *
  * Created on Jan 6, 2010, 10:46:02 PM
  */
-
 package mekhq.gui.dialog;
 
-import java.awt.BorderLayout;
-import java.awt.Frame;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.ResourceBundle;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.JFormattedTextField;
-import javax.swing.SpinnerNumberModel;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 import javax.swing.text.DefaultFormatter;
 
 import megamek.common.util.EncodeControl;
@@ -35,13 +25,8 @@ public class PopupValueChoiceDialog extends JDialog implements WindowListener {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private JButton btnDone;
-    private JButton btnCancel;
-    private JPanel pnlButton;
     private JSpinner value;
     private SpinnerNumberModel model;
-    private JFormattedTextField jtf;
-    private int max;
-    private int min;
     // End of variables declaration//GEN-END:variables
 
     /**
@@ -56,7 +41,6 @@ public class PopupValueChoiceDialog extends JDialog implements WindowListener {
 	/** Creates new form */
 	public PopupValueChoiceDialog(Frame parent, boolean modal, String title, int current, int min) {
 		super(parent, modal);
-        this.min = min;
         model = new SpinnerNumberModel(current, min, null, 1);
         setTitle(title);
         initComponents();
@@ -64,11 +48,9 @@ public class PopupValueChoiceDialog extends JDialog implements WindowListener {
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(this);
 	}
-	
+
     public PopupValueChoiceDialog(Frame parent, boolean modal, String title, int current, int min, int max) {
         super(parent, modal);
-        this.max = max;
-        this.min = min;
         model = new SpinnerNumberModel(current, min, max, 1);
         setTitle(title);
         initComponents();
@@ -78,14 +60,13 @@ public class PopupValueChoiceDialog extends JDialog implements WindowListener {
     }
 
     private void initComponents() {
-
-    	pnlButton = new JPanel();
+        JPanel pnlButton = new JPanel();
         btnDone = new JButton();
-        btnCancel = new JButton();
+        JButton btnCancel = new JButton();
         value = new JSpinner(model);
-        value.setEditor(new JSpinner.NumberEditor(value,"#")); //prevent digit grouping, e.g. 1,000
-        jtf = ((JSpinner.DefaultEditor) value.getEditor()).getTextField();
-        DefaultFormatter df = (DefaultFormatter)jtf.getFormatter();
+        value.setEditor(new JSpinner.NumberEditor(value, "#")); //prevent digit grouping, e.g. 1,000
+        JFormattedTextField jtf = ((JSpinner.DefaultEditor) value.getEditor()).getTextField();
+        DefaultFormatter df = (DefaultFormatter) jtf.getFormatter();
         df.setCommitsOnValidEdit(true);
 
 		ResourceBundle resourceMap = ResourceBundle.getBundle("mekhq.resources.PopupValueChoiceDialog", new EncodeControl()); //$NON-NLS-1$
@@ -94,19 +75,11 @@ public class PopupValueChoiceDialog extends JDialog implements WindowListener {
 
         btnDone.setText(resourceMap.getString("btnDone.text")); // NOI18N
         btnDone.setName("btnDone"); // NOI18N
-        btnDone.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                btnDoneActionPerformed(evt);
-            }
-        });
+        btnDone.addActionListener(this::btnDoneActionPerformed);
 
         btnCancel.setText(resourceMap.getString("btnCancel.text")); // NOI18N
         btnCancel.setName("btnCancel"); // NOI18N
-        btnCancel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent evt) {
-                btnCancelActionPerformed(evt);
-            }
-        });
+        btnCancel.addActionListener(this::btnCancelActionPerformed);
 
         pnlButton.setLayout(new GridLayout(0,2));
         pnlButton.add(btnDone);
@@ -119,31 +92,29 @@ public class PopupValueChoiceDialog extends JDialog implements WindowListener {
         getContentPane().add(value,BorderLayout.CENTER);
         getContentPane().add(pnlButton, BorderLayout.PAGE_END);
         pack();
-    }// </editor-fold>//GEN-END:initComponents
+    }
 
-    private void btnDoneActionPerformed(ActionEvent evt) {//GEN-FIRST:event_btnDoneActionPerformed
+    private void btnDoneActionPerformed(ActionEvent evt) {
         this.setVisible(false);
-    }//GEN-LAST:event_btnDoneActionPerformed
+    }
 
-    private void btnCancelActionPerformed(ActionEvent evt) {//GEN-FIRST:event_btnCancelActionPerformed
+    private void btnCancelActionPerformed(ActionEvent evt) {
         value.getModel().setValue(-1);
     	this.setVisible(false);
-    }//GEN-LAST:event_btnCancelActionPerformed
+    }
 
     /**
     * @param args the command line arguments
     */
-    public static void main(String args[]) {
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                PopupValueChoiceDialog dialog = new PopupValueChoiceDialog(new JFrame(), true, "Label", 0, 0, 1);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            PopupValueChoiceDialog dialog = new PopupValueChoiceDialog(new JFrame(), true, "Label", 0, 0, 1);
+            dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                public void windowClosing(WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+            dialog.setVisible(true);
         });
     }
 
