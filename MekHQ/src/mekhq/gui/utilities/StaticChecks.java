369e48414655703bdb23ad26b400b59a7ca20348
unknown <jrbowen24@gmail.com>
2020-03-07 22:33:21 -0500

    Reverting added imports to static checks

--- MekHQ/src/mekhq/gui/utilities/StaticChecks.java
+++ MekHQ/src/mekhq/gui/utilities/StaticChecks.java
@@ -1,12 +1,28 @@
+/*
+ * Copyright (c) 2014 - The MegaMek Team.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package mekhq.gui.utilities;
 
 import java.util.StringJoiner;
 import java.util.UUID;
 import java.util.Vector;
 
-import megamek.common.BattleArmor;
 import megamek.common.Entity;
-import megamek.common.Infantry;
 import megamek.common.UnitType;
 import mekhq.campaign.force.Force;
 import mekhq.campaign.personnel.Person;
