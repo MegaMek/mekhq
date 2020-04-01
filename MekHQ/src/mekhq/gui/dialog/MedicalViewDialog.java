1a194dc02c90a5464a0d090e08dbf309c297597b
Windchild292 <jrbowen24@gmail.com>
2020-04-01 17:06:37 -0400

    Removing unused import

--- MekHQ/src/mekhq/gui/dialog/MedicalViewDialog.java
+++ MekHQ/src/mekhq/gui/dialog/MedicalViewDialog.java
@@ -39,7 +39,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.stream.Collectors;
