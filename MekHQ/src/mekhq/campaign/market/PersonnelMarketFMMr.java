f622c9f502d5bbd4f300c62b20eb723b31a0bcc0
unknown <jrbowen24@gmail.com>
2020-04-07 14:06:26 -0400

    Switching FMMr Personnel Market to LocalDate

--- MekHQ/src/mekhq/campaign/market/PersonnelMarketFMMr.java
+++ MekHQ/src/mekhq/campaign/market/PersonnelMarketFMMr.java
@@ -19,7 +19,6 @@
 package mekhq.campaign.market;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
 
@@ -32,9 +31,8 @@ import mekhq.module.api.PersonnelMarketMethod;
 
 /**
  * Generation method for personnel market based on Field Manual: Mercenaries (Revised)
- * 
- * @author Neoancient
  *
+ * @author Neoancient
  */
 public class PersonnelMarketFMMr implements PersonnelMarketMethod {
 
@@ -45,7 +43,7 @@ public class PersonnelMarketFMMr implements PersonnelMarketMethod {
 
     @Override
     public List<Person> generatePersonnelForDay(Campaign c) {
-        if (c.getCalendar().get(Calendar.DAY_OF_MONTH) != 1) {
+        if (c.getLocalDate().getDayOfMonth() != 1) {
             return null;
         }
         List<Person> retVal = new ArrayList<>();
@@ -106,11 +104,10 @@ public class PersonnelMarketFMMr implements PersonnelMarketMethod {
 
     @Override
     public List<Person> removePersonnelForDay(Campaign c, List<Person> current) {
-        if (c.getCalendar().get(Calendar.DAY_OF_MONTH) == 1) {
+        if (c.getLocalDate().getDayOfMonth() == 1) {
             return current;
         } else {
             return null;
         }
     }
-
 }
