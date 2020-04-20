d0d8d9d1e48cbb0e13bedc84fee359c2c6d8e72c
Windchild292 <jrbowen24@gmail.com>
2020-04-20 13:07:04 -0400

    Removing whitespace to force a refresh of CI

--- MekHQ/src/mekhq/gui/adapter/PersonnelTableMouseAdapter.java
+++ MekHQ/src/mekhq/gui/adapter/PersonnelTableMouseAdapter.java
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package mekhq.gui.adapter;
 
 import java.awt.*;
