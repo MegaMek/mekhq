9356cae85a84a96c4c374a01adff6a64f6aae8c5
Aaron Gullickson <aarong@uoregon.edu>
2020-05-28 22:05:55 -0700

    procurement table for command center

--- MekHQ/src/mekhq/gui/CommandCenterTab.java
+++ MekHQ/src/mekhq/gui/CommandCenterTab.java
@@ -18,10 +18,21 @@
  */
 package mekhq.gui;
 
+import megamek.common.event.Subscribe;
 import mekhq.MekHQ;
+import mekhq.campaign.event.ProcurementEvent;
+import mekhq.campaign.unit.UnitOrder;
+import mekhq.gui.adapter.ProcurementTableMouseAdapter;
+import mekhq.gui.model.ProcurementTableModel;
+import mekhq.gui.sorter.FormattedNumberSorter;
+import mekhq.gui.sorter.TargetSorter;
 
 import javax.swing.*;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 /**
  * Collates important information about the campaign and displays it
@@ -31,6 +42,12 @@ public final class CommandCenterTab extends CampaignGuiTab {
 
     private JPanel panCommand;
 
+    // procurement table
+    private JTable procurementTable;
+    private ProcurementTableModel procurementModel;
+
+
+
     CommandCenterTab(CampaignGUI gui, String name) {
         super(gui, name);
         MekHQ.registerHandler(this);
@@ -39,17 +56,101 @@ public final class CommandCenterTab extends CampaignGuiTab {
     @Override
     public void initTab() {
         panCommand = new JPanel(new GridBagLayout());
+
+        /* Shopping Table */
+        procurementModel = new ProcurementTableModel(getCampaign());
+        procurementTable = new JTable(procurementModel);
+        TableRowSorter<ProcurementTableModel> shoppingSorter = new TableRowSorter<>(
+                procurementModel);
+        shoppingSorter.setComparator(ProcurementTableModel.COL_COST, new FormattedNumberSorter());
+        shoppingSorter.setComparator(ProcurementTableModel.COL_TARGET, new TargetSorter());
+        procurementTable.setRowSorter(shoppingSorter);
+        TableColumn column;
+        for (int i = 0; i < ProcurementTableModel.N_COL; i++) {
+            column = procurementTable.getColumnModel().getColumn(i);
+            column.setPreferredWidth(procurementModel.getColumnWidth(i));
+            column.setCellRenderer(procurementModel.getRenderer());
+        }
+        procurementTable.setIntercellSpacing(new Dimension(0, 0));
+        procurementTable.setShowGrid(false);
+        procurementTable.addMouseListener(new ProcurementTableMouseAdapter(getCampaignGui()));
+        procurementTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        procurementTable.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "ADD");
+        procurementTable.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_EQUALS, 0),
+                "ADD");
+        procurementTable.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0),
+                "REMOVE");
+        procurementTable.getInputMap(JComponent.WHEN_FOCUSED).put(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, 0),
+                "REMOVE");
+
+        procurementTable.getActionMap().put("ADD", new AbstractAction() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = 4958203340754214211L;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (procurementTable.getSelectedRow() < 0) {
+                    return;
+                }
+                procurementModel
+                        .incrementItem(procurementTable.convertRowIndexToModel(procurementTable.getSelectedRow()));
+            }
+        });
+
+        procurementTable.getActionMap().put("REMOVE", new AbstractAction() {
+            /**
+             *
+             */
+            private static final long serialVersionUID = -8377486575329708963L;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (procurementTable.getSelectedRow() < 0) {
+                    return;
+                }
+                if (procurementModel
+                        .getAcquisition(procurementTable.convertRowIndexToModel(procurementTable.getSelectedRow()))
+                        .getQuantity() > 0) {
+                    procurementModel.decrementItem(
+                            procurementTable.convertRowIndexToModel(procurementTable.getSelectedRow()));
+                }
+            }
+        });
+
+        JScrollPane scrollProcurement = new JScrollPane(procurementTable);
+        JPanel panProcurement = new JPanel(new GridLayout(0, 1));
+        panProcurement.setBorder(BorderFactory.createTitledBorder("Active Procurement List"));
+        panProcurement.add(scrollProcurement);
+        panProcurement.setMinimumSize(new Dimension(200, 200));
+        panProcurement.setPreferredSize(new Dimension(200, 200));
+
         setLayout(new BorderLayout());
-        add(panCommand, BorderLayout.CENTER);
+        //add(panCommand, BorderLayout.CENTER);
+        add(panProcurement, BorderLayout.CENTER);
     }
 
     @Override
     public void refreshAll() {
-
+        refreshProcurementList();
     }
 
     @Override
     public GuiTabType tabType() {
         return GuiTabType.COMMAND;
     }
+
+    private void refreshProcurementList() {
+        procurementModel.setData(getCampaign().getShoppingList().getAllShoppingItems());
+    }
+
+    private ActionScheduler procurementListScheduler = new ActionScheduler(this::refreshProcurementList);
+
+    @Subscribe
+    public void handle(ProcurementEvent ev) {
+        procurementListScheduler.schedule();
+    }
+
 }
