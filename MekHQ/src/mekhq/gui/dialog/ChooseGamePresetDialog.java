8cdeb401335fce1b1ddd66c401c905c94514a0b4
Windchild292 <jrbowen24@gmail.com>
2020-08-03 11:37:42 -0400

    Fixing tab spacing

--- MekHQ/src/mekhq/gui/dialog/ChooseGamePresetDialog.java
+++ MekHQ/src/mekhq/gui/dialog/ChooseGamePresetDialog.java
@@ -46,17 +46,17 @@ import mekhq.preferences.PreferencesNode;
  * @author Taharqa
  */
 public class ChooseGamePresetDialog extends javax.swing.JDialog {
-	private static final long serialVersionUID = -8038099101234445018L;
+    private static final long serialVersionUID = -8038099101234445018L;
 
 
-	private javax.swing.JPanel panButtons;
-	private javax.swing.JButton btnCancel;
+    private javax.swing.JPanel panButtons;
+    private javax.swing.JButton btnCancel;
     private javax.swing.JButton btnOk;
 
     private List<GamePreset> presets;
     private List<JRadioButton> presetButtons;
     private GamePreset selectedPreset;
-	private boolean cancelled;
+    private boolean cancelled;
 
     /** Creates new form NewTeamDialog */
     public ChooseGamePresetDialog(java.awt.Frame parent, boolean modal, List<GamePreset> p) {
@@ -70,83 +70,83 @@ public class ChooseGamePresetDialog extends javax.swing.JDialog {
     }
 
     private void initComponents() {
-    	 java.awt.GridBagConstraints gridBagConstraints;
+         java.awt.GridBagConstraints gridBagConstraints;
 
-    	 panButtons = new javax.swing.JPanel();
-    	 btnOk = new javax.swing.JButton();
-    	 btnCancel = new javax.swing.JButton();
-    	 presetButtons = new ArrayList<>();
+         panButtons = new javax.swing.JPanel();
+         btnOk = new javax.swing.JButton();
+         btnCancel = new javax.swing.JButton();
+         presetButtons = new ArrayList<>();
 
-    	 JPanel mainPanel = new JPanel(new GridBagLayout());
-    	 JScrollPane scrPane = new JScrollPane(mainPanel);
+         JPanel mainPanel = new JPanel(new GridBagLayout());
+         JScrollPane scrPane = new JScrollPane(mainPanel);
 
-    	 ButtonGroup group = new ButtonGroup();
+         ButtonGroup group = new ButtonGroup();
 
-    	 ResourceBundle resourceMap = ResourceBundle.getBundle("mekhq.resources.ChooseGamePresetDialog", new EncodeControl()); //$NON-NLS-1$
-    	 setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-    	 setName("Form"); // NOI18N
+         ResourceBundle resourceMap = ResourceBundle.getBundle("mekhq.resources.ChooseGamePresetDialog", new EncodeControl()); //$NON-NLS-1$
+         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+         setName("Form"); // NOI18N
 
-    	 getContentPane().setLayout(new BorderLayout());
-    	 setTitle(resourceMap.getString("title"));
+         getContentPane().setLayout(new BorderLayout());
+         setTitle(resourceMap.getString("title"));
 
-    	 int i = 0;
-    	 for (GamePreset preset : presets) {
-    		 JRadioButton presetButton = new JRadioButton("");
-    		 presetButtons.add(presetButton);
+         int i = 0;
+         for (GamePreset preset : presets) {
+             JRadioButton presetButton = new JRadioButton("");
+             presetButtons.add(presetButton);
              presetButton.setSelected(i == 0);
-    		 group.add(presetButton);
-        	 JLabel label = new JLabel("<html><b>" + preset.getTitle() + "</b><br>" + preset.getDescription() + "</html>");
+             group.add(presetButton);
+             JLabel label = new JLabel("<html><b>" + preset.getTitle() + "</b><br>" + preset.getDescription() + "</html>");
 
-        	 JPanel presetPanel = new JPanel(new BorderLayout());
-        	 presetPanel.add(presetButton, BorderLayout.WEST);
-        	 presetPanel.add(label, BorderLayout.CENTER);
-        	 presetPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-    		 presetPanel.setPreferredSize(new Dimension(450, 100));
+             JPanel presetPanel = new JPanel(new BorderLayout());
+             presetPanel.add(presetButton, BorderLayout.WEST);
+             presetPanel.add(label, BorderLayout.CENTER);
+             presetPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+             presetPanel.setPreferredSize(new Dimension(450, 100));
 
-    		 gridBagConstraints = new java.awt.GridBagConstraints();
-        	 gridBagConstraints.gridx = 0;
-        	 gridBagConstraints.gridy = i;
-        	 gridBagConstraints.weightx = 1.0;
-        	 gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        	 gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        	 gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
-        	 mainPanel.add(presetPanel, gridBagConstraints);
-        	 i++;
-    	 }
+             gridBagConstraints = new java.awt.GridBagConstraints();
+             gridBagConstraints.gridx = 0;
+             gridBagConstraints.gridy = i;
+             gridBagConstraints.weightx = 1.0;
+             gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+             gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+             gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+             mainPanel.add(presetPanel, gridBagConstraints);
+             i++;
+         }
 
-    	 panButtons.setName("panButtons");
-    	 panButtons.setLayout(new GridBagLayout());
+         panButtons.setName("panButtons");
+         panButtons.setLayout(new GridBagLayout());
 
-    	 btnOk.setText(resourceMap.getString("btnOk.text")); // NOI18N
-    	 btnOk.setName("btnOk"); // NOI18N
-    	 btnOk.addActionListener(this::btnOkActionPerformed);
-    	 gridBagConstraints = new java.awt.GridBagConstraints();
-    	 gridBagConstraints.gridx = 0;
-    	 gridBagConstraints.gridy = 0;
-    	 gridBagConstraints.gridwidth = 1;
-    	 gridBagConstraints.weightx = 1.0;
-    	 gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
-    	 gridBagConstraints.insets = new java.awt.Insets(5, 5, 0, 0);
-    	 panButtons.add(btnOk, gridBagConstraints);
+         btnOk.setText(resourceMap.getString("btnOk.text")); // NOI18N
+         btnOk.setName("btnOk"); // NOI18N
+         btnOk.addActionListener(this::btnOkActionPerformed);
+         gridBagConstraints = new java.awt.GridBagConstraints();
+         gridBagConstraints.gridx = 0;
+         gridBagConstraints.gridy = 0;
+         gridBagConstraints.gridwidth = 1;
+         gridBagConstraints.weightx = 1.0;
+         gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+         gridBagConstraints.insets = new java.awt.Insets(5, 5, 0, 0);
+         panButtons.add(btnOk, gridBagConstraints);
 
-    	 btnCancel.setText(resourceMap.getString("btnCancel.text")); // NOI18N
-    	 btnCancel.setName("btnClose"); // NOI18N
-    	 btnCancel.addActionListener(this::btnCancelActionPerformed);
-    	 gridBagConstraints = new java.awt.GridBagConstraints();
-    	 gridBagConstraints.gridx = 1;
-    	 gridBagConstraints.gridy = 0;
-    	 gridBagConstraints.gridwidth = 1;
-    	 gridBagConstraints.weightx = 0.0;
-    	 gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
-    	 gridBagConstraints.insets = new java.awt.Insets(5, 5, 0, 0);
-    	 panButtons.add(btnCancel, gridBagConstraints);
+         btnCancel.setText(resourceMap.getString("btnCancel.text")); // NOI18N
+         btnCancel.setName("btnClose"); // NOI18N
+         btnCancel.addActionListener(this::btnCancelActionPerformed);
+         gridBagConstraints = new java.awt.GridBagConstraints();
+         gridBagConstraints.gridx = 1;
+         gridBagConstraints.gridy = 0;
+         gridBagConstraints.gridwidth = 1;
+         gridBagConstraints.weightx = 0.0;
+         gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
+         gridBagConstraints.insets = new java.awt.Insets(5, 5, 0, 0);
+         panButtons.add(btnCancel, gridBagConstraints);
 
-    	 scrPane.setPreferredSize(new Dimension(500, 430));
+         scrPane.setPreferredSize(new Dimension(500, 430));
 
-    	 getContentPane().add(scrPane, BorderLayout.CENTER);
-    	 getContentPane().add(panButtons, BorderLayout.SOUTH);
+         getContentPane().add(scrPane, BorderLayout.CENTER);
+         getContentPane().add(panButtons, BorderLayout.SOUTH);
 
-    	 pack();
+         pack();
     }
 
     private void setUserPreferences() {
@@ -157,28 +157,28 @@ public class ChooseGamePresetDialog extends javax.swing.JDialog {
     }
 
     private void btnOkActionPerformed(java.awt.event.ActionEvent evt) {
-    	int sel = 0;
-    	for (JRadioButton button : presetButtons) {
-    		if (button.isSelected()) {
-    			selectedPreset = presets.get(sel);
-    			break;
-    		}
-    		sel++;
-    	}
+        int sel = 0;
+        for (JRadioButton button : presetButtons) {
+            if (button.isSelected()) {
+                selectedPreset = presets.get(sel);
+                break;
+            }
+            sel++;
+        }
 
-    	this.setVisible(false);
+        this.setVisible(false);
     }
 
     private void btnCancelActionPerformed(java.awt.event.ActionEvent evt) {
-    	cancelled = true;
-    	this.setVisible(false);
+        cancelled = true;
+        this.setVisible(false);
     }
 
     public boolean wasCancelled() {
-    	return cancelled;
+        return cancelled;
     }
 
     public GamePreset getSelectedPreset() {
-    	return selectedPreset;
+        return selectedPreset;
     }
 }
