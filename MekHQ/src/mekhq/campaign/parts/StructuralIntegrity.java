217d04d5cc9ac37bf8fe3c18bf9a9a993817e0e0
RexPearce <rex.pearce@risky.ca>
2020-09-28 13:02:42 -0400

    preserve damage to internals on refit for Aeros and Tanks

--- MekHQ/src/mekhq/campaign/parts/StructuralIntegrity.java
+++ MekHQ/src/mekhq/campaign/parts/StructuralIntegrity.java
@@ -208,7 +208,7 @@ public class StructuralIntegrity extends Part {
 
     @Override
     public void updateConditionFromPart() {
-        //you can't replace this part, so don't do anything here
+        ((Aero) unit.getEntity()).setSI(((Aero) unit.getEntity()).get0SI() - pointsNeeded);
     }
 
     @Override
