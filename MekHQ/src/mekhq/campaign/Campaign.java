211a0bb678070803c080859b84d42ddfc9c385c2
Christopher Watford <christopher.watford@gmail.com>
2020-10-05 15:28:35 -0400

    Split out sorted views of the hangar into HangarSorter

--- MekHQ/src/mekhq/campaign/Campaign.java
+++ MekHQ/src/mekhq/campaign/Campaign.java
@@ -1113,51 +1113,6 @@ public class Campaign implements Serializable, ITechManager {
         return getHangar().getUnits();
     }
 
-    public List<Unit> getUnits(boolean weightSorted) {
-        return getUnits(false, weightSorted, false);
-    }
-
-    /**
-     * This returns a list of the current units, sorted alphabetically and potentially by other methods
-     * @param weightSorted      True if the unit list is sorted by weight descending, otherwise false
-     * @param weightClassSorted True if the unit list is sorted by weight class in format heaviest to lightest, otherwise false
-     * @param unitTypeSorted    True if the unit list is sorted by the unit type
-     * @return a copy of the units in the campaign with the applicable sort format
-     */
-    public List<Unit> getUnits(boolean weightClassSorted, boolean weightSorted,
-                               boolean unitTypeSorted) {
-        List<Unit> units = getCopyOfUnits();
-
-        // Natural order sorting the units alphabetically is the default for getSortedUnits
-        units.sort(Comparator.comparing(Unit::getName, new NaturalOrderComparator()));
-
-        if (weightClassSorted || weightSorted || unitTypeSorted) {
-            // We need to determine these by both the weight sorted and weight class sorted values,
-            // as to properly sort by weight class and weight we should do both at the same time
-            if (weightSorted && weightClassSorted) {
-                units.sort((lhs, rhs) -> {
-                    int weightClass1 = lhs.getEntity().getWeightClass();
-                    int weightClass2 = rhs.getEntity().getWeightClass();
-                    if (weightClass1 == weightClass2) {
-                        return Double.compare(rhs.getEntity().getWeight(), lhs.getEntity().getWeight());
-                    } else {
-                        return weightClass2 - weightClass1;
-                    }
-                });
-            } else if (weightClassSorted) {
-                units.sort(Comparator.comparingInt(o -> o.getEntity().getWeightClass()));
-            } else if (weightSorted) {
-                // Sorted in descending order of weights
-                units.sort(Comparator.comparingDouble(o -> o.getEntity().getWeight()));
-            }
-
-            if (unitTypeSorted) {
-                units.sort(Comparator.comparingInt(e -> e.getEntity().getUnitType()));
-            }
-        }
-        return units;
-    }
-
     // Since getUnits doesn't return a defensive copy and I don't know what I might break if I made it do so...
     public ArrayList<Unit> getCopyOfUnits() {
         return new ArrayList<>(getHangar().getUnits());
