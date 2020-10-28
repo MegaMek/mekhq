5588a2cabc37ca4b8378bdea52ba85152b994b3e
Christopher Watford <christopher.watford@gmail.com>
2020-10-28 00:05:29 -0400

    Apply suggestions from code review
    Co-authored-by: Justin Bowen <39067288+Windchild292@users.noreply.github.com>

--- MekHQ/unittests/mekhq/campaign/WarehouseTest.java
+++ MekHQ/unittests/mekhq/campaign/WarehouseTest.java
@@ -10,11 +10,11 @@
  *
  * MekHQ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MekHQ. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package mekhq.campaign;
@@ -157,7 +157,7 @@ public class WarehouseTest {
         // We should have been assigned an ID...
         assertTrue(mockPart1.getId() > 0);
 
-        // ...that is not the same as our previous part
+        // ... that is not the same as our previous part
         assertNotEquals(mockPart0.getId(), mockPart1.getId());
     }
 
