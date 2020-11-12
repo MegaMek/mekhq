79ac01af9cdf4aaba393b7212679667cd8e1243c
Christopher Watford <christopher.watford@gmail.com>
2020-11-12 15:01:04 -0500

    Add Javelin 10N to 10A test

--- MekHQ/unittests/mekhq/campaign/parts/RefitTest.java
+++ MekHQ/unittests/mekhq/campaign/parts/RefitTest.java
@@ -22,7 +22,6 @@ package mekhq.campaign.parts;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -47,11 +46,13 @@ import megamek.common.IPlayer;
 import mekhq.MekHqXmlUtil;
 import mekhq.Version;
 import mekhq.campaign.Campaign;
+import mekhq.campaign.CampaignOptions;
 import mekhq.campaign.Warehouse;
 import mekhq.campaign.finances.Money;
 import mekhq.campaign.parts.equipment.AmmoBin;
 import mekhq.campaign.parts.equipment.EquipmentPart;
 import mekhq.campaign.parts.equipment.MissingEquipmentPart;
+import mekhq.campaign.personnel.Person;
 import mekhq.campaign.unit.Unit;
 import mekhq.campaign.unit.UnitTestUtilities;
 
@@ -132,7 +133,9 @@ public class RefitTest {
         // Everything else is the same.
         //
 
-        // TODO: confirm this
+        // Per SO p188:
+        //     "This kit permits players to install a new item
+        //      where previously there was none..."
         assertEquals(Refit.CLASS_D, refit.getRefitClass());
 
         // Time?
@@ -169,7 +172,7 @@ public class RefitTest {
     }
 
     @Test
-    public void testBasicRefitWriteToXml() throws ParserConfigurationException, SAXException, IOException {
+    public void testLocust1Vto1EWriteToXml() throws ParserConfigurationException, SAXException, IOException {
         Campaign mockCampaign = mock(Campaign.class);
         when(mockCampaign.getEntities()).thenReturn(new ArrayList<>());
         Warehouse mockWarehouse = mock(Warehouse.class);
@@ -232,6 +235,202 @@ public class RefitTest {
         assertEquals(refit.isCustomJob(), deserialized.isCustomJob());
         assertEquals(refit.kitFound(), deserialized.kitFound());
         assertEquals(refit.isBeingRefurbished(), deserialized.isBeingRefurbished());
+        assertEquals(refit.getTech(), deserialized.getTech());
+
+        // Check that we got all the correct old parts in the XML
+        Set<Integer> oldUnitParts = refit.getOldUnitParts()
+                .stream().map(p -> p.getId()).collect(Collectors.toSet());
+        Set<Integer> serializedOldParts = deserialized.getOldUnitParts()
+                .stream().map(p -> p.getId()).collect(Collectors.toSet());
+        assertEquals(oldUnitParts, serializedOldParts);
+
+        // Check that we got all the correct new parts in the XML
+        Set<Integer> newUnitParts = refit.getNewUnitParts()
+                .stream().map(p -> p.getId()).collect(Collectors.toSet());
+        Set<Integer> serializedNewParts = deserialized.getNewUnitParts()
+                .stream().map(p -> p.getId()).collect(Collectors.toSet());
+        assertEquals(newUnitParts, serializedNewParts);
+
+        // Check that we got all the shopping list entries (by name, not amazing but reasonable)
+        List<String> shoppingList = refit.getShoppingList()
+                .stream().map(p -> p.getName()).collect(Collectors.toList());
+        List<String> serializedShoppingList = deserialized.getShoppingList()
+                .stream().map(p -> p.getName()).collect(Collectors.toList());
+
+        // Make sure they're the same length first...
+        assertEquals(shoppingList.size(), serializedShoppingList.size());
+
+        // ...then make sure they're the "same" by removing them one by one...
+        for (String partName : shoppingList) {
+            assertTrue(serializedShoppingList.remove(partName));
+        }
+
+        // ...and ensuring nothing is left.
+        assertTrue(serializedShoppingList.isEmpty());
+
+        // Do the same for their descriptions, which include the quantities...
+        List<String> shoppingListDescs = Arrays.asList(refit.getShoppingListDescription());
+        // ...except the second list needs to be mutable.
+        List<String> serializedShoppingListDescs = new ArrayList<>(Arrays.asList(deserialized.getShoppingListDescription()));
+
+        assertEquals(shoppingListDescs.size(), serializedShoppingListDescs.size());
+        for (String desc : shoppingListDescs) {
+            assertTrue(serializedShoppingListDescs.remove(desc));
+        }
+
+        assertTrue(serializedShoppingListDescs.isEmpty());
+    }
+
+    @Test
+    public void javelinJVN10Nto10ATest() {
+        Campaign mockCampaign = mock(Campaign.class);
+        CampaignOptions mockCampaignOptions = mock(CampaignOptions.class);
+        when(mockCampaign.getCampaignOptions()).thenReturn(mockCampaignOptions);
+        Warehouse mockWarehouse = mock(Warehouse.class);
+        when(mockCampaign.getWarehouse()).thenReturn(mockWarehouse);
+
+        // Create the original entity backing the unit
+        Entity oldEntity = UnitTestUtilities.getJavelinJVN10N();
+        IPlayer mockPlayer = mock(IPlayer.class);
+        when(mockPlayer.getName()).thenReturn("Test Player");
+        oldEntity.setOwner(mockPlayer);
+
+        // Create the entity we're going to refit to
+        Entity newEntity = UnitTestUtilities.getJavelinJVN10A();
+
+        // Create the unit which will be refit
+        Unit oldUnit = new Unit(oldEntity, mockCampaign);
+        oldUnit.setId(UUID.randomUUID());
+        oldUnit.initializeParts(false);
+
+        // Create the Refit
+        Refit refit = new Refit(oldUnit, newEntity, false, false);
+        assertEquals(mockCampaign, refit.getCampaign());
+
+        //
+        // Javelin 10N to 10A Class C refit steps (in no particular order):
+        //     1. Remove excess SRM 6 (LT) [120 mins]
+        //     2. Remove excess SRM 6 (RT) [120 mins]
+        //     3. Remove SRM 6 Ammo Bin (LT) [120 mins]
+        //     4. Remove SRM 6 Ammo Bin (RT) [120 mins]
+        //     5. Add LRM 15 to (RT) [120 mins]
+        //     6. Add LRM 15 Ammo Bin to (RT) [120 mins]
+        //
+        // Everything else is the same.
+        //
+
+        // Per SO p188:
+        //     "A Class C kit also enables replacement of a weapon
+        //      or item of equipment with any other, even if it is
+        //      larger than the item(s) being replaced; for example,
+        //      replacing an ER large laser with an LRM-10 launcher
+        //      and ammunition."
+        assertEquals(Refit.CLASS_C, refit.getRefitClass());
+
+        // Time?
+        //     + 4 removals @ 120 mins ea
+        //     + 2 adds @ 120 mins ea
+        //     x 2 (Class C)
+        assertEquals((120.0 * 6.0) * 2.0, refit.getActualTime(), 0.1);
+
+        // Cost?
+        //    + 1 LRM 15 @ 175,000 ea
+        //    + 1 ton LRM 15 Ammo @ 30,000 ea
+        //    x 1.1 (Refit Kit cost, SO p188)
+        assertEquals(Money.of(175000.0 + 30000.0).multipliedBy(1.1), refit.getCost());
+
+        // We're removing 2 SRM 6s and two ammo bins
+        List<Part> removedParts = refit.getOldUnitParts();
+        assertEquals(4, removedParts.size());
+        assertEquals(2, removedParts.stream()
+                .filter(p -> (p instanceof EquipmentPart) && p.getName().equals("SRM 6")).count());
+        assertEquals(2, removedParts.stream()
+                .filter(p -> (p instanceof AmmoBin) && p.getName().equals("SRM 6 Ammo Bin")).count());
+
+        // All of the new parts should be from the old unit
+        List<Part> newParts = refit.getNewUnitParts();
+        assertTrue(newParts.stream().allMatch(p -> p.getUnit().equals(oldUnit)));
+
+        // We need to buy one LRM 15 and one LRM 15 Ammo Bin
+        List<Part> shoppingCart = refit.getShoppingList();
+        assertEquals(1, shoppingCart.stream()
+                .filter(p -> (p instanceof MissingEquipmentPart) && p.getName().equals("LRM 15")).count());
+        assertEquals(1, shoppingCart.stream()
+                .filter(p -> (p instanceof AmmoBin) && p.getName().equals("LRM 15 Ammo Bin")).count());
+    }
+
+    @Test
+    public void testJavelinJVN10Nto10AWriteToXml() throws ParserConfigurationException, SAXException, IOException {
+        Campaign mockCampaign = mock(Campaign.class);
+        when(mockCampaign.getEntities()).thenReturn(new ArrayList<>());
+        CampaignOptions mockCampaignOptions = mock(CampaignOptions.class);
+        when(mockCampaign.getCampaignOptions()).thenReturn(mockCampaignOptions);
+        Warehouse mockWarehouse = mock(Warehouse.class);
+        when(mockCampaign.getWarehouse()).thenReturn(mockWarehouse);
+        Person mockTech = mock(Person.class);
+        UUID techId = UUID.randomUUID();
+        when(mockTech.getId()).thenReturn(techId);
+
+        // Create the original entity backing the unit
+        Entity oldEntity = UnitTestUtilities.getJavelinJVN10N();
+        IPlayer mockPlayer = mock(IPlayer.class);
+        when(mockPlayer.getName()).thenReturn("Test Player");
+        oldEntity.setOwner(mockPlayer);
+
+        // Create the entity we're going to refit to
+        Entity newEntity = UnitTestUtilities.getJavelinJVN10A();
+
+        // Create the unit which will be refit
+        Unit oldUnit = new Unit(oldEntity, mockCampaign);
+        oldUnit.setId(UUID.randomUUID());
+        oldUnit.initializeParts(false);
+
+        // Make sure the unit parts have an ID before we serialize them
+        int partId = 1;
+        for (Part part : oldUnit.getParts()) {
+            part.setId(partId++);
+        }
+
+        // Create the Refit
+        Refit refit = new Refit(oldUnit, newEntity, false, false);
+        refit.setTech(mockTech);
+        refit.addTimeSpent(60); // 1 hour of work!
+
+        // Write the Refit XML
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        refit.writeToXml(pw, 0);
+
+        // Get the Refit XML
+        String xml = sw.toString();
+        assertFalse(xml.trim().isEmpty());
+
+        // Using factory get an instance of document builder
+        DocumentBuilder db = MekHqXmlUtil.newSafeDocumentBuilder();
+
+        // Parse using builder to get DOM representation of the XML file
+        Document xmlDoc = db.parse(new ByteArrayInputStream(xml.getBytes()));
+
+        Element refitElt = xmlDoc.getDocumentElement();
+        assertEquals("refit", refitElt.getNodeName());
+
+        // Deserialize the refit
+        Refit deserialized = Refit.generateInstanceFromXML(refitElt, oldUnit, new Version("1.0.0"));
+        assertNotNull(deserialized);
+
+        // Spot check the values
+        assertEquals(refit.getTime(), deserialized.getTime());
+        assertEquals(refit.getActualTime(), deserialized.getActualTime());
+        assertEquals(refit.getCost(), deserialized.getCost());
+        assertEquals(refit.isSameArmorType(), deserialized.isSameArmorType());
+        assertEquals(refit.hasFailedCheck(), deserialized.hasFailedCheck());
+        assertEquals(refit.getRefitClass(), deserialized.getRefitClass());
+        assertEquals(refit.getTimeSpent(), deserialized.getTimeSpent());
+        assertEquals(refit.getTimeLeft(), deserialized.getTimeLeft());
+        assertEquals(refit.isCustomJob(), deserialized.isCustomJob());
+        assertEquals(refit.kitFound(), deserialized.kitFound());
+        assertEquals(refit.isBeingRefurbished(), deserialized.isBeingRefurbished());
+        assertEquals(refit.getTech().getId(), deserialized.getTech().getId());
 
         // Check that we got all the correct old parts in the XML
         Set<Integer> oldUnitParts = refit.getOldUnitParts()
