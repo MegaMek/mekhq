fd50c955975799f08d5356c5d108f6e6ea7744f1
Windchild292 <jrbowen24@gmail.com>
2020-12-04 16:10:48 -0500

    Fixing missing comments on the migration

--- MekHQ/src/mekhq/campaign/io/Migration/PersonMigrator.java
+++ MekHQ/src/mekhq/campaign/io/Migration/PersonMigrator.java
@@ -1,6 +1,30 @@
+/*
+ * Copyright (c) 2020 - The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ. If not, see <http://www.gnu.org/licenses/>.
+ */
 package mekhq.campaign.io.Migration;
 
 public class PersonMigrator {
+
+    /**
+     * This migrates awards from the Default Set of pre-0.47.6 to the newer standard
+     * @param text the previous award name
+     * @return the new award name
+     */
     public static String awardDefaultSetMigrator(String text) {
         switch (text) {
             case "Armed Forces":
