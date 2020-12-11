d670875dd007cd8246d449028b1b2e0acceb3c12
Christopher Watford <christopher.watford@gmail.com>
2020-12-11 10:43:46 -0500

    Ensure we skip non-Large Craft bins

--- MekHQ/unittests/mekhq/campaign/unit/actions/AdjustLargeCraftAmmoActionTest.java
+++ MekHQ/unittests/mekhq/campaign/unit/actions/AdjustLargeCraftAmmoActionTest.java
@@ -36,6 +36,7 @@ import megamek.common.Mounted;
 import mekhq.campaign.Campaign;
 import mekhq.campaign.Quartermaster;
 import mekhq.campaign.parts.Part;
+import mekhq.campaign.parts.equipment.AmmoBin;
 import mekhq.campaign.parts.equipment.LargeCraftAmmoBin;
 import mekhq.campaign.unit.Unit;
 
@@ -161,6 +162,104 @@ public class AdjustLargeCraftAmmoActionTest {
         LargeCraftAmmoBin ammoBin = mock(LargeCraftAmmoBin.class);
         when(ammoBin.getEquipmentNum()).thenReturn(equipmentNum);
         when(unit.getParts()).thenReturn(Arrays.asList(new Part[] { ammoBin }));
+
+        AdjustLargeCraftAmmoAction action = new AdjustLargeCraftAmmoAction();
+
+        action.execute(campaign, unit);
+
+        // Ensure we didn't add any new parts
+        verify(unit, times(0)).addPart(any());
+        verify(quartermaster, times(0)).addPart(any(), anyInt());
+
+        // Ensure we updated the part's type
+        verify(ammoBin, times(1)).changeMunition(eq(ammoType0));
+    }
+
+    @Test
+    public void addsMissingBinsSkipsNonLargeCraftBins() {
+        Campaign campaign = mock(Campaign.class);
+        Quartermaster quartermaster = mock(Quartermaster.class);
+        when(campaign.getQuartermaster()).thenReturn(quartermaster);
+
+        // Put together a unit with 1 bay, with 1 ammo type
+        Unit unit = mock(Unit.class);
+        when(unit.getCampaign()).thenReturn(campaign);
+        Entity entity = mock(Entity.class);
+        when(entity.usesWeaponBays()).thenReturn(true);
+        when(entity.getWeight()).thenReturn(500.0);
+        int equipmentNum = 11;
+        Mounted bin0 = mock(Mounted.class);
+        doReturn(equipmentNum).when(entity).getEquipmentNum(eq(bin0));
+        AmmoType ammoType0 = getAmmoType("ISLRM20 Ammo");
+        when(bin0.getType()).thenReturn(ammoType0);
+        int capacity = 10;
+        when(bin0.getSize()).thenReturn((double) capacity);
+        when(bin0.getOriginalShots()).thenReturn(capacity * ammoType0.getShots());
+        int onHand = capacity - 1;
+        when(bin0.getBaseShotsLeft()).thenReturn(onHand * ammoType0.getShots());
+        ArrayList<Mounted> ammo = new ArrayList<>();
+        ammo.add(bin0);
+        when(entity.getAmmo()).thenReturn(ammo);
+        int bayEquipmentNum = 18;
+        Mounted bay = mock(Mounted.class);
+        doReturn(bay).when(entity).getBayByAmmo(eq(bin0));
+        doReturn(bayEquipmentNum).when(entity).getEquipmentNum(eq(bay));
+        when(unit.getEntity()).thenReturn(entity);
+        AmmoBin otherAmmoBin = mock(AmmoBin.class);
+        when(unit.getParts()).thenReturn(Arrays.asList(new Part[] { otherAmmoBin }));
+        // Handle parts being added to units having their unit set
+        doAnswer(inv -> {
+            Part part = inv.getArgument(0);
+            part.setUnit(unit);
+            return null;
+        }).when(unit).addPart(any());
+
+        AdjustLargeCraftAmmoAction action = new AdjustLargeCraftAmmoAction();
+
+        action.execute(campaign, unit);
+
+        ArgumentCaptor<Part> partCaptor = ArgumentCaptor.forClass(Part.class);
+        verify(unit, times(1)).addPart(partCaptor.capture());
+
+        Part addedPart = partCaptor.getValue();
+        verify(quartermaster, times(1)).addPart(eq(addedPart), eq(0));
+
+        assertNotNull(addedPart instanceof LargeCraftAmmoBin);
+
+        LargeCraftAmmoBin ammoBin = (LargeCraftAmmoBin) addedPart;
+        assertEquals(ammoType0, ammoBin.getType());
+        assertEquals(equipmentNum, ammoBin.getEquipmentNum());
+        assertEquals((double) capacity, ammoBin.getCapacity(), 0.001);
+        assertEquals((capacity - onHand) * ammoType0.getShots(), ammoBin.getShotsNeeded());
+        assertEquals(bayEquipmentNum, ammoBin.getBayEqNum());
+        assertEquals(bay, ammoBin.getBay());
+    }
+
+    @Test
+    public void updatesExistingBayToMatchTypeSkipsNonLargeCraftBins() {
+        Campaign campaign = mock(Campaign.class);
+        Quartermaster quartermaster = mock(Quartermaster.class);
+        when(campaign.getQuartermaster()).thenReturn(quartermaster);
+
+        // Put together a unit with 1 bay, 1 ammo type, and 1 bin on the unit already
+        Unit unit = mock(Unit.class);
+        when(unit.getCampaign()).thenReturn(campaign);
+        Entity entity = mock(Entity.class);
+        when(entity.usesWeaponBays()).thenReturn(true);
+        when(entity.getWeight()).thenReturn(500.0);
+        int equipmentNum = 11;
+        Mounted bin0 = mock(Mounted.class);
+        doReturn(equipmentNum).when(entity).getEquipmentNum(eq(bin0));
+        AmmoType ammoType0 = getAmmoType("ISLRM20 Ammo");
+        when(bin0.getType()).thenReturn(ammoType0);
+        ArrayList<Mounted> ammo = new ArrayList<>();
+        ammo.add(bin0);
+        when(entity.getAmmo()).thenReturn(ammo);
+        when(unit.getEntity()).thenReturn(entity);
+        LargeCraftAmmoBin ammoBin = mock(LargeCraftAmmoBin.class);
+        when(ammoBin.getEquipmentNum()).thenReturn(equipmentNum);
+        AmmoBin otherAmmoBin = mock(AmmoBin.class);
+        when(unit.getParts()).thenReturn(Arrays.asList(new Part[] { otherAmmoBin, ammoBin }));
 
         AdjustLargeCraftAmmoAction action = new AdjustLargeCraftAmmoAction();
 
