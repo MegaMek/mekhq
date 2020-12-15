04d16aa4a417374686cc3adcdeff465bcfdb40de
Christopher Watford <christopher.watford@gmail.com>
2020-12-15 17:21:30 -0500

    Add MissingMekLocation LAM tests

---
+++ MekHQ/unittests/mekhq/campaign/parts/MissingMekLocationTest.java
@@ -0,0 +1,131 @@
+package mekhq.campaign.parts;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.util.function.Predicate;
+
+import org.junit.Test;
+
+import megamek.common.CriticalSlot;
+import megamek.common.LandAirMech;
+import megamek.common.Mech;
+import mekhq.campaign.Campaign;
+import mekhq.campaign.unit.Unit;
+
+public class MissingMekLocationTest {
+    @Test
+    public void missingLAMTorsoRepairableOnlyWithMissingAvionicsAndLandingGear() {
+        Campaign mockCampaign = mock(Campaign.class);
+        Unit unit = mock(Unit.class);
+        LandAirMech entity = mock(LandAirMech.class);
+        when(unit.getEntity()).thenReturn(entity);
+        when(entity.getWeight()).thenReturn(30.0);
+        doCallRealMethod().when(entity).getLocationName(any());
+
+        int location = Mech.LOC_RT;
+        MissingMekLocation missing = new MissingMekLocation(location, 30, 0, false, false, false, mockCampaign);
+        missing.setUnit(unit);
+
+        // 2 criticals
+        doReturn(2).when(entity).getNumberOfCriticals(eq(location));
+        CriticalSlot mockLandingGear = mock(CriticalSlot.class);
+        when(mockLandingGear.isEverHittable()).thenReturn(true);
+        when(mockLandingGear.getType()).thenReturn(CriticalSlot.TYPE_SYSTEM);
+        when(mockLandingGear.getIndex()).thenReturn(LandAirMech.LAM_LANDING_GEAR);
+        doReturn(mockLandingGear).when(entity).getCritical(eq(location), eq(0));
+        CriticalSlot mockAvionics = mock(CriticalSlot.class);
+        when(mockAvionics.isEverHittable()).thenReturn(true);
+        when(mockAvionics.getType()).thenReturn(CriticalSlot.TYPE_SYSTEM);
+        when(mockAvionics.getIndex()).thenReturn(LandAirMech.LAM_AVIONICS);
+        doReturn(mockAvionics).when(entity).getCritical(eq(location), eq(1));
+
+        // No missing parts
+        doAnswer(inv -> {
+            return null;
+        }).when(unit).findPart(any());
+
+        // We cannot repair this torso
+        assertNotNull(missing.checkFixable());
+
+        // Only missing landing gear, avionics are still good
+        doAnswer(inv -> {
+            Predicate<Part> predicate = inv.getArgument(0);
+            MissingLandingGear missingLandingGear = mock(MissingLandingGear.class);
+            return predicate.test(missingLandingGear) ? missingLandingGear : null;
+        }).when(unit).findPart(any());
+
+        // We cannot repair this torso
+        assertNotNull(missing.checkFixable());
+
+        // Only missing avionics, landing gear is still good
+        doAnswer(inv -> {
+            Predicate<Part> predicate = inv.getArgument(0);
+            MissingAvionics missingAvionics = mock(MissingAvionics.class);
+            return predicate.test(missingAvionics) ? missingAvionics : null;
+        }).when(unit).findPart(any());
+
+        // We cannot repair this torso
+        assertNotNull(missing.checkFixable());
+
+        // Missing both Landing Gear and Avionics
+        doAnswer(inv -> {
+            Predicate<Part> predicate = inv.getArgument(0);
+            MissingLandingGear missingLandingGear = mock(MissingLandingGear.class);
+            if (predicate.test(missingLandingGear)) {
+                return missingLandingGear;
+            }
+
+            MissingAvionics missingAvionics = mock(MissingAvionics.class);
+            return predicate.test(missingAvionics) ? missingAvionics : null;
+        }).when(unit).findPart(any());
+
+        // We CAN repair this torso
+        assertNull(missing.checkFixable());
+    }
+
+    @Test
+    public void missingLAMHeadRepairableOnlyWithMissingAvionics() {
+        Campaign mockCampaign = mock(Campaign.class);
+        Unit unit = mock(Unit.class);
+        LandAirMech entity = mock(LandAirMech.class);
+        when(unit.getEntity()).thenReturn(entity);
+        when(entity.getWeight()).thenReturn(30.0);
+        doCallRealMethod().when(entity).getLocationName(any());
+
+        int location = Mech.LOC_HEAD;
+        MissingMekLocation missing = new MissingMekLocation(location, 30, 0, false, false, false, mockCampaign);
+        missing.setUnit(unit);
+
+        // 1 critical
+        doReturn(1).when(entity).getNumberOfCriticals(eq(location));
+        CriticalSlot mockAvionics = mock(CriticalSlot.class);
+        when(mockAvionics.isEverHittable()).thenReturn(true);
+        when(mockAvionics.getType()).thenReturn(CriticalSlot.TYPE_SYSTEM);
+        when(mockAvionics.getIndex()).thenReturn(LandAirMech.LAM_AVIONICS);
+        doReturn(mockAvionics).when(entity).getCritical(eq(location), eq(0));
+
+        // No missing parts
+        doAnswer(inv -> {
+            return null;
+        }).when(unit).findPart(any());
+
+        // We cannot repair this head
+        assertNotNull(missing.checkFixable());
+
+        // Missing avionics
+        doAnswer(inv -> {
+            Predicate<Part> predicate = inv.getArgument(0);
+            MissingLandingGear missingLandingGear = mock(MissingLandingGear.class);
+            if (predicate.test(missingLandingGear)) {
+                return missingLandingGear;
+            }
+
+            MissingAvionics missingAvionics = mock(MissingAvionics.class);
+            return predicate.test(missingAvionics) ? missingAvionics : null;
+        }).when(unit).findPart(any());
+
+        // We CAN repair this head
+        assertNull(missing.checkFixable());
+    }
+}
