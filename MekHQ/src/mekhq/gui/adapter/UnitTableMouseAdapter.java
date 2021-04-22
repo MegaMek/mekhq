b847f353732b53aa7a095e6ffe78ac0eead10e39
unknown <a_pokrovski@hotmail.com>
2021-04-21 23:37:32 -0400

    code review change

--- MekHQ/src/mekhq/gui/adapter/UnitTableMouseAdapter.java
+++ MekHQ/src/mekhq/gui/adapter/UnitTableMouseAdapter.java
@@ -880,7 +880,7 @@ public class UnitTableMouseAdapter extends JPopupMenuAdapter {
             
             // if we're using maintenance and have selected something that requires maintenance
             if (gui.getCampaign().getCampaignOptions().checkMaintenance() &&
-                    maintenanceTime > 0) {
+                    (maintenanceTime > 0)) {
                 menuItem = new JMenu("Set Maintenance Extra Time");
                 
                 for (int x = 1; x <= 4; x++) {
