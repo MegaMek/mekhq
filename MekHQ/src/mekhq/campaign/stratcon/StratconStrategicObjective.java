d0421da5db95760abd553541f22f459b04275372
unknown <a_pokrovski@hotmail.com>
2021-05-22 20:58:48 -0400

    cosmetic cleanup

--- MekHQ/src/mekhq/campaign/stratcon/StratconStrategicObjective.java
+++ MekHQ/src/mekhq/campaign/stratcon/StratconStrategicObjective.java
@@ -1,14 +1,32 @@
+/*
+ * Copyright (c) 2019 The Megamek Team. All rights reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package mekhq.campaign.stratcon;
 
 import mekhq.campaign.mission.ScenarioForceTemplate.ForceAlignment;
 import mekhq.campaign.stratcon.StratconContractDefinition.StrategicObjectiveType;
 
+/**
+ * This class is a data structure storing data relating to StratCon strategic objectives
+ * and also handles some small amount of "business logic"
+ */
 public class StratconStrategicObjective {
-    /*public enum StrategicObjectiveState {
-        InProgress,
-        Failed,
-        Succeeded
-    }*/
     
     private StratconCoords objectiveCoords;
     private StrategicObjectiveType objectiveType;
@@ -51,6 +69,9 @@ public class StratconStrategicObjective {
         this.desiredObjectiveCount = desiredObjectiveCount;
     }
 
+    /**
+     * Given the track that this objective is on, is it complete?
+     */
     public boolean isObjectiveCompleted(StratconTrackState trackState) {
         switch (getObjectiveType()) {
             case AnyScenarioVictory:
