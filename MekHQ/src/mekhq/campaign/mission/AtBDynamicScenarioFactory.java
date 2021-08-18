c9874e498381cb73e2966a26b89f90c951e9cc66
unknown <jrbowen24@gmail.com>
2021-08-17 22:32:51 -0400

    Round two of LGTM fix

--- MekHQ/src/mekhq/campaign/mission/AtBDynamicScenarioFactory.java
+++ MekHQ/src/mekhq/campaign/mission/AtBDynamicScenarioFactory.java
@@ -1605,7 +1605,7 @@ public class AtBDynamicScenarioFactory {
             }
         }
 
-        bvBudget += Math.round(bvBudget * scenario.getEffectivePlayerBVMultiplier());
+        bvBudget += (int) Math.round(bvBudget * scenario.getEffectivePlayerBVMultiplier());
 
         // allied bot forces that contribute to BV do not get multiplied by the difficulty
         // even if the player is super good, the AI doesn't get any better
