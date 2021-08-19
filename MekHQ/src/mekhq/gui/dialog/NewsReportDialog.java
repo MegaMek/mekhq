259547b7f0ddaa5df9d21159c35e667ec6a2191b
unknown <jrbowen24@gmail.com>
2021-08-18 21:34:39 -0400

    Moving NewsReportDialog to AbstractDialog

--- MekHQ/src/mekhq/gui/dialog/NewsReportDialog.java
+++ MekHQ/src/mekhq/gui/dialog/NewsReportDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 - The MegaMek Team. All Rights Reserved.
+ * Copyright (c) 2013-2021 - The MegaMek Team. All Rights Reserved.
  *
  * This file is part of MekHQ.
  *
@@ -18,55 +18,49 @@
  */
 package mekhq.gui.dialog;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
+import mekhq.campaign.universe.NewsItem;
+import mekhq.gui.baseComponents.AbstractMHQDialog;
 
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
-import mekhq.MekHQ;
-import mekhq.campaign.universe.NewsItem;
-import megamek.client.ui.preferences.JWindowPreference;
-import megamek.client.ui.preferences.PreferencesNode;
-
-/**
- * @author Jay Lawson
- */
-public class NewsReportDialog extends JDialog {
+public class NewsReportDialog extends AbstractMHQDialog {
+    //region Variable Declarations
     private static final long serialVersionUID = 3624327778807359294L;
 
-    private JTextPane txtNews;
+    private final NewsItem news;
+    //endregion Variable Declarations
 
-    public NewsReportDialog(JFrame parent, NewsItem news) {
-        super(parent, false);
+    //region Constructors
+    public NewsReportDialog(final JFrame frame, final NewsItem news) {
+        super(frame, "NewsReportDialog", "NewsReportDialog.title");
+        this.news = news;
         setTitle(news.getHeadline());
-        initComponents();
-        txtNews.setText(news.getFullDescription());
-        txtNews.setCaretPosition(0);
-        setMinimumSize(new Dimension(500, 300));
-        setPreferredSize(new Dimension(500, 300));
-        setLocationRelativeTo(parent);
-        setUserPreferences();
+        initialize();
     }
+    //endregion Constructors
 
-    private void initComponents() {
-        txtNews = new JTextPane();
+    //region Getters
+    public NewsItem getNews() {
+        return news;
+    }
+    //endregion Getters
+
+    //region Initialization
+    @Override
+    protected Container createCenterPane() {
+        final JTextPane txtNews = new JTextPane();
         txtNews.setContentType("text/html");
+        txtNews.setText(getNews().getFullDescription());
         txtNews.setEditable(false);
-        JScrollPane scrNews = new JScrollPane(txtNews);
-        scrNews.setBorder( new EmptyBorder(2,10,2,2));
+        txtNews.setCaretPosition(0);
 
-        setLayout(new BorderLayout());
+        final JScrollPane scrollPane = new JScrollPane(txtNews);
+        scrollPane.setBorder(new EmptyBorder(2, 10, 2, 2));
+        scrollPane.setName("newsPane");
 
-        txtNews.setEditable(false);
-
-        getContentPane().add(scrNews, BorderLayout.CENTER);
+        return scrollPane;
     }
-
-    private void setUserPreferences() {
-        PreferencesNode preferences = MekHQ.getPreferences().forClass(NewsReportDialog.class);
-
-        this.setName("dialog");
-        preferences.manage(new JWindowPreference(this));
-    }
+    //endregion Initialization
 }
