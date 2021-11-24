6d23bb8d22a4687320027d5cf2d991b5e7cc951c
unknown <jrbowen24@gmail.com>
2021-11-23 20:51:10 -0500

    Creating the RandomRetirementMethod enum

---
+++ MekHQ/src/mekhq/campaign/personnel/enums/RandomRetirementMethod.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) 2021 - The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ. If not, see <http://www.gnu.org/licenses/>.
+ */
+package mekhq.campaign.personnel.enums;
+
+import megamek.common.util.EncodeControl;
+
+import java.util.ResourceBundle;
+
+public enum RandomRetirementMethod {
+    //region Enum Declarations
+    NONE("RandomRetirementMethod.NONE.text", "RandomRetirementMethod.NONE.toolTipText"),
+    AGAINST_THE_BOT("RandomRetirementMethod.AGAINST_THE_BOT.text", "RandomRetirementMethod.AGAINST_THE_BOT.toolTipText"),
+    AGAINST_THE_BOT_YEARLY("RandomRetirementMethod.AGAINST_THE_BOT_YEARLY.text", "RandomRetirementMethod.AGAINST_THE_BOT_YEARLY.toolTipText"),
+    AGAINST_THE_BOT_CONTRACT_END("RandomRetirementMethod.AGAINST_THE_BOT_CONTRACT_END.text", "RandomRetirementMethod.AGAINST_THE_BOT_CONTRACT_END.toolTipText");
+    //endregion Enum Declarations
+
+    //region Variable Declarations
+    private final String name;
+    private final String toolTipText;
+    //endregion Variable Declarations
+
+    //region Constructors
+    RandomRetirementMethod(final String name, final String toolTipText) {
+        final ResourceBundle resources = ResourceBundle.getBundle("mekhq.resources.Personnel", new EncodeControl());
+        this.name = resources.getString(name);
+        this.toolTipText = resources.getString(toolTipText);
+    }
+    //endregion Constructors
+
+    //region Getters
+    public String getToolTipText() {
+        return toolTipText;
+    }
+    //endregion Getters
+
+    //region Boolean Comparisons
+    public boolean isNone() {
+        return this == NONE;
+    }
+
+    public boolean isAtB() {
+        return this == AGAINST_THE_BOT;
+    }
+
+    public boolean isAtBYearly() {
+        return this == AGAINST_THE_BOT_YEARLY;
+    }
+
+    public boolean isAtBContractEnd() {
+        return this == AGAINST_THE_BOT_CONTRACT_END;
+    }
+    //endregion Boolean Comparisons
+
+/*
+    public AbstractRetirement getMethod(final CampaignOptions options) {
+        switch (this) {
+            case AGAINST_THE_BOT:
+                return new AtBRandomRetirement(options);
+            case AGAINST_THE_BOT_YEARLY:
+                return new AtBYearlyRandomRetirement(options);
+            case AGAINST_THE_BOT_CONTRACT_END:
+                return new AtBContractEndRandomRetirement(options);
+            case NONE:
+            default:
+                return new DisabledRandomRetirement(options);
+        }
+    }
+*/
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
