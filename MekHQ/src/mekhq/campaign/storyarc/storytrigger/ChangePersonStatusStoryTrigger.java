aa2083d22b7f3433479f494b4cf5a866f61e3c38
Aaron Gullickson <aarongullickson@gmail.com>
2022-01-22 15:46:34 -0800

    Add ChangePersonStatusStoryTrigger and related Young Wolves story arc elements

---
+++ MekHQ/src/mekhq/campaign/storyarc/storytrigger/ChangePersonStatusStoryTrigger.java
@@ -0,0 +1,80 @@
+/*
+ * ChangePersonStatusStoryTrigger.java
+ *
+ * Copyright (c) 2021 - The MegaMek Team. All Rights Reserved
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package mekhq.campaign.storyarc.storytrigger;
+
+import mekhq.MekHqXmlSerializable;
+import mekhq.MekHqXmlUtil;
+import mekhq.campaign.Campaign;
+import mekhq.campaign.personnel.Person;
+import mekhq.campaign.personnel.enums.PersonnelStatus;
+import mekhq.campaign.storyarc.StoryTrigger;
+import org.apache.logging.log4j.LogManager;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.PrintWriter;
+import java.io.Serializable;
+import java.text.ParseException;
+import java.util.UUID;
+
+/**
+ * A StoryTrigger that will change a given Person's status
+ */
+public class ChangePersonStatusStoryTrigger extends StoryTrigger implements Serializable, MekHqXmlSerializable {
+
+    private UUID personId;
+    private PersonnelStatus status;
+
+    @Override
+    protected void execute() {
+        Person p = getCampaign().getPerson(personId);
+        if(null != p) {
+            p.changeStatus(getCampaign(), getCampaign().getLocalDate(), status);
+        }
+    }
+
+    @Override
+    public void writeToXml(PrintWriter pw1, int indent) {
+        writeToXmlBegin(pw1, indent++);
+        MekHqXmlUtil.writeSimpleXMLTag(pw1, indent, "personId", personId);
+        MekHqXmlUtil.writeSimpleXMLTag(pw1, indent, "status", status.name());
+        writeToXmlEnd(pw1, --indent);
+    }
+
+    @Override
+    protected void loadFieldsFromXmlNode(Node wn, Campaign c) throws ParseException {
+        NodeList nl = wn.getChildNodes();
+
+        for (int x = 0; x < nl.getLength(); x++) {
+            Node wn2 = nl.item(x);
+
+            try {
+                if (wn2.getNodeName().equalsIgnoreCase("personId")) {
+                    personId = UUID.fromString(wn2.getTextContent().trim());
+                } else if (wn2.getNodeName().equalsIgnoreCase("status")) {
+                    status = PersonnelStatus.parseFromString(wn2.getTextContent().trim());
+                }
+            } catch (Exception e) {
+                LogManager.getLogger().error(e);
+            }
+        }
+    }
+}
