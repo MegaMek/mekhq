1ec65839574c652bdf0d7cf8b4f0d65f849cdb33
unknown <jrbowen24@gmail.com>
2022-05-14 12:36:48 -0400

    MekHqXmlUtil: AeroSensor Swapover

--- MekHQ/src/mekhq/campaign/parts/AeroSensor.java
+++ MekHQ/src/mekhq/campaign/parts/AeroSensor.java
@@ -208,13 +208,10 @@ public class AeroSensor extends Part {
     }
 
     @Override
-    public void writeToXML(PrintWriter pw1, int indent) {
-        writeToXmlBegin(pw1, indent);
-        pw1.println(MekHqXmlUtil.indentStr(indent+1)
-                +"<dropship>"
-                +largeCraft
-                +"</dropship>");
-        writeToXmlEnd(pw1, indent);
+    public void writeToXML(final PrintWriter pw, int indent) {
+        writeToXmlBegin(pw, indent++);
+        MekHqXmlUtil.writeSimpleXMLTag(pw, indent, "dropship", largeCraft);
+        writeToXmlEnd(pw, --indent);
     }
 
     @Override
