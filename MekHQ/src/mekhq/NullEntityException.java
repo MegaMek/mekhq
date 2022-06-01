1ac93d8821cda95d5c9f944658317945e2fb3a86
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-01 14:45:46 +0800

    Delete MekHQ directory

--- MekHQ/src/mekhq/NullEntityException.java
+++
@@ -1,7 +0,0 @@
-package mekhq;
-
-public class NullEntityException extends Exception {
-    public NullEntityException(final String message) {
-        super(message);
-    }
-}
--- MekHQ/src/mekhq/campaign/mission/atb/AtBScenarioEnabled.java
+++
@@ -1,9 +0,0 @@
-package mekhq.campaign.mission.atb;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface AtBScenarioEnabled {
-
-}
