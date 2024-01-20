3fc49e6876ec4ed040bf83437b0c60ceeb0ea12d
kuronekochomusuke <kuronekochomusuke@gmail.com>
2024-01-20 14:10:18 -0500

    in repair bay unit list show if unit is in repair or salvage mode

--- MekHQ/src/mekhq/gui/model/UnitTableModel.java
+++ MekHQ/src/mekhq/gui/model/UnitTableModel.java
@@ -124,7 +124,7 @@ public class UnitTableModel extends DataTableModel {
             case COL_QUIRKS:
                 return "Quirks";
             case COL_RSTATUS:
-                return "Repair Status";
+                return "Mode";
             default:
                 return "?";
         }
@@ -145,7 +145,7 @@ public class UnitTableModel extends DataTableModel {
             case COL_COST:
             case COL_STATUS:
             case COL_RSTATUS:
-                return 80;
+                return 40;
             default:
                 return 20;
         }
