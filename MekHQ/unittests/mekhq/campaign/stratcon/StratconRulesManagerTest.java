da8e5f9dc3db3c7bc5f73059f0d76fe7997c8ad9
IllianiCBT <danielfarbrace@icloud.com>
2024-03-18 14:32:07 -0500

    Added test for processScenarioCompletion

---
+++ MekHQ/unittests/mekhq/campaign/stratcon/StratconRulesManagerTest.java
@@ -0,0 +1,12 @@
+package mekhq.campaign.stratcon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StratconRulesManagerTest {
+
+  @Test
+  void processScenarioCompletion() {
+  }
+}
