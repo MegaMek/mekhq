09c925c241b595b3556d4f25a1941182b7baa93a
IllianiCBT <danielfarbrace@icloud.com>
2024-03-19 13:34:46 -0500

    Closes #3730

--- MekHQ/src/mekhq/campaign/stratcon/StratconRulesManager.java
+++ MekHQ/src/mekhq/campaign/stratcon/StratconRulesManager.java
@@ -65,7 +65,7 @@ public class StratconRulesManager {
         ChainedScenario,
 
         /**
-         * We pay a support point or convert a victory point to a support point
+         * We pay a support point or convert a Campaign Victory Point to a support point
          */
         SupportPoint,
 
@@ -192,7 +192,7 @@ public class StratconRulesManager {
                 backingScenario.getTerrainType() != Scenario.TER_SPACE) {
             backingScenario.setTemperature(track.getTemperature());
         }
-        
+
         // for now, if we're using a fixed map or in a facility, don't replace the scenario
         // TODO: facility spaces will always have a relevant biome
         if (backingScenario.isUsingFixedMap()) {
@@ -201,12 +201,12 @@ public class StratconRulesManager {
 
         StratconFacility facility = track.getFacility(scenario.getCoords());
         String terrainType;
-        
+
         // facilities have their own terrain lists
         if (facility != null) {
             int kelvinTemp = track.getTemperature() + StratconContractInitializer.ZERO_CELSIUS_IN_KELVIN;
             StratconBiome facilityBiome;
-            
+
             // if facility doesn't have a biome temp map or no entry for the current temperature, use the default one
             if (facility.getBiomes().isEmpty() || (facility.getBiomeTempMap().floorEntry(kelvinTemp) == null)) {
                 facilityBiome = StratconBiomeManifest.getInstance().getTempMap(StratconBiomeManifest.TERRAN_FACILITY_BIOME)
@@ -218,7 +218,7 @@ public class StratconRulesManager {
         } else {
             terrainType = track.getTerrainTile(coords);
         }
-        
+
         var mapTypes = StratconBiomeManifest.getInstance().getBiomeMapTypes();
 
         // don't have a map list for the given terrain, leave it alone
