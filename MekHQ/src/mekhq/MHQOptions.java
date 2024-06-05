dfc3ad936fe782cd139033d06060ebb845c5c9eb
IllianiCBT <danielfarbrace@icloud.com>
2024-06-05 15:44:34 -0500

    feat: rolled back changing font color from red to orange

--- MekHQ/src/mekhq/MHQOptions.java
+++ MekHQ/src/mekhq/MHQOptions.java
@@ -463,12 +463,26 @@ public final class MHQOptions extends SuiteOptions {
         return new Color(userPreferences.node(MHQConstants.DISPLAY_NODE).getInt(MHQConstants.FONT_COLOR_NEGATIVE, Color.RED.getRGB()));
     }
 
+    /**
+     * @return the hexadecimal color code for the negative event font color.
+     */
+    public String getFontColorNegativeHexColor() {
+        return convertFontColorToHexColor(getFontColorNegative());
+    }
+
     public void setFontColorNegative(Color value) {
         userPreferences.node(MHQConstants.DISPLAY_NODE).putInt(MHQConstants.FONT_COLOR_NEGATIVE, value.getRGB());
     }
 
     public Color getFontColorPositive() {
         return new Color(userPreferences.node(MHQConstants.DISPLAY_NODE).getInt(MHQConstants.FONT_COLOR_POSITIVE, Color.GREEN.getRGB()));
+    }
+
+    /**
+     * @return the hexadecimal color code for the positive event font color.
+     */
+    public String getFontColorPositiveHexColor() {
+        return convertFontColorToHexColor(getFontColorPositive());
     }
 
     public void setFontColorPositive(Color value) {
@@ -479,8 +493,29 @@ public final class MHQOptions extends SuiteOptions {
         return new Color(userPreferences.node(MHQConstants.DISPLAY_NODE).getInt(MHQConstants.FONT_COLOR_WARNING, Color.ORANGE.getRGB()));
     }
 
+    /**
+     * @return the hexadecimal color code for the warning event font color.
+     */
+    public String getFontColorWarningHexColor() {
+        return convertFontColorToHexColor(getFontColorWarning());
+    }
+
     public void setFontColorWarning(Color value) {
         userPreferences.node(MHQConstants.DISPLAY_NODE).putInt(MHQConstants.FONT_COLOR_WARNING, value.getRGB());
+    }
+
+    /**
+     * Converts the font color to a hexadecimal color representation.
+     *
+     * @param color the font color to convert
+     * @return the hexadecimal color representation of the font color
+     */
+    public String convertFontColorToHexColor(Color color) {
+        int red = color.getRed();
+        int green = color.getGreen();
+        int blue = color.getBlue();
+
+        return String.format("#%02x%02x%02x", red, green, blue);
     }
     //endregion Colours
 
