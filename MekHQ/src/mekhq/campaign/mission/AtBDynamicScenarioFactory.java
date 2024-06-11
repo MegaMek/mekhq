8ee8d69ffbb877a628191d83df02b6edc68c3f9e
SuperStucco <SuperStucco>
2024-06-10 20:08:03 -0600

    Minor code standards update

--- MekHQ/src/mekhq/campaign/mission/AtBDynamicScenarioFactory.java
+++ MekHQ/src/mekhq/campaign/mission/AtBDynamicScenarioFactory.java
@@ -1580,7 +1580,7 @@ public class AtBDynamicScenarioFactory {
         UnitGeneratorParameters newParams = params.clone();
         newParams.setUnitType(UnitType.INFANTRY);
         MechSummary unitData;
-        boolean temporaryXCT =false;
+        boolean temporaryXCT = false;
         UnitGeneratorParameters noXCTParams;
         Entity crewedPlatoon;
 
