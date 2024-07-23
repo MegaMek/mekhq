83a3809ca9ca34c9494c8eec70102b424815bebf
IllianiCBT <danielfarbrace@icloud.com>
2024-07-23 00:31:45 -0500

    Added Intelligence attribute to Personnel
    Added a new attribute, Intelligence, to the Personnel object. This attribute has been populated using a random roll and adds further personality details to each person. The Intelligence attribute was also integrated into the personality description.

--- MekHQ/src/mekhq/campaign/personnel/Person.java
+++ MekHQ/src/mekhq/campaign/personnel/Person.java
@@ -217,6 +217,7 @@ public class Person {
     private Greed greed;
     private Social social;
     private PersonalityQuirk personalityQuirk;
+    private Intelligence intelligence;
     private String personalityDescription;
     //endregion Personality
 
@@ -375,6 +376,7 @@ public class Person {
         greed = Greed.NONE;
         social = Social.NONE;
         personalityQuirk = PersonalityQuirk.NONE;
+        intelligence = Intelligence.AVERAGE;
         personalityDescription = "";
 
         //region Flags
@@ -1672,6 +1674,14 @@ public class Person {
         this.personalityQuirk = personalityQuirk;
     }
 
+    public Intelligence getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(final Intelligence intelligence) {
+        this.intelligence = intelligence;
+    }
+
     public String getPersonalityDescription() {
         return personalityDescription;
     }
@@ -2029,6 +2039,10 @@ public class Person {
                 MHQXMLUtility.writeSimpleXMLTag(pw, indent, "personalityQuirk", personalityQuirk.toString());
             }
 
+            if (intelligence != Intelligence.AVERAGE) {
+                MHQXMLUtility.writeSimpleXMLTag(pw, indent, "intelligence", intelligence.toString());
+            }
+
             if (!StringUtility.isNullOrBlank(personalityDescription)) {
                 MHQXMLUtility.writeSimpleXMLTag(pw, indent, "personalityDescription", personalityDescription);
             }
@@ -2369,6 +2383,8 @@ public class Person {
                     retVal.social = Social.parseFromString(wn2.getTextContent());
                 } else if (wn2.getNodeName().equalsIgnoreCase("personalityQuirk")) {
                     retVal.personalityQuirk = PersonalityQuirk.parseFromString(wn2.getTextContent());
+                } else if (wn2.getNodeName().equalsIgnoreCase("intelligence")) {
+                    retVal.intelligence = Intelligence.parseFromString(wn2.getTextContent());
                 } else if (wn2.getNodeName().equalsIgnoreCase("personalityDescription")) {
                     retVal.personalityDescription = wn2.getTextContent();
                 } else if (wn2.getNodeName().equalsIgnoreCase("clanPersonnel")
