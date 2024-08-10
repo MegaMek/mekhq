b17b4b57e49605f2c01635a0bdb20774d8b4c5bc
IllianiCBT <danielfarbrace@icloud.com>
2024-08-09 20:59:57 -0500

    Add CommandRating and integrate with ReputationController
    Added a new CommandRating class to calculate commander ratings based on skills and personality traits. Integrated this functionality into the ReputationController, storing the calculated ratings in a map and updating the total commander rating.

---
+++ MekHQ/src/mekhq/campaign/rating/CamOpsRatingV2/CommandRating.java
@@ -0,0 +1,103 @@
+package mekhq.campaign.rating.CamOpsRatingV2;
+
+import mekhq.campaign.Campaign;
+import mekhq.campaign.personnel.Person;
+import mekhq.campaign.personnel.SkillType;
+import mekhq.campaign.personnel.enums.randomEvents.personalities.Aggression;
+import mekhq.campaign.personnel.enums.randomEvents.personalities.Ambition;
+import mekhq.campaign.personnel.enums.randomEvents.personalities.Greed;
+import mekhq.campaign.personnel.enums.randomEvents.personalities.Social;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandRating {
+    /**
+     * Calculates the rating of a commander based on their skills and personality.
+     *
+     * @param campaign   the campaign the commander belongs to
+     * @param commander  the commander to calculate the rating for
+     * @return a map containing the commander's rating in different areas:
+     *         - "leadership": the commander's leadership skill value
+     *         - "tactics": the commander's tactics skill value
+     *         - "strategy": the commander's strategy skill value
+     *         - "negotiation": the commander's negotiation skill value
+     *         - "traits": the commander's traits (not currently tracked, always 0)
+     *         - "personality": the value of the commander's personality characteristics (or 0, if disabled)
+     */
+    protected static Map<String, Integer> calculateCommanderRating(Campaign campaign, Person commander) {
+        Map<String, Integer> commanderRating = new HashMap<>();
+
+        commanderRating.put("leadership", getSkillValue(commander, SkillType.S_LEADER));
+        commanderRating.put("tactics", getSkillValue(commander, SkillType.S_TACTICS));
+        commanderRating.put("strategy", getSkillValue(commander, SkillType.S_STRATEGY));
+        commanderRating.put("negotiation", getSkillValue(commander, SkillType.S_NEG));
+
+        // ATOW traits are not currently tracked by mhq, but when they are, this is where we'd add that data
+        commanderRating.put("traits", 0);
+
+        // this will return 0 if personalities are disabled
+        commanderRating.put("personality", getPersonalityValue(campaign, commander));
+
+        commanderRating.put("total", commanderRating.values().stream().mapToInt(rating -> rating).sum());
+
+        return commanderRating;
+    }
+
+    /**
+     * @return the final skill value for the given skill,
+     * or 0 if the person does not have the skill
+     *
+     * @param person the person
+     * @param skill the skill
+     */
+    private static int getSkillValue(Person person, String skill) {
+        if (person.hasSkill(skill)) {
+            return person.getSkill(skill).getFinalSkillValue();
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Calculates the total value of a person's personality characteristics.
+     *
+     * @param campaign the current campaign
+     * @param person the person to calculate the personality value for
+     * @return the total personality value of the person in the campaign
+     */
+    private static int getPersonalityValue(Campaign campaign, Person person) {
+        if (campaign.getCampaignOptions().isUseRandomPersonalities()) {
+            int personalityValue = 0;
+            int modifier;
+
+            Aggression aggression = person.getAggression();
+            if (!person.getAggression().isNone()) {
+                modifier = aggression.isTraitPositive() ? 1 : -1;
+                personalityValue += aggression.isTraitMajor() ? modifier * 2 : modifier;
+            }
+
+            Ambition ambition = person.getAmbition();
+            if (!person.getAmbition().isNone()) {
+                modifier = ambition.isTraitPositive() ? 1 : -1;
+                personalityValue += ambition.isTraitMajor() ? modifier * 2 : modifier;
+            }
+
+            Greed greed = person.getGreed();
+            if (!person.getGreed().isNone()) {
+                modifier = greed.isTraitPositive() ? 1 : -1;
+                personalityValue += greed.isTraitMajor() ? modifier * 2 : modifier;
+            }
+
+            Social social = person.getSocial();
+            if (!person.getSocial().isNone()) {
+                modifier = social.isTraitPositive() ? 1 : -1;
+                personalityValue += social.isTraitMajor() ? modifier * 2 : modifier;
+            }
+
+            return personalityValue;
+        } else {
+            return 0;
+        }
+    }
+}
--- MekHQ/src/mekhq/campaign/rating/CamOpsRatingV2/ReputationController.java
+++ MekHQ/src/mekhq/campaign/rating/CamOpsRatingV2/ReputationController.java
@@ -3,9 +3,18 @@ package mekhq.campaign.rating.CamOpsRatingV2;
 import megamek.common.enums.SkillLevel;
 import mekhq.campaign.Campaign;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReputationController {
+    // average experience rating
     private SkillLevel averageSkillLevel = SkillLevel.NONE;
     private int averageExperienceRating = 0;
+
+    // command rating
+    private Map<String, Integer> commanderMap = new HashMap<>();
+    private int commanderRating = 0;
+
 
     public void initializeReputation(Campaign campaign) {
         int reputation = 0;
@@ -14,7 +23,10 @@ public class ReputationController {
         averageSkillLevel = AverageExperienceRating.getSkillLevel(campaign);
         averageExperienceRating = AverageExperienceRating.getReputationModifier(averageSkillLevel);
 
-        // step two:
+        // step two: calculate command rating
+        // TODO add a campaign option to disable personality rating
+        commanderMap = CommandRating.calculateCommanderRating(campaign, campaign.getFlaggedCommander());
+        commanderRating = commanderMap.get("total");
 
     }
 }
