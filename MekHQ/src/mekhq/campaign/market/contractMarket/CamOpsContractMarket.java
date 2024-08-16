629860753e985b05571bfdedcfc671d7c591bfc8
algebro <algebro@tuta.io>
2024-08-16 12:53:27 -0400

    WIP: implement camopscontractmarket

--- MekHQ/src/mekhq/campaign/market/contractMarket/CamOpsContractMarket.java
+++ MekHQ/src/mekhq/campaign/market/contractMarket/CamOpsContractMarket.java
@@ -3,6 +3,9 @@ package mekhq.campaign.market.contractMarket;
 import mekhq.campaign.Campaign;
 import mekhq.campaign.market.enums.ContractMarketMethod;
 import mekhq.campaign.mission.AtBContract;
+import mekhq.campaign.personnel.Person;
+import mekhq.campaign.personnel.SkillType;
+import mekhq.campaign.universe.PlanetarySystem;
 
 public class CamOpsContractMarket extends AbstractContractMarket {
     public CamOpsContractMarket() {
@@ -15,7 +18,17 @@ public class CamOpsContractMarket extends AbstractContractMarket {
 
     @Override
     public void generateContractOffers(Campaign campaign, boolean newCampaign) {
-
+        if (!(campaign.getLocalDate().getDayOfMonth() == 1) && !newCampaign) {
+            return;
+        }
+        int repFactor = campaign.getReputationFactor();
+        // TODO: Allow subcontracts?
+        for (AtBContract contract : campaign.getActiveAtBContracts()) {
+            //checkForSubcontracts(campaign, contract, unitRatingMod);
+        }
+        // TODO: CamopsMarket: allow players to choose negotiators and send them out, removing them
+        // from other tasks they're doing. For now just use the highest negotiation skill on the force.
+        Person negotiator = campaign.findBestAtSkill(SkillType.S_NEG);
     }
 
     @Override
@@ -26,5 +39,9 @@ public class CamOpsContractMarket extends AbstractContractMarket {
     @Override
     protected void setAtBContractClauses(AtBContract contract, int unitRatingMod, Campaign campaign) {
 
+    }
+
+    private int getHiringHallMod(PlanetarySystem system) {
+        return 0;
     }
 }
