e99b243579a22ad9da9e6470107b7c72ee3d4967
algebro <algebro@tuta.io>
2024-10-04 10:30:01 -0400

    refactor to switch blocks and add legal statement

--- MekHQ/src/mekhq/campaign/market/contractMarket/ContractTerms.java
+++ MekHQ/src/mekhq/campaign/market/contractMarket/ContractTerms.java
@@ -1,5 +1,24 @@
+/*
+ * Copyright (c) 2024 - The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ. If not, see <http://www.gnu.org/licenses/>.
+ */
 package mekhq.campaign.market.contractMarket;
 
+import megamek.codeUtilities.MathUtility;
 import mekhq.campaign.mission.enums.AtBContractType;
 import mekhq.campaign.mission.enums.ContractCommandRights;
 import mekhq.campaign.universe.Faction;
@@ -59,46 +78,32 @@ public class ContractTerms {
 
     public int getSalvagePercentage(int roll) {
         roll += salvageModifier;
-        if (roll < 4) {
-            return 0;
-        } else if (roll == 4) {
-            return 10;
-        } else if (roll == 5) {
-            return 20;
-        } else if (roll == 6) {
-            return 30;
-        } else if (roll == 7) {
-            return 40;
-        } else if (roll == 8) {
-            return 50;
-        } else if (roll == 9) {
-            return 60;
-        } else if (roll == 10) {
-            return 70;
-        } else if (roll == 11) {
-            return 80;
-        } else if (roll == 12) {
-            return 90;
-        } else {
-            return 100;
-        }
+        return switch (MathUtility.clamp(roll, 3, 13)) {
+            case 3 -> 0;
+            case 4 -> 10;
+            case 5 -> 20;
+            case 6 -> 30;
+            case 7 -> 40;
+            case 8 -> 50;
+            case 9 -> 60;
+            case 10 -> 70;
+            case 11 -> 80;
+            case 12 -> 90;
+            default -> 100;
+        };
     }
 
     public int getSupportPercentage(int roll) {
         roll += supportModifier;
-        int percentage = 0;
-        if (roll == 3 || roll == 9) {
-            percentage = 20;
-        } else if (roll == 4 || roll == 10) {
-            percentage = 40;
-        } else if (roll == 5 || roll == 11) {
-            percentage = 60;
-        } else if (roll == 6 || roll == 12) {
-            percentage = 80;
-        } else if (roll == 7 || roll > 12) {
-            percentage = 100;
-        }
-        return percentage;
+        return switch(MathUtility.clamp(roll, 2, 13)) {
+            case 2 -> 0;
+            case 3, 9 -> 20;
+            case 4, 10 -> 40;
+            case 5, 11 -> 60;
+            case 6, 12 -> 80;
+            case 8 -> 10;
+            default -> 100;
+        };
     }
 
     public boolean isStraightSupport(int roll) {
@@ -113,27 +118,18 @@ public class ContractTerms {
 
     public int getTransportTerms(int roll) {
         roll += transportModifier;
-        if (roll < 2) {
-            return 0;
-        } else if (roll == 2) {
-            return 20;
-        } else if (roll == 3) {
-            return 25;
-        } else if (roll == 4) {
-            return 30;
-        } else if (roll == 5) {
-            return 35;
-        } else if (roll == 6) {
-            return 45;
-        } else if (roll == 7) {
-            return 50;
-        } else if (roll == 8) {
-            return 55;
-        } else if (roll == 9) {
-            return 60;
-        } else {
-            return 100;
-        }
+        return switch (MathUtility.clamp(roll, 1, 10)) {
+            case 1 -> 0;
+            case 2 -> 20;
+            case 3 -> 25;
+            case 4 -> 30;
+            case 5 -> 35;
+            case 6 -> 45;
+            case 7 -> 50;
+            case 8 -> 55;
+            case 9 -> 60;
+            default -> 100;
+        };
     }
 
     private void addMissionTypeModifiers(AtBContractType mission) {
@@ -250,41 +246,53 @@ public class ContractTerms {
     }
 
     private void addUnitReputationModifiers(int reputationFactor) {
-        if (reputationFactor <= 0) {
-            commandModifier += -2;
-            salvageModifier += -1;
-            supportModifier += -1;
-            transportModifier += -3;
-        } else if (reputationFactor == 1) {
-            commandModifier += -1;
-            salvageModifier += -1;
-            supportModifier += -1;
-            transportModifier += -2;
-        } else if (reputationFactor == 2) {
-            commandModifier += -1;
-            transportModifier += -2;
-        } else if (reputationFactor == 3) {
-            commandModifier += -1;
-            transportModifier += -1;
-        } else if (reputationFactor == 4) {
-            transportModifier += -1;
-        } else if (reputationFactor == 6 || reputationFactor == 7) {
-            commandModifier += 1;
-            salvageModifier += 1;
-        } else if (reputationFactor == 8) {
-            commandModifier += 1;
-            salvageModifier += 1;
-            supportModifier += 1;
-        } else if (reputationFactor == 9) {
-            commandModifier += 2;
-            salvageModifier += 2;
-            supportModifier += 1;
-            transportModifier += 1;
-        } else {
-            commandModifier += 3;
-            salvageModifier += 2;
-            supportModifier += 2;
-            transportModifier += 2;
+        switch(MathUtility.clamp(reputationFactor, 0, 10)) {
+            case 0:
+                commandModifier += -2;
+                salvageModifier += -1;
+                supportModifier += -1;
+                transportModifier += -3;
+                break;
+            case 1:
+                commandModifier += -1;
+                salvageModifier += -1;
+                supportModifier += -1;
+                transportModifier += -2;
+                break;
+            case 2:
+                commandModifier += -1;
+                transportModifier += -2;
+                break;
+            case 3:
+                commandModifier += -1;
+                transportModifier += -1;
+                break;
+            case 4:
+                transportModifier += -1;
+                break;
+            case 6:
+            case 7:
+                commandModifier += 1;
+                salvageModifier += 1;
+                break;
+            case 8:
+                commandModifier += 1;
+                salvageModifier += 1;
+                supportModifier += 1;
+                break;
+            case 9:
+                commandModifier += 2;
+                salvageModifier += 2;
+                supportModifier += 1;
+                transportModifier += 1;
+                break;
+            default:
+                commandModifier += 3;
+                salvageModifier += 2;
+                supportModifier += 2;
+                transportModifier += 2;
+                break;
+
         }
     }
 }
