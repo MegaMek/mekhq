3ce82e9c606d0961372dd7c1da3722cf9e20eab7
IllianiCBT <danielfarbrace@icloud.com>
2024-10-07 16:00:50 -0500

    Add tabs for Administrators and Awards in PersonnelTab
    Implemented new tabs within PersonnelTab for administrators and awards. Added respective UI elements and layout configurations to support negotiation, scrounge, and award options.

--- MekHQ/src/mekhq/gui/panes/campaignOptions/tabs/PersonnelTab.java
+++ MekHQ/src/mekhq/gui/panes/campaignOptions/tabs/PersonnelTab.java
@@ -1,11 +1,14 @@
 package mekhq.gui.panes.campaignOptions.tabs;
 
 import megamek.client.ui.baseComponents.MMComboBox;
+import mekhq.campaign.personnel.enums.AwardBonus;
 import mekhq.campaign.personnel.enums.TimeInDisplayFormat;
 
 import javax.swing.*;
 import javax.swing.GroupLayout.Alignment;
+import java.awt.*;
 
+import static megamek.client.ui.WrapLayout.wordWrap;
 import static mekhq.gui.panes.campaignOptions.tabs.CampaignOptionsUtilities.*;
 
 public class PersonnelTab {
@@ -48,11 +51,39 @@ public class PersonnelTab {
     private JCheckBox chkShowOriginFaction;
     //end Personnel Information Tab
 
-    //start Awards Tab
-    //end Awards Tab
-
     //start Administrators Tab
+    private JCheckBox chkAdminsHaveNegotiation;
+    private JCheckBox chkAdminExperienceLevelIncludeNegotiation;
+    private JCheckBox chkAdminsHaveScrounge;
+    private JCheckBox chkAdminExperienceLevelIncludeScrounge;
     //end Administrators Tab
+
+    //start Awards Tab
+    private JLabel lblAwardBonusStyle;
+    private MMComboBox<AwardBonus> comboAwardBonusStyle;
+    private JLabel lblAwardTierSize;
+    private JSpinner spnAwardTierSize;
+    private JCheckBox chkEnableAutoAwards;
+    private JCheckBox chkIssuePosthumousAwards;
+    private JCheckBox chkIssueBestAwardOnly;
+    private JCheckBox chkIgnoreStandardSet;
+
+    private JPanel autoAwardsFilterPanel;
+    private JCheckBox chkEnableContractAwards;
+    private JCheckBox chkEnableFactionHunterAwards;
+    private JCheckBox chkEnableInjuryAwards;
+    private JCheckBox chkEnableIndividualKillAwards;
+    private JCheckBox chkEnableFormationKillAwards;
+    private JCheckBox chkEnableRankAwards;
+    private JCheckBox chkEnableScenarioAwards;
+    private JCheckBox chkEnableSkillAwards;
+    private JCheckBox chkEnableTheatreOfWarAwards;
+    private JCheckBox chkEnableTimeAwards;
+    private JCheckBox chkEnableTrainingAwards;
+    private JCheckBox chkEnableMiscAwards;
+    private JLabel lblAwardSetFilterList;
+    private JTextArea txtAwardSetFilterList;
+    //end Awards Tab
 
     //start Medical Tab
     //end Medical Tab
@@ -98,11 +129,11 @@ public class PersonnelTab {
         chkDisplayKillRecord = new JCheckBox();
         //end Personnel Logs Tab
 
-        //start Awards Tab
+        //start Personnel Information Tab
         chkUseTimeInService = new JCheckBox();
         lblTimeInServiceDisplayFormat = new JLabel();
         comboTimeInServiceDisplayFormat = new MMComboBox<>("comboTimeInServiceDisplayFormat",
-        TimeInDisplayFormat.values());
+            TimeInDisplayFormat.values());
         chkUseTimeInRank = new JCheckBox();
         lblTimeInRankDisplayFormat = new JLabel();
         comboTimeInRankDisplayFormat = new MMComboBox<>("comboTimeInRankDisplayFormat",
@@ -110,13 +141,39 @@ public class PersonnelTab {
         chkTrackTotalEarnings = new JCheckBox();
         chkTrackTotalXPEarnings = new JCheckBox();
         chkShowOriginFaction = new JCheckBox();
-        //end Awards Tab
-
-        //start Personnel Information Tab
         //end Personnel Information Tab
 
         //start Administrators Tab
+        chkAdminsHaveNegotiation = new JCheckBox();
+        chkAdminExperienceLevelIncludeNegotiation = new JCheckBox();
+        chkAdminsHaveScrounge = new JCheckBox();
+        chkAdminExperienceLevelIncludeScrounge = new JCheckBox();
         //end Administrators Tab
+
+        //start Awards Tab
+        lblAwardBonusStyle = new JLabel();
+        comboAwardBonusStyle = new MMComboBox<>("comboAwardBonusStyle", AwardBonus.values());
+        lblAwardTierSize = new JLabel();
+        spnAwardTierSize = new JSpinner();
+        chkEnableAutoAwards = new JCheckBox();
+        chkIssuePosthumousAwards = new JCheckBox();
+        chkIssueBestAwardOnly = new JCheckBox();
+        chkIgnoreStandardSet = new JCheckBox();
+        chkEnableContractAwards = new JCheckBox();
+        chkEnableFactionHunterAwards = new JCheckBox();
+        chkEnableInjuryAwards = new JCheckBox();
+        chkEnableIndividualKillAwards = new JCheckBox();
+        chkEnableFormationKillAwards = new JCheckBox();
+        chkEnableRankAwards = new JCheckBox();
+        chkEnableScenarioAwards = new JCheckBox();
+        chkEnableSkillAwards = new JCheckBox();
+        chkEnableTheatreOfWarAwards = new JCheckBox();
+        chkEnableTimeAwards = new JCheckBox();
+        chkEnableTrainingAwards = new JCheckBox();
+        chkEnableMiscAwards = new JCheckBox();
+        lblAwardSetFilterList = new JLabel();
+        txtAwardSetFilterList = new JTextArea();
+        //end Awards Tab
 
         //start Medical Tab
         //end Medical Tab
@@ -286,5 +343,206 @@ public class PersonnelTab {
 
         // Create Parent Panel and return
         return createParentPanel(panel, "PersonnelInformation");
+    }
+
+    public JPanel createAdministratorsTab() {
+        // Header
+        JPanel headerPanel = createHeaderPanel("AdministratorsTab",
+            getImageDirectory() + "logo_clan_diamond_sharks.png",
+            false, "", true);
+
+        // Contents
+        chkAdminsHaveNegotiation = createCheckBox("AdminsHaveNegotiation", null);
+        chkAdminExperienceLevelIncludeNegotiation = createCheckBox("AdminExperienceLevelIncludeNegotiation", null);
+        chkAdminsHaveScrounge = createCheckBox("AdminsHaveScrounge", null);
+        chkAdminExperienceLevelIncludeScrounge = createCheckBox("AdminExperienceLevelIncludeScrounge", null);
+
+        // Layout the Panel
+        final JPanel panel = createStandardPanel("AdministratorsTab", true, "");
+        final GroupLayout layout = createStandardLayout(panel);
+        panel.setLayout(layout);
+
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+                .addComponent(headerPanel)
+                .addComponent(chkAdminsHaveNegotiation)
+                .addComponent(chkAdminExperienceLevelIncludeNegotiation)
+                .addComponent(chkAdminsHaveScrounge)
+                .addComponent(chkAdminExperienceLevelIncludeScrounge));
+
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(Alignment.LEADING)
+                .addComponent(headerPanel)
+                .addComponent(chkAdminsHaveNegotiation)
+                .addComponent(chkAdminExperienceLevelIncludeNegotiation)
+                .addComponent(chkAdminsHaveScrounge)
+                .addComponent(chkAdminExperienceLevelIncludeScrounge));
+
+        // Create Parent Panel and return
+        return createParentPanel(panel, "AdministratorsTab");
+    }
+
+    public JPanel createAwardsTab() {
+        // Header
+        JPanel headerPanel = createHeaderPanel("AwardsTab",
+            getImageDirectory() + "logo_draconis_combine.png",
+            false, "", true);
+
+        // Contents
+        lblAwardBonusStyle = createLabel("AwardBonusStyle", null);
+        comboAwardBonusStyle.setRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(final JList<?> list, final Object value,
+                                                          final int index, final boolean isSelected,
+                                                          final boolean cellHasFocus) {
+                super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                if (value instanceof AwardBonus) {
+                    list.setToolTipText(((AwardBonus) value).getToolTipText());
+                }
+                return this;
+            }
+        });
+
+        lblAwardTierSize = createLabel("", null);
+        spnAwardTierSize = createSpinner("AwardTierSize", null,
+            5, 1, 100, 1);
+
+        chkEnableAutoAwards = createCheckBox("EnableAutoAwards", null);
+
+        chkIssuePosthumousAwards = createCheckBox("IssuePosthumousAwards", null);
+
+        chkIssueBestAwardOnly = createCheckBox("IssueBestAwardOnly", null);
+
+        chkIgnoreStandardSet = createCheckBox("IgnoreStandardSet", null);
+
+        autoAwardsFilterPanel = createAutoAwardsFilterPanel();
+
+        lblAwardSetFilterList = createLabel("AwardSetFilterList", null);
+        txtAwardSetFilterList = new JTextArea(5, 20);
+        txtAwardSetFilterList.setLineWrap(true);
+        txtAwardSetFilterList.setWrapStyleWord(true);
+        txtAwardSetFilterList.setToolTipText(
+            wordWrap(resources.getString("lblAwardSetFilterList.toolTipText")));
+        txtAwardSetFilterList.setName("txtAwardSetFilterList");
+        txtAwardSetFilterList.setText("");
+        JScrollPane scrollAwardSetFilterList = new JScrollPane(txtAwardSetFilterList);
+        scrollAwardSetFilterList.setHorizontalScrollBarPolicy(
+            ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollAwardSetFilterList.setVerticalScrollBarPolicy(
+            ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        // Layout the Panel
+        final JPanel panel = createStandardPanel("AwardsTab", true, "");
+        final GroupLayout layout = createStandardLayout(panel);
+        panel.setLayout(layout);
+
+        layout.setVerticalGroup(
+            layout.createSequentialGroup()
+                .addComponent(headerPanel)
+                .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+                    .addComponent(lblAwardBonusStyle)
+                    .addComponent(comboAwardBonusStyle))
+                .addGroup(layout.createParallelGroup(Alignment.BASELINE)
+                    .addComponent(lblAwardTierSize)
+                    .addComponent(spnAwardTierSize))
+                .addComponent(chkEnableAutoAwards)
+                .addComponent(chkIssuePosthumousAwards)
+                .addComponent(chkIssueBestAwardOnly)
+                .addComponent(chkIgnoreStandardSet)
+                .addComponent(autoAwardsFilterPanel)
+                .addComponent(lblAwardSetFilterList)
+                .addComponent(txtAwardSetFilterList));
+
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(Alignment.LEADING)
+                .addComponent(headerPanel)
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(lblAwardBonusStyle)
+                    .addComponent(comboAwardBonusStyle)
+                    .addContainerGap(Short.MAX_VALUE, Short.MAX_VALUE))
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(lblAwardTierSize)
+                    .addComponent(spnAwardTierSize)
+                    .addContainerGap(Short.MAX_VALUE, Short.MAX_VALUE))
+                .addComponent(chkEnableAutoAwards)
+                .addComponent(chkIssuePosthumousAwards)
+                .addComponent(chkIssueBestAwardOnly)
+                .addComponent(chkIgnoreStandardSet)
+                .addComponent(autoAwardsFilterPanel)
+                .addComponent(lblAwardSetFilterList)
+                .addComponent(txtAwardSetFilterList));
+
+        // Create Parent Panel and return
+        return createParentPanel(panel, "AwardsTab");
+    }
+
+    /**
+     * Creates a panel with checkboxes for various types of autoAwards award filter options.
+     * <p>
+     * This method creates checkboxes for different types of awards filters such as contract awards,
+     * faction hunter awards, injury awards, individual kill awards, etc.
+     *
+     * @return a {@link JPanel} containing checkboxes for various types of autoAwards award filter
+     * options
+     */
+    public JPanel createAutoAwardsFilterPanel() {
+        // Contents
+        chkEnableContractAwards = createCheckBox("EnableContractAwards", null);
+        chkEnableFactionHunterAwards = createCheckBox("EnableFactionHunterAwards", null);
+        chkEnableInjuryAwards = createCheckBox("EnableInjuryAwards", null);
+        chkEnableIndividualKillAwards = createCheckBox("EnableIndividualKillAwards", null);
+        chkEnableFormationKillAwards = createCheckBox("EnableFormationKillAwards", null);
+        chkEnableRankAwards = createCheckBox("EnableRankAwards", null);
+        chkEnableScenarioAwards = createCheckBox("EnableScenarioAwards", null);
+        chkEnableSkillAwards = createCheckBox("EnableSkillAwards", null);
+        chkEnableTheatreOfWarAwards = createCheckBox("EnableTheatreOfWarAwards", null);
+        chkEnableTimeAwards = createCheckBox("EnableTimeAwards", null);
+        chkEnableTrainingAwards = createCheckBox("EnableTrainingAwards", null);
+        chkEnableMiscAwards = createCheckBox("EnableMiscAwards", null);
+
+        // Layout the Panel
+        final JPanel panel = createStandardPanel("AutoAwardsFilterPanel", true, "");
+        panel.setBorder(BorderFactory.createTitledBorder(resources.getString("autoAwardsFilterPanel.title")));
+        final GroupLayout layout = createStandardLayout(panel);
+        panel.setLayout(layout);
+
+        layout.setVerticalGroup(
+            layout.createParallelGroup(Alignment.BASELINE)
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(chkEnableContractAwards)
+                    .addComponent(chkEnableFactionHunterAwards)
+                    .addComponent(chkEnableInjuryAwards)
+                    .addComponent(chkEnableIndividualKillAwards))
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(chkEnableRankAwards)
+                    .addComponent(chkEnableScenarioAwards)
+                    .addComponent(chkEnableSkillAwards)
+                    .addComponent(chkEnableFormationKillAwards))
+                .addGroup(layout.createSequentialGroup()
+                    .addComponent(chkEnableTheatreOfWarAwards)
+                    .addComponent(chkEnableTimeAwards)
+                    .addComponent(chkEnableTrainingAwards)
+                    .addComponent(chkEnableMiscAwards)));
+
+        layout.setHorizontalGroup(
+            layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(chkEnableContractAwards)
+                    .addComponent(chkEnableFactionHunterAwards)
+                    .addComponent(chkEnableInjuryAwards)
+                    .addComponent(chkEnableIndividualKillAwards))
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(chkEnableFormationKillAwards)
+                    .addComponent(chkEnableRankAwards)
+                    .addComponent(chkEnableScenarioAwards)
+                    .addComponent(chkEnableSkillAwards))
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(chkEnableTheatreOfWarAwards)
+                    .addComponent(chkEnableTimeAwards)
+                    .addComponent(chkEnableTrainingAwards)
+                    .addComponent(chkEnableMiscAwards)));
+
+        // Create Parent Panel and return
+        return createParentPanel(panel, "AutoAwardsFilterPanel");
     }
 }
