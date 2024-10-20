ac97987ed2c8530bda87e5d1f0b4542ecc9f22f9
IllianiCBT <danielfarbrace@icloud.com>
2024-10-20 16:27:26 -0500

    Fixed SPA Tooltip HTML Wrapping in `PersonViewPanel`
    Removed the unnecessary "<html>" tags from tooltip descriptions in `PersonViewPanel`.

--- MekHQ/src/mekhq/gui/view/PersonViewPanel.java
+++ MekHQ/src/mekhq/gui/view/PersonViewPanel.java
@@ -1449,9 +1449,8 @@ public class PersonViewPanel extends JScrollablePanel {
                 IOption option = i.nextElement();
                 if (option.booleanValue()) {
                     JLabel lblAbility2 = new JLabel(Utilities.getOptionDisplayName(option));
-                    lblAbility2.setToolTipText(wordWrap("<html>"
-                        + option.getDescription().replaceAll("\\n", "<br>")
-                        + "</html>"));
+                    lblAbility2.setToolTipText(wordWrap(
+                        option.getDescription().replaceAll("\\n", "<br>")));
                     lblAbility2.setName("lblAbility2");
                     lblAbility2.getAccessibleContext().getAccessibleRelationSet().add(
                             new AccessibleRelation(AccessibleRelation.LABELED_BY, lblAbility1));
@@ -1482,9 +1481,8 @@ public class PersonViewPanel extends JScrollablePanel {
 
                 if (option.booleanValue()) {
                     JLabel lblImplants2 = new JLabel(Utilities.getOptionDisplayName(option));
-                    lblImplants2.setToolTipText(wordWrap("<html>"
-                        + option.getDescription().replaceAll("\\n", "<br>")
-                        + "</html>"));
+                    lblImplants2.setToolTipText(wordWrap(
+                        option.getDescription().replaceAll("\\n", "<br>")));
                     lblImplants2.setName("lblImplants2");
                     lblImplants2.getAccessibleContext().getAccessibleRelationSet().add(
                             new AccessibleRelation(AccessibleRelation.LABELED_BY, lblImplants1));
