70ae6a39c5814e23e724301c5e2d3d79680d9dc3
IllianiCBT <danielfarbrace@icloud.com>
2024-10-27 23:01:02 -0500

    Refactor Resupply class and update resource file
    Consolidated import statements in the Resupply.java class and introduced a Logger for debugging. Modified the Stratcon track selection logic along with adjustments in reporting methods. Updated Resupply.properties for better readability and maintainability.

--- MekHQ/src/mekhq/campaign/stratcon/StratconContractInitializer.java
+++ MekHQ/src/mekhq/campaign/stratcon/StratconContractInitializer.java
@@ -376,7 +376,7 @@ public class StratconContractInitializer {
      * Utility function that, given a track state, picks a random set of unoccupied
      * coordinates.
      */
-    private static StratconCoords getUnoccupiedCoords(StratconTrackState trackState) {
+    static StratconCoords getUnoccupiedCoords(StratconTrackState trackState) {
         // plonk
         int x = Compute.randomInt(trackState.getWidth());
         int y = Compute.randomInt(trackState.getHeight());
