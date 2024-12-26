054ed9e172d073558661f3a95d00c1d26cd08a95
IllianiCBT <danielfarbrace@icloud.com>
2024-12-26 14:16:27 -0600

    Add unit tests for loan and jump nag dialogs
    Added unit tests for `UnableToAffordLoanPaymentNagDialog` and `UnableToAffordJumpNagDialog` classes. These tests cover scenarios to ensure accurate behavior when determining affordability of loan payments and jump costs.

---
+++ MekHQ/unittests/mekhq/gui/dialog/nagDialogs/UnableToAffordJumpNagDialogTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2024 - The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MekHQ. If not, see <http://www.gnu.org/licenses/>.
+ */
+package mekhq.gui.dialog.nagDialogs;
+
+import mekhq.campaign.Campaign;
+import mekhq.campaign.CampaignOptions;
+import mekhq.campaign.CurrentLocation;
+import mekhq.campaign.JumpPath;
+import mekhq.campaign.finances.Money;
+import mekhq.campaign.universe.PlanetarySystem;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * This class is a test class for the {@link UnableToAffordJumpNagDialog} class.
+ * It contains tests for various scenarios related to the {@code isUnableToAffordNextJump} method
+ */
+class UnableToAffordJumpNagDialogTest {
+    // Mock objects for the tests
+    private Campaign campaign;
+    private UnableToAffordJumpNagDialog unableToAffordJumpNagDialog;
+
+    /**
+     * Test setup for each test, runs before each test.
+     * Initializes the mock objects and sets up the necessary mock behaviors.
+     */
+    @BeforeEach
+    void init() {
+        // Initialize the mock objects
+        campaign = mock(Campaign.class);
+        CampaignOptions options = mock(CampaignOptions.class);
+        CurrentLocation location = mock(CurrentLocation.class);
+        JumpPath jumpPath = mock(JumpPath.class);
+        PlanetarySystem originSystem = mock(PlanetarySystem.class);
+        PlanetarySystem destinationSystem = mock(PlanetarySystem.class);
+
+        unableToAffordJumpNagDialog = new UnableToAffordJumpNagDialog(campaign);
+
+        when(campaign.getCampaignOptions()).thenReturn(options);
+        when(campaign.getFunds()).thenReturn(Money.of(1));
+
+        when(campaign.getLocation()).thenReturn(location);
+        when(location.getCurrentSystem()).thenReturn(originSystem);
+        when(location.getJumpPath()).thenReturn(jumpPath);
+        when(jumpPath.getLastSystem()).thenReturn(destinationSystem);
+
+        when(options.isEquipmentContractBase()).thenReturn(false);
+    }
+
+    @Test
+    void canAffordNextJump() {
+        when(campaign.calculateCostPerJump(true, false)).thenReturn(Money.of(0));
+
+        assertFalse(unableToAffordJumpNagDialog.unableToAffordNextJump());
+    }
+
+    @Test
+    void cannotAffordNextJump() {
+        when(campaign.calculateCostPerJump(true, false)).thenReturn(Money.of(2));
+
+        assertFalse(unableToAffordJumpNagDialog.unableToAffordNextJump());
+    }
+}
