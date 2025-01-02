23e7c7ca0c1082b7e041f0aecbef72634f62921b
jwdogg609 <jermurray2@gmail.com>
2025-01-02 16:12:17 -0500

    Small scale Autologistics Fixes

--- MekHQ/src/mekhq/campaign/Campaign.java
+++ MekHQ/src/mekhq/campaign/Campaign.java
@@ -8680,8 +8680,8 @@ public class Campaign implements ITechManager {
             if (toBuy > 0) {
                 IAcquisitionWork partToBuy = partInUse.getPartToBuy();
                 getShoppingList().addShoppingItem(partToBuy, toBuy, this);
+                bought += 1;
             }
-            bought += 1;
         }
         return bought;
     }
