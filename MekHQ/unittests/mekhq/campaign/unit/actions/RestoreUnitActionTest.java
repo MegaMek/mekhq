3443d6b9f4c312df830371be8d8541f50ddf5102
psikomonkie <189469115+psikomonkie@users.noreply.github.com>
2025-01-11 14:20:35 -0500

    Issue 3650: Fixed RestoreUnitActionTest.java

--- MekHQ/unittests/mekhq/campaign/unit/actions/RestoreUnitActionTest.java
+++ MekHQ/unittests/mekhq/campaign/unit/actions/RestoreUnitActionTest.java
@@ -83,8 +83,7 @@ public class RestoreUnitActionTest {
 
         verify(unit, times(1)).setEntity(eq(mockNewEntity));
         verify(unit, times(1)).removeParts();
-        verify(unit, times(1)).initializeShipTransportSpace();
-        verify(unit, times(1)).initializeTacticalTransportSpace();
+        verify(unit, times(1)).initializeAllTransportSpace();
         verify(unit, times(1)).initializeParts(eq(true));
         verify(unit, times(1)).runDiagnostic(eq(false));
         verify(unit, times(1)).setSalvage(eq(false));
