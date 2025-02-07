fff3c87c9bf389b13d125aba0e360b8ecbe1eef5
firefly2442 <firefly2442@gmail.com>
2025-02-06 22:34:06 -0600

    fix: pull out skill level into variable to prevent duplicate calls

--- MekHQ/src/mekhq/gui/dialog/BatchXPDialog.java
+++ MekHQ/src/mekhq/gui/dialog/BatchXPDialog.java
@@ -171,7 +171,8 @@ public final class BatchXPDialog extends JDialog {
         DefaultComboBoxModel<PersonTypeItem> personExpModel = new DefaultComboBoxModel<>();
         personExpModel.addElement(new PersonTypeItem(resourceMap.getString("experience.choice.text"), null));
         for (int i = SkillLevel.ULTRA_GREEN.ordinal(); i <= SkillLevel.ELITE.ordinal(); i++) {
-            personExpModel.addElement(new PersonTypeItem(SkillLevel.parseFromInteger(i).toString(), SkillLevel.parseFromInteger(i).getAdjustedValue()));
+            final SkillLevel skillLevel = SkillLevel.parseFromInteger(i);
+            personExpModel.addElement(new PersonTypeItem(skillLevel.toString(), skillLevel.getAdjustedValue()));
         }
         choiceExp.setModel(personExpModel);
         choiceExp.setSelectedIndex(0);
