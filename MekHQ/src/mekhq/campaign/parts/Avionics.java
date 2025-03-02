efeeb9202637f26760151ca41f4752eebdc4ec3d
Richard J Hancock <rjhancock@tapenvy.us>
2025-03-02 15:09:51 -0600

    Next 132...

--- MekHQ/src/mekhq/campaign/parts/Avionics.java
+++ MekHQ/src/mekhq/campaign/parts/Avionics.java
@@ -1,23 +1,29 @@
 /*
- * Avionics.java
- *
  * Copyright (c) 2009 Jay Lawson (jaylawson39 at yahoo.com). All rights reserved.
+ * Copyright (C) 2013-2025 The MegaMek Team
  *
  * This file is part of MekHQ.
  *
- * MekHQ is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
  *
- * MekHQ is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with MekHQ. If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community. BattleMech,
+ * BattleTech, and MechWarrior are trademarks of The Topps Company, Inc.
+ * The MegaMek organization is not affiliated with The Topps Company, Inc.
+ * or Catalyst Game Labs.
  */
+
 package mekhq.campaign.parts;
 
 import java.io.PrintWriter;
