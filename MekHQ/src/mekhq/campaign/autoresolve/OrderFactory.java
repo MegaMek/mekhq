fd8fcd0c64c789e302bc5378ea6bed1a9355b1c0
Richard J Hancock <rjhancock@tapenvy.us>
2025-03-02 14:42:53 -0600

    First 76 files...

--- MekHQ/src/mekhq/campaign/autoresolve/OrderFactory.java
+++ MekHQ/src/mekhq/campaign/autoresolve/OrderFactory.java
@@ -1,16 +1,26 @@
 /*
- * Copyright (c) 2024-2025 - The MegaMek Team. All Rights Reserved.
+ * Copyright (C) 2024-2025 The MegaMek Team
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * This file is part of MekHQ.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
+ * MegaMek is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
  *
+ * MegaMek is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community. BattleMech,
+ * BattleTech, and MechWarrior are trademarks of The Topps Company, Inc.
+ * The MegaMek organization is not affiliated with The Topps Company, Inc.
+ * or Catalyst Game Labs.
  */
 
 package mekhq.campaign.autoresolve;
