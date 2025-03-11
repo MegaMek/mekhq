c624e15d86fef4ca4631aa870a17a97cff031aaa
IllianiCBT <danielfarbrace@icloud.com>
2025-03-11 16:25:07 -0500

    Added Limb Replacement Functionality to Advanced Medical
    - Introduced a new ReplacementLimbDialog to manage limb replacement procedures with in-character messaging.
    - Added support for limb replacement costs, requirements, and recovery in `InjuryTypes.java` and related classes.
    - Updated `PersonnelTableMouseAdapter.java` to handle limb replacement actions via the context menu.
    - Implemented RAW (ATOW) handling of doctor skill requirements and non-RAW increased costs for hiring external specialists.
    - Added a new injury type, `ReplacementLimbRecovery`, which tracks recovery time and penalties during the healing period.

---
+++ MekHQ/src/mekhq/gui/dialog/ReplacementLimbDialog.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2025 The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ */
+package mekhq.gui.dialog;
+
+import mekhq.campaign.Campaign;
+import mekhq.campaign.finances.Money;
+import mekhq.campaign.personnel.Person;
+import mekhq.gui.baseComponents.MHQDialogImmersive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static mekhq.campaign.Campaign.AdministratorSpecialization.HR;
+import static mekhq.utilities.MHQInternationalization.getFormattedTextAt;
+
+/**
+ * A dialog for managing the replacement of limbs in a campaign. This immersive dialog provides
+ * an in-character message and appropriate buttons based on the scenario, such as availability
+ * of doctors and sufficient funds.
+ *
+ * <p>The dialog uses localized resources to display messages and creates responses based
+ * on campaign and patient details.</p>
+ */
+public class ReplacementLimbDialog extends MHQDialogImmersive {
+    final private static String RESOURCE_BUNDLE = "mekhq.resources." + ReplacementLimbDialog.class.getSimpleName();
+
+    /**
+     * Constructor to create a Replacement Limb Dialog.
+     *
+     * @param campaign         The associated {@link Campaign} object, holding the state of the game.
+     * @param suitableDoctors  A list of {@link Person} objects representing doctors qualified to
+     *                        perform the replacement procedure. May be empty if no suitable doctors
+     *                        are available.
+     * @param patient          The {@link Person} who is receiving the limb replacement.
+     * @param cost             The {@link Money} cost of the limb replacement procedure.
+     */
+    public ReplacementLimbDialog(Campaign campaign, List<Person> suitableDoctors, Person patient, Money cost) {
+        super(campaign, getSpeaker(campaign), patient,
+              createInCharacterMessage(campaign.getLocation().isOnPlanet(), !suitableDoctors.isEmpty(),
+                    campaign.getCommanderAddress(false), patient, cost, campaign.getFunds().isGreaterOrEqualThan(cost)),
+              createButtons(campaign.getFunds().isGreaterOrEqualThan(cost)), getFormattedTextAt(RESOURCE_BUNDLE, "message.ooc"),
+              null, false);
+    }
+
+    /**
+     * Creates the buttons for the dialog based on whether sufficient funds are available for the
+     * procedure.
+     *
+     * @param hasSufficientFunds {@code true} if the campaign has enough funds to cover the procedure,
+     *                           {@code false} otherwise.
+     * @return A {@link List} of {@link ButtonLabelTooltipPair} objects representing the dialog buttons.
+     */
+    private static List<ButtonLabelTooltipPair> createButtons(boolean hasSufficientFunds) {
+        List<ButtonLabelTooltipPair> buttons = new ArrayList<>();
+
+        if (!hasSufficientFunds) {
+            ButtonLabelTooltipPair btnDecline = new ButtonLabelTooltipPair(getFormattedTextAt(RESOURCE_BUNDLE,
+                  "understood.button"), null);
+            buttons.add(btnDecline);
+            return buttons;
+        }
+
+        ButtonLabelTooltipPair btnDecline = new ButtonLabelTooltipPair(getFormattedTextAt(RESOURCE_BUNDLE,
+              "decline.button"), null);
+        buttons.add(btnDecline);
+
+        ButtonLabelTooltipPair btnAccept = new ButtonLabelTooltipPair(getFormattedTextAt(RESOURCE_BUNDLE,
+              "accept.button"), null);
+        buttons.add(btnAccept);
+
+        return buttons;
+    }
+
+    /**
+     * Creates an in-character message for the dialog based on various conditions related to planet
+     * location, doctor availability, and funds.
+     *
+     * @param isPlanetside        {@code true} if the campaign is on a planet, {@code false} otherwise.
+     * @param hasQualifiedDoctors {@code true} if there are qualified doctors available, {@code false}
+     *                                       otherwise.
+     * @param commanderAddress    The address for the campaign commander.
+     * @param patient             The {@link Person} receiving the procedure.
+     * @param cost                The {@link Money} cost of the procedure.
+     * @param hasSufficientFunds  {@code true} if enough funds are available, {@code false} otherwise.
+     * @return A {@link String} containing the localized in-character message for the dialog.
+     */
+    private static String createInCharacterMessage(boolean isPlanetside, boolean hasQualifiedDoctors,
+                                                   String commanderAddress, Person patient, Money cost,
+                                                   boolean hasSufficientFunds) {
+        String keyAddendum = "normal";
+
+        if (!hasQualifiedDoctors && hasSufficientFunds) {
+            if (isPlanetside) {
+                keyAddendum = "noSurgeonOnPlanet";
+            } else {
+                keyAddendum = "noSurgeonInTravel";
+            }
+        }
+
+        if (!hasSufficientFunds) {
+            keyAddendum = "insufficientFunds";
+        }
+
+        final String KEY = "message.inCharacter." + keyAddendum;
+
+        return getFormattedTextAt(RESOURCE_BUNDLE, KEY, commanderAddress, patient.getFullTitle(),
+              cost.toAmountString());
+    }
+
+    /**
+     * Determines the speaker for the dialog, prioritizing the senior doctor among the active personnel.
+     * If no doctors are available, the campaign's senior HR administrator is selected as the speaker.
+     *
+     * @param campaign The {@link Campaign} instance containing the active personnel and administrators.
+     * @return The speaker for the dialog. This is the most senior doctor, if available; otherwise,
+     *         it is the campaign's senior HR administrator.
+     */
+    private static Person getSpeaker(Campaign campaign) {
+        Person seniorDoctor = null;
+
+        for (Person person : campaign.getActivePersonnel(false)) {
+            if (person.isDoctor()) {
+                if (person.outRanksUsingSkillTiebreaker(campaign, seniorDoctor)) {
+                    seniorDoctor = person;
+                }
+            }
+        }
+
+        if (seniorDoctor != null) {
+            return seniorDoctor;
+        } else {
+            return campaign.getSeniorAdminPerson(HR);
+        }
+    }
+}
