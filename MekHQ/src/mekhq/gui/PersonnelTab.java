ee38f7f0a8700e6368339ccf3c37002c28dc137d
IllianiCBT <danielfarbrace@icloud.com>
2025-05-06 19:13:49 -0500

    Improvement: Improved Person View to Include a Larger Portrait & Upgraded Several Components; Moved Medical Record Button to Personnel Right-Click Menu

--- MekHQ/src/mekhq/gui/PersonnelTab.java
+++ MekHQ/src/mekhq/gui/PersonnelTab.java
@@ -32,6 +32,9 @@
  */
 package mekhq.gui;
 
+import static java.lang.Math.round;
+import static megamek.client.ui.swing.util.UIUtil.scaleForGUI;
+
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
@@ -53,7 +56,6 @@ import megamek.client.ui.preferences.JTablePreference;
 import megamek.client.ui.preferences.JToggleButtonPreference;
 import megamek.client.ui.preferences.PreferencesNode;
 import megamek.client.ui.swing.GUIPreferences;
-import megamek.client.ui.swing.util.UIUtil;
 import megamek.common.event.Subscribe;
 import megamek.common.preference.IPreferenceChangeListener;
 import megamek.logging.MMLogger;
@@ -76,7 +78,7 @@ import mekhq.gui.view.PersonViewPanel;
 public final class PersonnelTab extends CampaignGuiTab {
     private static final MMLogger logger = MMLogger.create(PersonnelTab.class);
 
-    public static final int PERSONNEL_VIEW_WIDTH = 490;
+    public static final int PERSONNEL_VIEW_WIDTH = scaleForGUI(600);
 
     private JSplitPane splitPersonnel;
     private JTable personnelTable;
@@ -223,7 +225,7 @@ public final class PersonnelTab extends CampaignGuiTab {
         personnelTable.getSelectionModel().addListSelectionListener(ev -> refreshPersonnelView());
 
         scrollPersonnelView = new JScrollPaneWithSpeed();
-        scrollPersonnelView.setMinimumSize(new Dimension(PERSONNEL_VIEW_WIDTH, 600));
+        scrollPersonnelView.setMinimumSize(new Dimension((int) round(PERSONNEL_VIEW_WIDTH * 0.9), 600));
         scrollPersonnelView.setPreferredSize(new Dimension(PERSONNEL_VIEW_WIDTH, 600));
         scrollPersonnelView.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPersonnelView.setViewportView(null);
@@ -322,7 +324,7 @@ public final class PersonnelTab extends CampaignGuiTab {
                                             PersonnelTabView.GENERAL :
                                             choicePersonView.getSelectedItem();
         final XTableColumnModel columnModel = (XTableColumnModel) getPersonnelTable().getColumnModel();
-        getPersonnelTable().setRowHeight(UIUtil.scaleForGUI(15));
+        getPersonnelTable().setRowHeight(scaleForGUI(15));
 
         // set the renderer
         for (final PersonnelTableModelColumn column : PersonnelTableModel.PERSONNEL_COLUMNS) {
