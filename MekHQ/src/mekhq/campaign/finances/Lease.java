7f3206c28349df3bb1ca6f2056ccc04cc83ffc0c
Craig Rickel <travistgr@gmail.com>
2025-05-16 03:45:42 -0500

    Implements second part of leases, acquisition rolls are now more or less sane.

--- MekHQ/src/mekhq/campaign/finances/Lease.java
+++ MekHQ/src/mekhq/campaign/finances/Lease.java
@@ -1,17 +1,55 @@
+/*
+ * Copyright (c) 2009 Jay Lawson (jaylawson39 at yahoo.com). All rights reserved.
+ * Copyright (C) 2013-2025 The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ */
 package mekhq.campaign.finances;
 
-import megamek.common.Entity;
-import mekhq.campaign.Campaign;
-import mekhq.campaign.unit.Unit;
-
+import java.io.PrintWriter;
 import java.time.LocalDate;
 
-public class Lease implements mekhq.campaign.work.IAcquisitionWork {
+import megamek.common.Entity;
+import megamek.logging.MMLogger;
+import mekhq.MekHQ;
+import mekhq.campaign.Campaign;
+import mekhq.campaign.unit.Unit;
+import mekhq.campaign.unit.UnitOrder;
+import mekhq.utilities.MHQXMLUtility;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class Lease extends UnitOrder {
+    private static final MMLogger logger = MMLogger.create(Lease.class);
     private Money leaseCost;
     private LocalDate acquisitionDate;
-    private Entity futureUnit;
+    private Unit leasedUnit;
     private int daysToWait;
     private Campaign campaign;
+    private int quantity;
 
     /*
      * Leases are nominally attached to units while they are in the hanger.
@@ -25,28 +63,36 @@ public class Lease implements mekhq.campaign.work.IAcquisitionWork {
     /*
      * If a lease is in the shopping list, it doesn't have a unit yet to attach to.
      */
-    public Lease(Entity futureUnit, Campaign campaign) {
-        this.futureUnit = futureUnit;
+    public Lease(Entity entity, Campaign campaign) {
+        this.entity = entity;
+        this.leasedUnit = new Unit(entity, campaign);
         this.campaign = campaign;
+    }
+
+    public Lease() {
+        super(null, null);
     }
 
     public void realizeLease(LocalDate currentDay, Unit unit) {
         acquisitionDate = currentDay;
         leaseCost = unit.getSellValue().multipliedBy(0.005);
-        this.futureUnit = null;
     }
 
     /**
-     * Gets the lease cost, for the accountant. Lease cost is prorated for the first month, so we need to check
-     * if yesterday was the first month. Should only be called on the 1st.
+     * Gets the lease cost, for the accountant. Lease cost is prorated for the first month, so we need to check if
+     * yesterday was the first month. Should only be called on the 1st. It's also possible we get a lease object before
+     * the lease actually starts, if it takes a while to deliver the unit. Cost will be zero in this case.
      *
      * @params time The current campaign LocalDate
      */
     public Money getLeaseCost(LocalDate time) {
-        if (isLeaseFirstMonth(time.minusDays(1))) {
-            return getFirstLeaseCost(time);
+        if (getLeaseStart().isAfter(campaign.getLocalDate())) {
+            if (isLeaseFirstMonth(time.minusDays(1))) {
+                return getFirstLeaseCost(time);
+            }
+            return leaseCost;
         }
-        return leaseCost;
+        return Money.zero();
     }
 
     /**
@@ -87,8 +133,8 @@ public class Lease implements mekhq.campaign.work.IAcquisitionWork {
     }
 
     /**
-     * Gets the cost of the lease, prorated in the first month.
-     * Assumes that it's only called on the first day of the month, so we need to find yesterday for the last.
+     * Gets the cost of the lease, prorated in the first month. Assumes that it's only called on the first day of the
+     * month, so we need to find yesterday for the last.
      *
      * @return Money Prorated first payment of lease
      */
@@ -104,31 +150,54 @@ public class Lease implements mekhq.campaign.work.IAcquisitionWork {
     }
 
     @Override
-    public String getAcquisitionName() {
-        return futureUnit.generalName();
-    }
-
-    @Override
-    public String getAcquisitionDisplayName() {
-        return futureUnit.generalName();
-    }
-
-    @Override
-    public int getDaysToWait() {
-        return daysToWait;
-    }
-
-    @Override
-    public void resetDaysToWait() {
-        this.daysToWait = campaign.getCampaignOptions().getWaitingPeriod();
-        ;
-    }
-
-    @Override
     public void decrementDaysToWait() {
         daysToWait = daysToWait > 0 ? daysToWait-- : 0;
     }
 
+    @Override
+    public String find(int transitDays) {
+        campaign.getQuartermaster().leaseUnit((Entity) getNewEquipment(), transitDays);
+        return "<font color='" +
+                     MekHQ.getMHQOptions().getFontColorPositiveHexColor() +
+                     "'><b> unit found</b>.</font> It will be delivered in " +
+                     transitDays +
+                     " days.";
+    }
 
+    @Override
+    public void writeToXML(final PrintWriter pw, int indent) {
+        MHQXMLUtility.writeSimpleXMLOpenTag(pw, indent++, "unitLease");
+        pw.println(MHQXMLUtility.writeEntityToXmlString(getEntity(), indent, getCampaign().getEntities()));
+        MHQXMLUtility.writeSimpleXMLTag(pw, indent, "quantity", quantity);
+        MHQXMLUtility.writeSimpleXMLTag(pw, indent, "daysToWait", daysToWait);
+        MHQXMLUtility.writeSimpleXMLCloseTag(pw, --indent, "unitOrder");
+    }
+
+    public static Lease generateInstanceFromXML(Node wn, Campaign c) {
+        Lease retVal = new Lease();
+        retVal.setCampaign(c);
+
+        NodeList nl = wn.getChildNodes();
+
+        try {
+            for (int x = 0; x < nl.getLength(); x++) {
+                Node wn2 = nl.item(x);
+
+                if (wn2.getNodeName().equalsIgnoreCase("quantity")) {
+                    retVal.quantity = Integer.parseInt(wn2.getTextContent());
+                } else if (wn2.getNodeName().equalsIgnoreCase("daysToWait")) {
+                    retVal.daysToWait = Integer.parseInt(wn2.getTextContent());
+                } else if (wn2.getNodeName().equalsIgnoreCase("entity")) {
+                    retVal.entity = MHQXMLUtility.parseSingleEntityMul((Element) wn2, c);
+                }
+            }
+        } catch (Exception ex) {
+            logger.error("", ex);
+        }
+
+        retVal.initializeParts(false);
+
+        return retVal;
+    }
 }
 
