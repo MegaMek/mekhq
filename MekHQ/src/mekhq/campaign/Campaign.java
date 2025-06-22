ac56cb03117702c0138dab6d1e02f467076a757d
IllianiCBT <danielfarbrace@icloud.com>
2025-06-22 15:26:30 -0500

    Fix: Fixed Recruitment Report Not Showing Rank

--- MekHQ/src/mekhq/campaign/Campaign.java
+++ MekHQ/src/mekhq/campaign/Campaign.java
@@ -2218,7 +2218,7 @@ public class Campaign implements ITechManager {
                                                                 "personnelRecruitmentPrisoner.text")) :
                                "";
             addReport(String.format(resources.getString("personnelRecruitmentAddedToRoster.text"),
-                  person.getHyperlinkedName(),
+                  person.getHyperlinkedFullTitle(),
                   formerSurname,
                   add));
         }
