43a917fe75adcf228a1004a8ac3b8481d0716a12
IllianiCBT <danielfarbrace@icloud.com>
2025-07-17 16:37:28 -0500

    Improvement: Implemented Pathologic Racism Compulsion Flaw

--- MekHQ/src/mekhq/campaign/personnel/Person.java
+++ MekHQ/src/mekhq/campaign/personnel/Person.java
@@ -5485,13 +5485,17 @@ public class Person {
      */
     public int getAdjustedReputation(boolean isUseAgingEffects, boolean isClanCampaign, LocalDate today,
           int rankLevel) {
-        int modifier = isUseAgingEffects ?
+        int modifiers = isUseAgingEffects ?
                              getReputationAgeModifier(getAge(today),
                                    isClanCampaign,
                                    !isNullOrBlank(bloodname),
                                    rankLevel) :
                              0;
-        return reputation + modifier;
+
+        boolean hasPathologicRacism = options.booleanOption(COMPULSION_PATHOLOGIC_RACISM);
+        modifiers -= hasPathologicRacism ? 2 : 0;
+
+        return clamp(reputation + modifiers, MINIMUM_REPUTATION, MAXIMUM_REPUTATION);
     }
 
     public void setReputation(final int reputation) {
