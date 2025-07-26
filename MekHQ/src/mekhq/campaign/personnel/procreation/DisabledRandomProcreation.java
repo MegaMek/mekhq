ff79ba024b8263c9d751cd251f65f9fde37a128f
IllianiCBT <danielfarbrace@icloud.com>
2025-07-26 18:15:30 -0500

    Moved Campaign Options into own package

--- MekHQ/src/mekhq/campaign/personnel/procreation/DisabledRandomProcreation.java
+++ MekHQ/src/mekhq/campaign/personnel/procreation/DisabledRandomProcreation.java
@@ -24,14 +24,19 @@
  *
  * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
  * InMediaRes Productions, LLC.
+ *
+ * MechWarrior Copyright Microsoft Corporation. MekHQ was created under
+ * Microsoft's "Game Content Usage Rules"
+ * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
+ * affiliated with Microsoft.
  */
 package mekhq.campaign.personnel.procreation;
 
-import mekhq.campaign.CampaignOptions;
+import java.time.LocalDate;
+
+import mekhq.campaign.campaignOptions.CampaignOptions;
 import mekhq.campaign.personnel.Person;
 import mekhq.campaign.personnel.enums.RandomProcreationMethod;
-
-import java.time.LocalDate;
 
 public class DisabledRandomProcreation extends AbstractProcreation {
     //region Constructors
