00623357190d57b8558f129d9a263e4cb41e8b4c
IllianiBird <danielfarbrace@icloud.com>
2025-10-15 13:21:40 -0500

    Rolled back unchanged class

--- MekHQ/src/mekhq/gui/model/UnitTableModel.java
+++ MekHQ/src/mekhq/gui/model/UnitTableModel.java
@@ -201,6 +201,8 @@ public class UnitTableModel extends DataTableModel<Unit> {
         int crewAssigned = unit.getCrew().size() - (gunnersAssigned + driversAssigned + navigatorsAssigned);
 
         StringBuilder report = new StringBuilder("<html>");
+
+
         if (driversNeeded > 0 && soldiersNeeded == 0) {
             appendReport(report, "Drivers", driversAssigned, driversNeeded);
         }
