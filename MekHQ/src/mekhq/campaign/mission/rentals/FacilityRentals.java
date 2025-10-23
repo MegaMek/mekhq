5a2352271ae6fd4cf8e38d8b4c65ab5d4af42df2
IllianiBird <danielfarbrace@icloud.com>
2025-10-23 14:58:52 -0500

    Implemented rental support classes

---
+++ MekHQ/src/mekhq/campaign/mission/rentals/FacilityRentals.java
@@ -0,0 +1,410 @@
+/*
+ * Copyright (C) 2025 The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ *
+ * MechWarrior Copyright Microsoft Corporation. MekHQ was created under
+ * Microsoft's "Game Content Usage Rules"
+ * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
+ * affiliated with Microsoft.
+ */
+package mekhq.campaign.mission.rentals;
+
+import static mekhq.utilities.MHQInternationalization.getFormattedTextAt;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import megamek.common.units.Entity;
+import mekhq.MekHQ;
+import mekhq.campaign.Campaign;
+import mekhq.campaign.campaignOptions.CampaignOptions;
+import mekhq.campaign.events.RepairStatusChangedEvent;
+import mekhq.campaign.finances.Finances;
+import mekhq.campaign.finances.Money;
+import mekhq.campaign.finances.enums.TransactionType;
+import mekhq.campaign.mission.AtBContract;
+import mekhq.campaign.mission.Contract;
+import mekhq.campaign.mission.Mission;
+import mekhq.campaign.unit.Unit;
+import mekhq.gui.baseComponents.immersiveDialogs.ImmersiveDialogConfirmation;
+import mekhq.gui.dialog.ContractStartRentalDialog;
+
+/**
+ * Handles rental opportunities and transactions for various campaign facilities such as repair bays, hospital beds,
+ * kitchens, and holding cells.
+ *
+ * <p>Provides utility methods for offering facility rentals, calculating costs, and managing rental-related finances
+ * and reporting within the campaign context.</p>
+ *
+ * @author Illiani
+ * @since 0.50.10
+ */
+public class FacilityRentals {
+    private static final String RESOURCE_BUNDLE = "mekhq.resources.FacilityRentals";
+
+    private static final int FACTORY_CONDITIONS_MULTIPLIER = 20;
+
+    /**
+     * Offers the opportunity to rent a specified contract facility (hospital beds, kitchens, holding cells) to the
+     * campaign.
+     *
+     * <p>If the rental cost is not set or rental is disabled, nothing is offered.</p>
+     *
+     * @param campaign   the campaign to which the rental is being offered
+     * @param contract   the contract for which the rental applies
+     * @param rentalType the type of facility being rented
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    public static void offerContractRentalOpportunity(Campaign campaign, Contract contract,
+          ContractRentalType rentalType) {
+        CampaignOptions campaignOptions = campaign.getCampaignOptions();
+        int rentalCost = switch (rentalType) {
+            case HOSPITAL_BEDS -> campaignOptions.getRentedFacilitiesCostHospitalBeds();
+            case KITCHENS -> campaignOptions.getRentedFacilitiesCostKitchens();
+            case HOLDING_CELLS -> campaignOptions.getRentedFacilitiesCostHoldingCells();
+            default -> 0;
+        };
+
+        if (rentalCost <= 0) { // This rental option is disabled
+            return;
+        }
+
+        int rentalCount = presentDialog(campaign, rentalType, rentalCost);
+        if (rentalCount <= 0) { // The player chose not to rent anything
+            return;
+        }
+
+        switch (rentalType) {
+            case HOSPITAL_BEDS -> contract.setHospitalBedsRented(rentalCount);
+            case KITCHENS -> contract.setKitchensRented(rentalCount);
+            case HOLDING_CELLS -> contract.setHoldingCellsRented(rentalCount);
+            default -> {}
+        }
+    }
+
+    /**
+     * Offers a rental opportunity for repair bays or factory conditions bays.
+     *
+     * <p>Presents dialog, handles cost calculation, and attempts to debit the player's account.</p>
+     *
+     * @param campaign           the active campaign
+     * @param activeAtBContracts list of active AtB contracts (used for context, if needed)
+     * @param unitCount          the number of units intended for bay rental
+     * @param rentalType         the type of bay rental (maintenance or factory conditions)
+     *
+     * @return {@code true} if the transaction completes successfully
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    public static boolean offerBayRentalOpportunity(Campaign campaign, List<AtBContract> activeAtBContracts,
+          int unitCount, ContractRentalType rentalType) {
+        CampaignOptions campaignOptions = campaign.getCampaignOptions();
+        int baseCost = campaignOptions.getRentedFacilitiesCostRepairBays();
+        if (baseCost <= 0) { // This rental option is disabled
+            return true;
+        }
+
+        if (rentalType == ContractRentalType.FACTORY_CONDITIONS) {
+            baseCost *= FACTORY_CONDITIONS_MULTIPLIER;
+        }
+
+        int totalCost = baseCost * unitCount;
+
+        int rentalCount = presentDialog(campaign, rentalType, totalCost);
+        if (rentalCount <= 0) { // The player chose not to rent anything
+            return false;
+        }
+
+        // Returns false if the player cannot afford the rental
+        if (!performRentalTransaction(campaign.getFinances(),
+              campaign.getLocalDate(),
+              Money.of(totalCost),
+              ContractRentalType.MAINTENANCE_BAYS)) {
+            String report = getFormattedTextAt(RESOURCE_BUNDLE, "PLACEHOLDER");
+            campaign.addReport(report);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Presents the rental dialog to the user for facility or bay rental.
+     *
+     * @param campaign   the current campaign for context
+     * @param rentalType the facility type being rented
+     * @param rentalCost the base cost of the rental
+     *
+     * @return spinner value for most types, or 1 if accepted for bay/factory, or 0 if canceled
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    private static int presentDialog(Campaign campaign, ContractRentalType rentalType, int rentalCost) {
+        boolean wasConfirmedOverall = false;
+        ContractStartRentalDialog offerDialog = null;
+        while (!wasConfirmedOverall) {
+            offerDialog = new ContractStartRentalDialog(campaign, rentalType, rentalCost);
+            ImmersiveDialogConfirmation confirmation = new ImmersiveDialogConfirmation(campaign);
+            wasConfirmedOverall = confirmation.wasConfirmed();
+        }
+
+        if (rentalType == ContractRentalType.MAINTENANCE_BAYS || rentalType == ContractRentalType.FACTORY_CONDITIONS) {
+            return offerDialog.wasConfirmed() ? 1 : 0;
+        } else {
+            return offerDialog.getSpinnerValue();
+        }
+    }
+
+    /**
+     * Calculates the total rental cost for a group of active contracts and a given rental type.
+     *
+     * @param cost            the cost per rental unit
+     * @param activeContracts list of active contracts
+     * @param rentalType      the facility type being calculated
+     *
+     * @return the total rental cost as a {@link Money} object
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    public static Money calculateContractRentalCost(int cost, List<Contract> activeContracts,
+          ContractRentalType rentalType) {
+        int rentalCount = 0;
+
+        for (Contract contract : activeContracts) {
+            rentalCount += switch (rentalType) {
+                case HOSPITAL_BEDS -> contract.getHospitalBedsRented();
+                case KITCHENS -> contract.getKitchensRented();
+                case HOLDING_CELLS -> contract.getHoldingCellsRented();
+                default -> 0; // We don't care about other types of rentals
+            };
+        }
+
+        return Money.of(cost * rentalCount);
+    }
+
+    /**
+     * Debits the finances for all contract rentals (hospitals, kitchens, holding cells) for the current day.
+     *
+     * <p>Reports errors for each type (if payment unsuccessful).</p>
+     *
+     * @param finances         the finances object to debit from
+     * @param today            the date of transaction
+     * @param hospitalCosts    the rental cost for hospital beds
+     * @param kitchenCosts     the rental cost for kitchens
+     * @param holdingCellCosts the rental cost for holding cells
+     *
+     * @return list of error report strings, if any
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    private static List<String> payForAllContractRentals(Finances finances, LocalDate today, Money hospitalCosts,
+          Money kitchenCosts, Money holdingCellCosts) {
+        List<String> reports = new ArrayList<>();
+
+        // Will return false if the payment was unsuccessful
+        if (!performRentalTransaction(finances, today, hospitalCosts, ContractRentalType.HOSPITAL_BEDS)) {
+            String report = getFormattedTextAt(RESOURCE_BUNDLE, "PLACEHOLDER");
+            reports.add(report);
+        }
+
+        if (!performRentalTransaction(finances, today, kitchenCosts, ContractRentalType.KITCHENS)) {
+            String report = getFormattedTextAt(RESOURCE_BUNDLE, "PLACEHOLDER");
+            reports.add(report);
+        }
+
+        if (!performRentalTransaction(finances, today, holdingCellCosts, ContractRentalType.HOLDING_CELLS)) {
+            String report = getFormattedTextAt(RESOURCE_BUNDLE, "PLACEHOLDER");
+            reports.add(report);
+
+        }
+
+        return reports;
+    }
+
+    /**
+     * Calculates and performs payment for all rented maintenance or factory repair bays for all units in the hangar.
+     *
+     * <p>If total available funds are exceeded, units revert to a fallback repair site.</p>
+     *
+     * @param campaign the campaign in which to process bay rental payment
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    public static void payForAllRentedBays(Campaign campaign) {
+        CampaignOptions campaignOptions = campaign.getCampaignOptions();
+        int costPerBay = campaignOptions.getRentedFacilitiesCostRepairBays();
+        if (costPerBay <= 0) { // Costs have been disabled, so we're not going to perform any actions
+            return;
+        }
+
+        LocalDate today = campaign.getLocalDate();
+        List<Mission> activeMissions = campaign.getActiveMissions(false);
+        Finances finances = campaign.getFinances();
+        Money totalAvailableFunds = finances.getBalance();
+        Collection<Unit> units = campaign.getHangar().getUnits();
+
+        Money totalCharge = Money.zero();
+
+        int fallbackSite = getFallbackRepairSite(activeMissions);
+        for (Unit unit : units) {
+            if (shouldBeIgnoredByBayRentals(unit)) {
+                continue;
+            }
+
+            int unitSite = unit.getSite();
+            if (unitSite == Unit.SITE_FACILITY_MAINTENANCE) {
+                totalCharge = updateTotalCharge(campaign, costPerBay, unit, totalCharge, totalAvailableFunds,
+                      fallbackSite);
+            } else if (unitSite == Unit.SITE_FACTORY_CONDITIONS) {
+                int cost = costPerBay * FACTORY_CONDITIONS_MULTIPLIER;
+                totalCharge = updateTotalCharge(campaign, cost, unit, totalCharge, totalAvailableFunds, fallbackSite);
+            }
+        }
+
+        performRentalTransaction(finances, today, totalCharge, ContractRentalType.MAINTENANCE_BAYS);
+    }
+
+    /**
+     * Determines whether a unit should be ignored for bay rental cost calculation (e.g., mothballed or large craft).
+     *
+     * @param unit the unit to check
+     *
+     * @return {@code true} if the unit should be ignored
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    public static boolean shouldBeIgnoredByBayRentals(Unit unit) {
+        if (unit.isMothballed()) {
+            return true;
+        }
+
+        Entity entity = unit.getEntity();
+        if (entity != null) {
+            // These are left to GM fiat and not handled by MekHQ automatically
+            return entity.isLargeCraft();
+        }
+
+        return false;
+    }
+
+    /**
+     * Updates the total repair bay rental charge for a unit, handling overflow, and fallback site logic.
+     *
+     * <p>Sets the repair site of the unit to a fallback site if funds are insufficient.</p>
+     *
+     * @param campaign            the campaign context
+     * @param cost                the cost to add for the unit
+     * @param unit                the unit being processed
+     * @param totalCharge         current accumulated total
+     * @param totalAvailableFunds current available funds for rentals
+     * @param fallbackSite        site used if payment is impossible
+     *
+     * @return updated total charge (unchanged if fallback applied)
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    private static Money updateTotalCharge(Campaign campaign, int cost, Unit unit, Money totalCharge,
+          Money totalAvailableFunds, int fallbackSite) {
+        Money newTotalCharge = totalCharge.plus(cost);
+        if (newTotalCharge.isGreaterThan(totalAvailableFunds)) {
+            unit.setSite(fallbackSite);
+            String report = getFormattedTextAt(RESOURCE_BUNDLE, "PLACEHOLDER_MESSAGE", Unit.getSiteName(fallbackSite));
+            campaign.addReport(report);
+
+            MekHQ.triggerEvent(new RepairStatusChangedEvent(unit));
+        } else {
+            totalCharge = newTotalCharge;
+        }
+
+        return totalCharge;
+    }
+
+    /**
+     * Gets the best fallback repair site from the list of active missions, favoring higher-priority sites.
+     *
+     * <p>Returns {@link Unit#SITE_FACILITY_BASIC} if no missions are active.</p>
+     *
+     * @param activeMissions list of currently active missions
+     *
+     * @return fallback repair site constant
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    private static int getFallbackRepairSite(List<Mission> activeMissions) {
+        if (activeMissions.isEmpty()) {
+            return Unit.SITE_FACILITY_BASIC;
+        }
+
+        int fallbackSite = Unit.SITE_IMPROVISED;
+        for (Mission contract : activeMissions) {
+            int newSite = contract.getRepairLocation();
+            if (newSite > fallbackSite) {
+                fallbackSite = newSite;
+            }
+        }
+
+        return fallbackSite;
+    }
+
+    /**
+     * Performs a rental transaction by debiting the corresponding amount for the specified rental type.
+     *
+     * <p>Adjusts rentalType for historical display if needed.</p>
+     *
+     * @param finances   finances object to debit from
+     * @param today      date of the transaction
+     * @param rentalCost amount to be debited
+     * @param rentalType type of rental (MAINTENANCE_BAYS, FACTORY_CONDITIONS, etc.)
+     *
+     * @return {@code true} if the debit was successful
+     *
+     * @author Illiani
+     * @since 0.50.10
+     */
+    private static boolean performRentalTransaction(Finances finances, LocalDate today, Money rentalCost,
+          ContractRentalType rentalType) {
+        if (rentalType == ContractRentalType.FACTORY_CONDITIONS) {
+            rentalType = ContractRentalType.MAINTENANCE_BAYS; // No text differentiates Factory and Maintenance
+        }
+
+        return finances.debit(TransactionType.RENT,
+              today,
+              rentalCost,
+              getFormattedTextAt(RESOURCE_BUNDLE, "PLACEHOLDER" + rentalType, rentalCost.toAmountString()));
+    }
+}
--- MekHQ/src/mekhq/gui/dialog/ContractStartRentalDialog.java
+++ MekHQ/src/mekhq/gui/dialog/ContractStartRentalDialog.java
@@ -1,3 +1,35 @@
+/*
+ * Copyright (C) 2025 The MegaMek Team. All Rights Reserved.
+ *
+ * This file is part of MekHQ.
+ *
+ * MekHQ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License (GPL),
+ * version 3 or (at your option) any later version,
+ * as published by the Free Software Foundation.
+ *
+ * MekHQ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * A copy of the GPL should have been included with this project;
+ * if not, see <https://www.gnu.org/licenses/>.
+ *
+ * NOTICE: The MegaMek organization is a non-profit group of volunteers
+ * creating free software for the BattleTech community.
+ *
+ * MechWarrior, BattleMech, `Mech and AeroTech are registered trademarks
+ * of The Topps Company, Inc. All Rights Reserved.
+ *
+ * Catalyst Game Labs and the Catalyst Game Labs logo are trademarks of
+ * InMediaRes Productions, LLC.
+ *
+ * MechWarrior Copyright Microsoft Corporation. MekHQ was created under
+ * Microsoft's "Game Content Usage Rules"
+ * <https://www.xbox.com/en-US/developers/rules> and it is not endorsed by or
+ * affiliated with Microsoft.
+ */
 package mekhq.gui.dialog;
 
 import static mekhq.utilities.MHQInternationalization.getFormattedTextAt;
@@ -13,23 +45,43 @@ import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 
 import mekhq.campaign.Campaign;
+import mekhq.campaign.finances.Money;
 import mekhq.campaign.mission.rentals.ContractRentalType;
 import mekhq.gui.baseComponents.immersiveDialogs.ImmersiveDialogCore;
 import mekhq.gui.baseComponents.immersiveDialogs.ImmersiveDialogWidth;
 
+/**
+ * Dialog for initiating a facility rental agreement with the player in the campaign.
+ *
+ * <p>Presents the cost, allows the player to input a quantity (if applicable), and offers confirm/cancel choices.</p>
+ *
+ * <p>Used for renting hospital beds, kitchens, holding cells, or similar contract-related facilities.</p>
+ */
 public class ContractStartRentalDialog extends ImmersiveDialogCore {
     private static final String RESOURCE_BUNDLE = "mekhq.resources.FacilityRentals";
 
-    private static final int DIALOG_CANCEL_OPTION = 0;
     private static final int DIALOG_CONFIRM_OPTION = 1;
 
     private static JLabel lblRentalCost;
 
+    /**
+     * Checks if the user confirmed the rental.
+     *
+     * @return {@code true} if the user chose to confirm the rental
+     */
     public boolean wasConfirmed() {
         return this.getDialogChoice() == DIALOG_CONFIRM_OPTION;
     }
 
-    public ContractStartRentalDialog(Campaign campaign, ContractRentalType hospitalBeds, int rentalCost) {
+
+    /**
+     * Constructs a new {@link ContractStartRentalDialog} for the specified campaign and rental type.
+     *
+     * @param campaign   the {@link Campaign} the rental belongs to
+     * @param rentalType the {@link ContractRentalType} of facility being rented (label placeholder may differ)
+     * @param rentalCost the cost per unit or total cost of the rental being considered
+     */
+    public ContractStartRentalDialog(Campaign campaign, ContractRentalType rentalType, Money rentalCost) {
         super(campaign,
               campaign.getSeniorAdminPerson(Campaign.AdministratorSpecialization.LOGISTICS),
               null,
@@ -43,10 +95,22 @@ public class ContractStartRentalDialog extends ImmersiveDialogCore {
               true);
     }
 
+    /**
+     * Gets the formatted message to display in the center of the dialog, usually addressed to the commander.
+     *
+     * @param commanderAddress the name/address/pronoun of the commander for context
+     *
+     * @return the message string to display
+     */
     private static String getCenterMessage(String commanderAddress) {
         return getFormattedTextAt(RESOURCE_BUNDLE, "PLACEHOLDER_MESSAGE", commanderAddress);
     }
 
+    /**
+     * Provides the labeled buttons for the dialog (Cancel and Confirm).
+     *
+     * @return a list of button/tooltip pairs for dialog actions
+     */
     private static List<ButtonLabelTooltipPair> getButtons() {
         return List.of(
               new ButtonLabelTooltipPair(getTextAt(RESOURCE_BUNDLE, "CANCEL"), null),
@@ -54,10 +118,21 @@ public class ContractStartRentalDialog extends ImmersiveDialogCore {
         );
     }
 
+    /**
+     * Gets the out-of-character message (footer/help text) for instructional dialog context.
+     *
+     * @return out-of-character/informational text
+     */
     private static String getOutOfCharacterMessage() {
         return getFormattedTextAt(RESOURCE_BUNDLE, "PLACEHOLDER_MESSAGE");
     }
 
+    /**
+     * Creates the supplemental panel for the dialog, which includes the spinner for rental count and displays the total
+     * cost.
+     *
+     * @return a {@link JPanel} containing the extra controls for the dialog
+     */
     private static JPanel getSupplementalPanel() {
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
