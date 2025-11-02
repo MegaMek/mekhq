3944171c73de7b23dd9d72c6c787fe32a6087866
IllianiBird <danielfarbrace@icloud.com>
2025-11-02 12:40:29 -0600

    Fixed conditional (again)

--- MekHQ/src/mekhq/campaign/rating/CamOpsReputation/AverageExperienceRating.java
+++ MekHQ/src/mekhq/campaign/rating/CamOpsReputation/AverageExperienceRating.java
@@ -155,11 +155,8 @@ public class AverageExperienceRating {
             }
 
             // if both primary and secondary roles are support roles, skip this person as they are also not
-            // considered combat personnel. Combat Technicians need special handling as they're technically a Combat
-            // Role not Support
-            boolean isDedicatedCombatTechnician = person.getPrimaryRole().isCombatTechnician() &&
-                                                        person.getSecondaryRole().isCombatTechnician();
-            if (isDedicatedCombatTechnician || person.isSupport()) {
+            // considered combat personnel.
+            if (person.isSupport()) {
                 continue;
             }
 
