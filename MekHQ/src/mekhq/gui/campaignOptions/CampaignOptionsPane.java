2ac3b4ac397eb48b592bf0c5018de5614ba45ec7
IllianiBird <danielfarbrace@icloud.com>
2025-11-12 12:56:41 -0600

    Improvement: Updated Company Generator to Include Infantry for Prisoner Management; Added Campaign Options Changed Trigger for Prisoner Capacity to Give Players Free Infantry Unit

--- MekHQ/src/mekhq/gui/campaignOptions/CampaignOptionsPane.java
+++ MekHQ/src/mekhq/gui/campaignOptions/CampaignOptionsPane.java
@@ -79,6 +79,7 @@ import mekhq.gui.campaignOptions.optionChangeDialogs.AdvancedScoutingCampaignOpt
 import mekhq.gui.campaignOptions.optionChangeDialogs.FactionStandingCampaignOptionsChangedConfirmationDialog;
 import mekhq.gui.campaignOptions.optionChangeDialogs.FatigueTrackingCampaignOptionsChangedConfirmationDialog;
 import mekhq.gui.campaignOptions.optionChangeDialogs.MASHTheaterTrackingCampaignOptionsChangedConfirmationDialog;
+import mekhq.gui.campaignOptions.optionChangeDialogs.PrisonerTrackingCampaignOptionsChangedConfirmationDialog;
 import mekhq.gui.campaignOptions.optionChangeDialogs.SalvageCampaignOptionsChangedConfirmationDialog;
 import mekhq.gui.campaignOptions.optionChangeDialogs.StratConConvoyCampaignOptionsChangedConfirmationDialog;
 import mekhq.gui.campaignOptions.optionChangeDialogs.VeterancyAwardsCampaignOptionsChangedConfirmationDialog;
@@ -515,6 +516,7 @@ public class CampaignOptionsPane extends AbstractMHQTabbedPane {
         // Store old values for use if we want to trigger certain dialogs
         boolean oldAwardVeterancySPAs = options.isAwardVeterancySPAs();
         boolean oldIsTrackFactionStanding = options.isTrackFactionStanding();
+        boolean oldIsTrackPrisoners = !options.getPrisonerCaptureStyle().isNone();
         boolean oldIsUseMASHTheatres = options.isUseMASHTheatres();
         boolean oldIsUseFatigue = options.isUseFatigue();
         boolean oldIsUseAdvancedSalvage = options.isUseCamOpsSalvage();
@@ -560,7 +562,7 @@ public class CampaignOptionsPane extends AbstractMHQTabbedPane {
         }
 
         boolean newIsTrackFactionStandings = options.isTrackFactionStanding();
-        if (!isStartUp && newIsTrackFactionStandings != oldIsTrackFactionStanding) { // Has tracking changed?
+        if (!isStartUp && newIsTrackFactionStandings && !oldIsTrackFactionStanding) { // Has tracking changed?
             FactionStandingCampaignOptionsChangedConfirmationDialog dialog = new FactionStandingCampaignOptionsChangedConfirmationDialog(
                   null,
                   campaign.getCampaignFactionIcon(),
@@ -587,6 +589,11 @@ public class CampaignOptionsPane extends AbstractMHQTabbedPane {
         boolean newIsUseMASHTheatres = options.isUseMASHTheatres();
         if (!isStartUp && newIsUseMASHTheatres && !oldIsUseMASHTheatres) { // Has tracking changed?
             new MASHTheaterTrackingCampaignOptionsChangedConfirmationDialog(campaign);
+        }
+
+        boolean newIsTrackPrisoners = !options.getPrisonerCaptureStyle().isNone();
+        if (!isStartUp && newIsTrackPrisoners && !oldIsTrackPrisoners) { // Has tracking changed?
+            new PrisonerTrackingCampaignOptionsChangedConfirmationDialog(campaign);
         }
 
         boolean newIsUseFatigue = options.isUseFatigue();
