95cbcab031c546bd237bd5fd19be69e472bf2cd1
IllianiBird <danielfarbrace@icloud.com>
2025-11-23 22:35:20 -0600

    Fix #8317: Fixed Error When Loading Campaign With Non-StratCon Contracts While StratCon is Enabled

--- MekHQ/src/mekhq/campaign/mission/AtBContract.java
+++ MekHQ/src/mekhq/campaign/mission/AtBContract.java
@@ -2468,6 +2468,10 @@ public class AtBContract extends Contract {
      * @since 0.50.10
      */
     public int getRequiredVictoryPoints() {
+        if (stratconCampaignState == null) {
+            return 0;
+        }
+
         double baseRequirement = getRequiredCombatTeams();
 
         int duration = getLength();
