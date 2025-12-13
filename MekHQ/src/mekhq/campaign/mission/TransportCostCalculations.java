ec9beb88f7454814829515bf4eb6f597c0477507
IllianiBird <danielfarbrace@icloud.com>
2025-12-13 15:52:48 -0600

    Copilot

--- MekHQ/src/mekhq/campaign/mission/TransportCostCalculations.java
+++ MekHQ/src/mekhq/campaign/mission/TransportCostCalculations.java
@@ -765,7 +765,7 @@ public class TransportCostCalculations {
      * <ul>
      *     <li>They are not associated with any unit.</li>
      *     <li>Their associated unit does not have an associated entity.</li>
-     *     <li>The entity associated with their unit is not a large craft.</li>
+     *     <li>The entity associated with their unit is not a WarShip, JumpShip, or DropShip.</li>
      * </ul
      *
      * @return The total count of passengers based on the conditions specified.
