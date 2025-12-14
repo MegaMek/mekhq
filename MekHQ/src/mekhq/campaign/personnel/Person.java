283cf29b92c59281adb632d0972d9b99a8b3c79c
IllianiBird <danielfarbrace@icloud.com>
2025-12-13 22:02:56 -0600

    Fix: Fixed Remove All Non-Prosthetic Injuries Only Removing Prosthetic Injuries

--- MekHQ/src/mekhq/campaign/personnel/Person.java
+++ MekHQ/src/mekhq/campaign/personnel/Person.java
@@ -7133,7 +7133,7 @@ public class Person {
     public void clearInjuriesExcludingProsthetics() {
         for (Injury injury : new ArrayList<>(injuries)) {
             InjurySubType injurySubType = injury.getSubType();
-            if (injurySubType.isPermanentModification()) {
+            if (!injurySubType.isPermanentModification()) {
                 removeInjury(injury);
             }
         }
