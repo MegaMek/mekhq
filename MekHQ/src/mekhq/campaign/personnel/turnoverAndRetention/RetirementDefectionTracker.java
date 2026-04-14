228e8852908be6539079510be1fe835e168dea5e
Vicente Cartas <vicente.cartas@outlook.com>
2026-04-13 22:49:21 -0700

    Removing prothesis from injuries list

--- MekHQ/src/mekhq/campaign/personnel/turnoverAndRetention/RetirementDefectionTracker.java
+++ MekHQ/src/mekhq/campaign/personnel/turnoverAndRetention/RetirementDefectionTracker.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2014 - Carl Spain. All rights reserved.
- * Copyright (C) 2014-2025 The MegaMek Team. All Rights Reserved.
+ * Copyright (C) 2014-2026 The MegaMek Team. All Rights Reserved.
  *
  * This file is part of MekHQ.
  *
@@ -399,7 +399,7 @@ public class RetirementDefectionTracker {
             }
 
             // Injury Modifiers
-            int injuryMod = (int) person.getInjuries().stream().filter(Injury::isPermanent).count();
+            int injuryMod = getInjuryTurnoverModifier(person);
 
             if (injuryMod > 0) {
                 targetNumber.addModifier(injuryMod, resources.getString("injuries.text"));
@@ -1045,6 +1045,34 @@ public class RetirementDefectionTracker {
     }
 
     /**
+     * Returns the number of permanent, non-prosthetic injuries for turnover modifier calculation.
+     *
+     * <p>Prosthetics and implants are excluded because they are elective modifications,
+     * not debilitating injuries that would cause a person to leave a unit.</p>
+     *
+     * @param person the person to evaluate
+     * @return count of permanent injuries excluding prosthetics and implants
+     *
+     * @see <a href="https://github.com/MegaMek/mekhq/issues/8631">#8631</a>
+     */
+    static int getInjuryTurnoverModifier(final Person person) {
+        return (int) person.getNonProstheticInjuries().stream().filter(Injury::isPermanent).count();
+    }
+
+    /**
+     * Returns whether a person has permanent injuries (excluding prosthetics/implants)
+     * that qualify them for medical discharge.
+     *
+     * @param person the person to evaluate
+     * @return {@code true} if the person has at least one permanent non-prosthetic injury
+     *
+     * @see <a href="https://github.com/MegaMek/mekhq/issues/8631">#8631</a>
+     */
+    static boolean hasMedicalDischargeInjuries(final Person person) {
+        return person.getNonProstheticInjuries().stream().anyMatch(Injury::isPermanent);
+    }
+
+    /**
      * Class used to record the required payout to each retired/defected/killed/sacked person.
      */
     public static class Payout {
@@ -1081,7 +1109,7 @@ public class RetirementDefectionTracker {
                 payoutAmount = getPayoutOrBonusValue(campaign, person).multipliedBy(campaign.getCampaignOptions()
                                                                                           .getPayoutRetirementMultiplier());
                 // person is getting medically discharged
-            } else if (!person.getPermanentInjuries().isEmpty()) {
+            } else if (hasMedicalDischargeInjuries(person)) {
                 payoutAmount = getPayoutOrBonusValue(campaign, person).multipliedBy(campaign.getCampaignOptions()
                                                                                           .getPayoutRetirementMultiplier());
                 // person is defecting
